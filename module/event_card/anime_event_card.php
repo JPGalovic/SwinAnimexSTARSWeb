f0af697267d855b4cf77ea9634411aad93261709
James P. Galovic <jpgalovic@gmail.com>
2017-12-03 17:20:13 +1100

    Change: Some settings on CSS

--- module/event_card/anime_event_card.php
+++ module/event_card/anime_event_card.php
@@ -1,3 +1,13 @@
 <?php
 	include('sql/events/get_anime_event_data.php');
 ?>
+
+
+<section class="quater" id="event_card">
+	<img src="image/anime/image.png">
+	<h4>EVENT TITLE</h4>
+	<p>EVENT DATE</p>
+	<P>EVENT LOCATION</P>
+	<img src="image/classification/image.png">
+	
+</section>
