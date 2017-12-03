32d66a0bc072c3315355e7b6a9df72dd2b835c1d
James P. Galovic <jpgalovic@gmail.com>
2017-12-03 13:38:06 +1100

    Add: Copy Data File for Anime Data
    Add: Ben-to Example Anime Data
    Modify: Correntions Made to Anime Data Core SQL Data

---
+++ sql/data/anime/madman/ben-to_data.sql
@@ -0,0 +1,95 @@
+INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
+		("Ben-to",
+		 12,
+		 "Every day an epic struggle rages in grocery stores across Japan - the battle for half-priced bento boxes! Once the discount stickers go on, ravenous brawlers start throwing punches in a knockdown, drag-out war over who gets to take home the cheap eats. When a young, broke high school student named Sato joins the Half-Priced Food Lovers Club, he proves to be a rising talent in the world of insane food fights. But does he have what it takes to become the king of clearance cuisine?",
+		 "Every day an epic struggle rages in grocery stores across Japan - the battle for half-priced bento boxes!",
+		 "Madman",
+		 "&copy;Asaura, Kaito Shibano / SHUEISHA, HalfPrice-club"
+		)
+;
+
+INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
+		("Ben-to",
+		 3,
+		 1,
+		 "M - Sexual references",
+		 12,
+		 "https://www.madman.com.au/catalogue/view/23702/"
+		)
+	,	("Ben-to",
+		 4,
+		 1,
+		 "M - Sexual references",
+		 12,
+		 "https://www.madman.com.au/catalogue/view/23703/"
+		)
+;
+
+INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SYNOPSYS) VALUES
+		("Ben-to",
+		 1,
+		 "Stick With It, Natto Okra Rice Bowl with Cheese Topping Bento 440kcal",
+		 "You Satou finds himself beaten up on the supermarket floor, the last thing he remembers being going to get some half-priced bento. The next day, he encounters a silver haired girl who warns him not to go to the store again. He goes again alongside another student, Hana Oshiroi, and finds himself beaten up again upon reaching for the half price bento. The next day, he decides to act more strategically and witnesses an all-out battle for the bento that gets marked half-price, which is dominated by the silver haired girl, Sen Yarizui. After the battle ends in total defeat for You, Sen tells him to come to the Half-Priced Food Lovers club room."
+		)
+	,	("Ben-to",
+		 2,
+		 "Mackerel Boiled in Miso Bento 674kcal",
+		 "Sen gets You and Hana to join the Half-Priced Food Lovers Club and starts training them in the ways of Bento Brawling. During one of the supermarket visits, they encounter a 'boar', a fat woman who raids all the half-priced bentos before a battle starts. The next day, You meets Yū 'Wizard' Kaneshiro, who encourages You to stand up to a boar so that the bento battle can properly take place. You comes to understand the pride of the 'wolves' and comes out with his first victory, obtaining a bento with an honor seal from the 'God of Discounts'. You then returns to the clubroom, where Sen congratulates him on his first victory and shows him a scrapbook showing the victories Yū managed to obtain."
+		)
+	,	("Ben-to",
+		 3,
+		 "Large Cheese Pork Cutlet Curry Rice 1080kcal",
+		 "You is beaten up by the class president, Ume Shiraume, due to him hanging around Hana. As You and Hana go to a supermarket, they meet a kendo club member named Yamahara, who invites them to join a group called the Hounds, who use teamwork to help members get half-priced bento. As You and Hana take a trial period with the group, You feels the bento he obtains this method don't taste as good as he thinks. After reminiscing about old times, You turns down Yamahara's offer, realizing that food tastes best when you fight for it. Having realized the true meaning of half-price bento, Sen gives You and Hana keys to the club room. The next morning, You finds his cousin, Ayame Shaga, sleeping in his bed."
+		)
+	,	("Ben-to",
+		 4,
+		 "	Ginger Fried Pork Bento 852kcal",
+		 "Ayame is revealed to be a wolf known as 'The Beauty of the Lake' who throws down a challenge to Sen. After barely escaping from the lust of Ume, Ayame accompanies You and Sen to a bento brawl, where she and Sen fight for the same bento. As Sen manages to defeat Ayame and claim her bento, You finds himself against a tough opponent but manages to win in the end."
+		)
+	,	("Ben-to",
+		 5,
+		 "Hokkaido Salmon Fish Soup 326kcal",
+		 "You goes through some hassle to meet up with Ayame, where he also meets Asebi Inoue, who gives bad luck to anyone she touches. As You goes to a store with the others, he finds himself up against Ren Nikaidou, who is part of a group known as Gabriel Ratchett, and gets severely beaten by Tadaaki 'Monarch' Endo. After You regains consciousness, Ayame explains how Endo seeks revenge for some ancient slight, while Sen questions why she didn't warn You about him."
+		)
+	,	("Ben-to",
+		 6,
+		 "Special Hokkaido Style Fried Chicken Bento 795kcal",
+		 "As Sen heads towards a store alone, Ayame learns about how Yū triumphed over the previous monarch butterfly, Matsuba, with Endo, her successor, wanting revenge on Yū's successor, Sen. As Ayame warns You about this, Sen, who had gotten severely beaten up by the Gabriel Retchetts, arrives at the Ralph Store to face against Endo. As Ren feels regret after hearing words of disappointment from Matsuba, the current God of Discounts, he tries to stop Endo but is knocked away. As the brawl begins, You and Ayame arrive and together they manage to defeat Endo. Afterwards, Ren follows Matsuba's advice and starts to live for himself. Meanwhile, Yū leaves the country to study overseas, and Matsuba bids him farewell, after reminiscing about their bento brawl with each other and discussing Endo's plan to succeed her."
+		)
+	,	("Ben-to",
+		 7,
+		 "Bosomelet Bento 752kcal and Loco Moco Bulging Bento 1100kcal",
+		 "Matsuba gives You, Sen, Ayame and Hana free tickets to an indoor swimming pool, where they also run into Ren. There, they discover even a swimming pool has half-priced bento, featuring a unique bento brawl across the pool. As the waves provide a fierce battlefield, You manages to achieve his prey, albeit not without an embarrassing comeuppance."
+		)
+	,	("Ben-to",
+		 8,
+		 "Hamburg Steak Loaded with Garlic Chives Bento 765kcal",
+		 "You becomes hospitalized after jumping out of a window while trying to protect his Sega Saturn that Sen threw out the window, ending up completely wrapped in bandages. Meanwhile, a pair of twin sisters from Ayame's school both named Kyou Sawagi, the older of which appears to have a thing for Sen, infiltrate the hospital hoping to meet Sen. They mistake You for Sen and perform some curious inspections on him, leading to chaos when they actually learn the truth."
+		)
+	,	("Ben-to",
+		 9,
+		 "Deluxe Western and Japanese Food Assortment 2910kcal and Piping Hot Porridge Bento 340kcal",
+		 "Asebi decides to make You a homemade bento, in which every component tastes of something completely different. Meanwhile, Ume meets up with the Sawagi sisters concerning both You and Ayame trespassing on each others' schools. Later that night, as Hana has an accident with her computer while finishing up one of her erotic novels, Ume believes her to have fallen sick and comes over to tend to her 'fever'. As she does, she discovers one of Hana's books which she had posted You's face all over. Ume hits Yo once again, but not before he reveals Hana's passion for writing yaoi."
+		)
+	,	("Ben-to",
+		 10,
+		 "A Warm, Gentle Flavor I Remember from Long Ago at My Grandmother`s House. A Japanese Dish Easy on Both Mind and Body. Plum and Dried Sardines over Rice with Plenty of Stewed Seasonal Vegetables Bento 480kcal",
+		 "Ayame is visited by Ren who tells her about a group called Orthros who are severely attacking other wolves and placing baskets on their heads. As You resumes his bento brawl duties, he becomes the latest victim to the Sawagi sisters. As You, Sen, Ayame and the others get together at a supermarket for a bento brawl the next day, Sen soon finds herself attacked by the sisters and, despite assistance from the others, is severely beaten."
+		)
+	,	("Ben-to",
+		 11,
+		 "Pocari Sweat 125kcal",
+		 "You goes to visit Sen, who had been absent from school with a cold, where she discusses plans to fight against the twins for eel bentos in a few days. After another defeat by the twins, You and Ren learn from Ayame that they are the daughters of a chain store owner who came over from Yumehiro. Ren travels to Yumehiro to find out more while Ayame passes on Sen's message to the twins, although Sen's cold has gotten worse. Ren learns that, three years ago, the twins were driven out of Yumehiro by a brawler known as The Club of Hercules."
+		)
+	,	("Ben-to",
+		 12,
+		 "Domestic Eel Bento 790kcal",
+		 "With four hours left until the bento brawl and Sen still not over her fever, You decides to let her sleep in while he goes in her stead. On the way he encounters Hercules, who tells him something that causes him to run away. As the twins arrive at the store, they encounter Hercules, who is forcing the cooperation of the other wolves to exclude them from the fight, just like he did three years ago. However, You returns, having run to build up an appetite which reminds the other wolves why they fight and convinces them to brawl fairly. After beating Hercules, Yo manages to convince the twins to join in the fight, where the twins are defeated and You wins the prize seal to share with Sen."
+		)
+;
+
+INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
+		("Ben-to", 0, 1, 4)
+	,	("Ben-to", 0, 2, 4)
+	,	("Ben-to", 0, 3, 4)
+;
