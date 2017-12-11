3164d2367ff3131aea254e586bebf16897c76fb6
C1and00j <34433447+C1and00j@users.noreply.github.com>
2017-12-11 17:03:13 +1100

    Add' Highschool dxd SQL file

---
+++ sql/data/anime/madman/Highschool_dxd_data.sql
@@ -0,0 +1,107 @@
+-- Anime Data
+INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
+		("High School DxD",
+		 12,
+		 "The story centers around Issei Hyoudou, a perverted high school student who was nearly nearly killed by his first date, later revealed to be a Fallen Angel. He is soon saved by Rias Gremory, a crimson-haired beauty who reveals herself as a Devil of the Gremory Family, and finds out that he himself has been reborn as a Devil made to serve her. The story focuses on the ongoing battle among the Fallen Angels, Angels, and Devils, and Issei's ongoing relationship with Rias, her peerage, alongside other Devils and Angels.",
+		 "The story centers around Issei Hyoudou, a perverted high school student who was nearly nearly killed by his first date, later revealed to be a Fallen Angel. He is soon saved by Rias Gremory, a crimson-haired beauty who reveals herself as a Devil of the Gremory Family, and finds out that he himself has been reborn as a Devil made to serve her. The story focuses on the ongoing battle among the Fallen Angels, Angels, and Devils, and Issei's ongoing relationship with Rias, her peerage, alongside other Devils and Angels.",
+		 "Madman"
+		 "@copy;Tetsuya Yanagisawa, Takao Yoshioka / TNK"
+		)
+;
+
+INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
+		("High School DxD",
+		 4,
+		 1,
+		 "MA 15+ - Strong sexual references, animated nudity and violence",
+		 12,
+		 "https://www.madman.com.au/catalogue/view/20932"
+		)
+;
+
+INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SYNOPSYS) VALUES
+       ,	("Highschool DxD",
+		 1,
+		 I Got a Girlfriend!,
+		 "Issei Hyodo is a high school student who is unpopular with girls at Kuoh Academy. One day, Rias Gremory, the crimson-red haired top idol of the academy and president of the Occult Research Club, discovers Issei and asks her vice-president Akeno Himejima about his identity. As he dreams of having a girlfriend, another girl named Yuma Amano shyly asks Issei out, to his excitement. Shortly afterwards, Yuma asks Issei to die for her sake and reveals herself as Raynare, a fallen angel. After Raynare kills Issei, he sees a vision of Rias. Assuming the incident to be a dream, he resumes his normal life. However, when Issei encounters another fallen angel, Dohnaseek, he is wounded. Just as Dohnaseek is about to finish Issei, Rias intervenes and saves him. The next morning, Issei discovers he and Rias are naked together in his bedroom. Reassuring that the dreams were real, Rias introduces herself as a devil and his new master. "
+		)
+       ,         ("Highschool DxD",
+		 2,
+		 "I'm Done Being Human!",
+		 "Rias explains to Issei that she used her magic to heal his wounds as she gets ready for school, and also reveals that she is a virgin. At Kuoh Academy, the school prince Yuto Kiba leads Issei to the Occult Research Club where he meets the popular "mascot" girl Koneko Toujou and the club's vice-president Akeno Himejima. Rias briefs Issei on the current situation with angels, fallen angels, and devils; and the duties assigned to him. After going to his first assignment which involves an otaku, he encounters another fallen angel named Kalawarner, but uses his Sacred Gear to transform his hand into an armored Dragon Arm and defeats her. Shortly afterwards, Rias warns Issei that since he has the Sacred Gear, he will be sought after by the fallen angels. "
+		)
+	,	("Highschool DxD",
+		 3,
+		 "I Made a Friend!",
+		 "Issei befriends a teenage nun Asia Argento but hides his devil identity from her. After Rias warns Issei about the dangers of the Church, the team must battle a stray devil. Rias briefs Issei on the Evil Pieces system where each member of her team has a certain role similar to chess pieces, where Rias is the king, but Issei is the pawn. Issei's next assignment becomes difficult when he must deal with a ruthless exorcist Freed Sellzen. However, Asia, working for Freed, witnesses the incident and attempts to stop Freed from killing Issei. Rias and the other members rescue Issei but leave Asia behind, to his dismay. "
+		)
+	,	("Highschool DxD",
+		 4,
+		 "I'm Saving My Friend!",
+		 "While on a date with Asia, Issei finds out her past as an excommunicated nun from the Church. Issei vows to be her friend regardless of being a devil, but they are interrupted by Raynare, who captures Asia. Later, Rias explains a special ability that pawns have: Promotion. She suddenly leaves with Akeno, leaving an ambiguous note about the Church as enemy grounds. Issei remembers Raynare's speech about a ritual with Asia tonight and is determined to rescue her. As Issei heads for the Church, Kiba and Koneko follow him having understood Rias's message. They storm into the sanctuary and defeat Freed, however, Issei finds Asia chained to a cross where Raynare is about to complete her ritual. "
+		)
+	,	("Highschool DxD",
+		 5,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Highschool DxD",
+		 6,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Highschool DxD",
+		 7,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Highschool DxD",
+		 8,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Highschool DxD",
+		 9,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Highschool DxD",
+		 10,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Highschool DxD",
+		 11,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Highschool DxD",
+		 12,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+
+;
+
+INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
+		("Title", x, y, z)
+;
+		
+-- General Anime Volume Data
+	,	("Title",
+		 n,
+		 m,
+		 "Classification",
+		 x,
+		 "URL"
+		)
+		
+-- General Anime Episode Data
+	,	("Title",
+		 n,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+		
+-- General Anime Session Data
+	,	("Title", x, y, z)
