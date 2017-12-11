39463961a4295edf80ab82a1912b191cb5c2b51d
C1and00j <34433447+C1and00j@users.noreply.github.com>
2017-12-11 17:33:47 +1100

    Change: added episodes 5-12, added dvd infomation volume data

--- sql/data/anime/madman/Highschool_dxd_data.sql
+++ sql/data/anime/madman/Highschool_dxd_data.sql
@@ -5,17 +5,23 @@ INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIP
 		 "The story centers around Issei Hyoudou, a perverted high school student who was nearly nearly killed by his first date, later revealed to be a Fallen Angel. He is soon saved by Rias Gremory, a crimson-haired beauty who reveals herself as a Devil of the Gremory Family, and finds out that he himself has been reborn as a Devil made to serve her. The story focuses on the ongoing battle among the Fallen Angels, Angels, and Devils, and Issei's ongoing relationship with Rias, her peerage, alongside other Devils and Angels.",
 		 "The story centers around Issei Hyoudou, a perverted high school student who was nearly nearly killed by his first date, later revealed to be a Fallen Angel. He is soon saved by Rias Gremory, a crimson-haired beauty who reveals herself as a Devil of the Gremory Family, and finds out that he himself has been reborn as a Devil made to serve her. The story focuses on the ongoing battle among the Fallen Angels, Angels, and Devils, and Issei's ongoing relationship with Rias, her peerage, alongside other Devils and Angels.",
 		 "Madman"
-		 "@copy;Tetsuya Yanagisawa, Takao Yoshioka / TNK"
+		 "&copy;Tetsuya Yanagisawa, Takao Yoshioka / TNK"
 		)
 ;
 
 INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
-		("High School DxD",
+		("High School DxD (Blu-Ray)",
 		 4,
 		 1,
-		 "MA 15+ - Strong sexual references, animated nudity and violence",
+		 "MA15+ - Strong sexual references, animated nudity and violence",
 		 12,
 		 "https://www.madman.com.au/catalogue/view/20932"
+	,	("High School DxD (DVD)",
+		 4,
+		 1,
+		 "MA15+ - Strong sexual references, animated nudity and violence",
+		 12,
+		 "https://www.madman.com.au/catalogue/view/20931"
 		)
 ;
 
@@ -42,66 +48,49 @@ INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SY
 		)
 	,	("Highschool DxD",
 		 5,
-		 "Episode_Title",
-		 "Episode_Synopsys"
+		 "I Will Defeat My Ex-girlfriend!",
+		 "Raynare removes Asia's sacred gear and puts it into herself, killing Asia. She even releases Asia to Issei, but mentions the price a host pays if their Sacred Gear is removed or destroyed. She is surprised that her superiors were wary of his Sacred Gear as its ability is only able to double the power of its user, especially when Issei is so weak that he is not a threat even with his power doubled. Issei furiously prays to God and Lucifer before the extreme intensity of his emotions unlocks an ability in his Sacred Gear revealing just part of its real potential. In a state of righteous manic rage he is able to use his newfound power to knock out Raynare in a single devastating punch even after she had impaled both of his legs. Rias appears and admonishes Raynare for both underestimating Issei's Sacred Gear and for injuring one of her servants. In a desperate attempt to manipulate him into trusting her again, Raynare changes into her human form and pleads for him to protect her. Angered by Raynare's behavior and with his heart breaking, Issei orders Rias to kill Raynare. Shortly afterwards, Rias revives Asia as a bishop. "
 		)
 	,	("Highschool DxD",
 		 6,
-		 "Episode_Title",
-		 "Episode_Synopsys"
+		 ""I Work as a Devil!",
+		 "After Raynare's demise, new living arrangements are made for Asia, who has been living in the club's room. Meanwhile, Issei begins a new training regimen with Rias to improve his physical fitness. During training, Issei is surprised to find Asia and her boxes. Rias asked Asia her living preferences and she chose Issei's home. Asia also begins attending school where she is placed in Issei's class. She quickly attracts other classmates including jealousy from Matsuda and Motohama. The following night, Issei is assigned another job and is unable to receive a pact. Issei thinks it over during P.E. class and ends up sleeping in the infirmary along with Rias. Noting Issei's lecherous desires, Rias offers her body to him in exchange for a pact. That night, Rias accompanies him on his next job: helping a foreign exchange student named Susan inside a Samurai outfit to retrieve her notebook. She also requests help to attract her love. After the unusual meet up with her love, Issei successfully completes the job. He tries to get his reward from Rias, but loses when the other members arrive. Rias instead gives him a hug, shocking everyone. "
 		)
 	,	("Highschool DxD",
 		 7,
-		 "Episode_Title",
-		 "Episode_Synopsys"
+		 "I Get a Familiar!",
+		 "Rias decides that it is time for Issei and Asia to have servant familiars. The school's student council visits the club room, and Issei learns that they are another faction of devils. Since only one group is allowed to enter the world of familiars per month, the two groups compete to see who gets to go first. As they compete in a tennis match, what starts as a normal human competitions turns frightful as Rias and Sona use magic to enhance their moves. They end up destroying much of the equipment, leading to a draw. Rias's group later wins in the dodge ball match, allowing them to pick new familiars first. During the search for familiars, they are greeted by Master Familiar Towji. He guides Issei and Asia throughout the forest and shows different creatures they can choose. The group later finds a Sprite Dragon, a familiar difficult to obtain. Suddenly, slime litters the forest and obliterates clothes. Issei wants to make slime as his familiar and uses Asia to protect it. He is knocked out by the Sprite Dragon, who has taken a liking to Asia. In the end, Asia accepts him as a familiar while Rias notes Issei still has ways to go. "
 		)
 	,	("Highschool DxD",
 		 8,
-		 "Episode_Title",
-		 "Episode_Synopsys"
+		 "I Pick a Fight!",
+		 "Issei dreams that he is marrying Rias, which leads him to get in touch with his inner dragon. After school, Issei encounters Asia in the shower, who confesses a desire to have a deeper relationship with Issei. Later, Rias appears in Issei's bedroom that night looking to lose her virginity. However, her attempt is suddenly interrupted by the arrival of Grayfia Lucifuge, the wife and queen of Rias's brother Sirzechs Lucifer, who initially mocks Issei as a lowborn. At Kuoh Academy, Issei and his group are introduced to Riser Phoenix, a devil who is to be engaged to Rias. When Riser proposes to Rias, she declines. He mocks Issei's dream of having a harem, leading to him having one of her pieces hit Issei in the stomach. In response, the Gremory and the Phoenix clans organize a ratings game. "
 		)
 	,	("Highschool DxD",
 		 9,
-		 "Episode_Title",
-		 "Episode_Synopsys"
+		 "I've Begun My Training!",
+		 "Issei begins to train in an isolated area under Rias' supervision as they prepare for the rating game Rias agreed to fight with Phoenix in ten days time. There, Issei creates his special move, called Dress Break. "
 		)
 	,	("Highschool DxD",
 		 10,
-		 "Episode_Title",
-		 "Episode_Synopsys"
+		 "The Showdown Begins!",
+		 "The battle between the Phoenix clan and the Gremory clan begins. Issei uses an embarrassing finishing move but proves his worth. The Gremory team suffers their first loss when a comrade fights against superior numbers of the Phoenix clan. Issei, enraged by the fate of his comrade, challenges the enemy Queen. "
 		)
 	,	("Highschool DxD",
 		 11,
-		 "Episode_Title",
-		 "Episode_Synopsys"
+		 "The Acclaimed Battle Continues!",
+		 "The Rating Game continues. Both sides endure further losses and Riser has not received a scratch due to the Phoenix family's special ability. Rias concludes the game by making a hard choice. "
 		)
 	,	("Highschool DxD",
 		 12,
-		 "Episode_Title",
-		 "Episode_Synopsys"
+		 "I'm Here to Keep My Promise!",
+		 "Issei wakes up to find himself passed out in his own bed after the Rating Game. Soon after, Grayfia informs Issei the results of the Rating Game. She gives Issei a magic circle that will allow him to attend the Gremory-Phoenix engagement party with Sirzechs's message that if Issei wants to do something, then to go in and rescue Rias by force. Upon arriving, Issei crashes the party, but is interrupted when Sirzechs comes in and offers to host a one-on-one battle between Riser and Issei to liven things up since he found the Rating Game unsatisfying. Riser and Issei engage in fierce duel that ends with him hitting Riser with holy water. Issei discovers what was on the back of the transport circle Grayfia gave him. Sirzechs discusses his feelings and plans about Rias' forced engagement with Grayfia. Rias, overwhelmed by Issei's feelings and determination, acts on impulse. As a result of the duel between Dragon and Phoenix, some changes are going to occur at Issei's house while Asia and Akeno try to help him deal with the consequences of a choice he made. "
 		)
 
 ;
 
 INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
-		("Title", x, y, z)
+		("Highschool DxD", 0, 1, 4)
+	,	("Highschool DxD", 0, 2, 4)
+	,	("Highschool DxD", 0, 3, 4)
 ;
-		
--- General Anime Volume Data
-	,	("Title",
-		 n,
-		 m,
-		 "Classification",
-		 x,
-		 "URL"
-		)
-		
--- General Anime Episode Data
-	,	("Title",
-		 n,
-		 "Episode_Title",
-		 "Episode_Synopsys"
-		)
-		
--- General Anime Session Data
-	,	("Title", x, y, z)
