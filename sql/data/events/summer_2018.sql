60a3c5a72636440681191f472854d181efd17238
James P. Galovic <jpgalovic@gmail.com>
2017-12-11 19:43:27 +1100

    Change: Add New Event Data & Fix Full Data PHP Generator

--- sql/data/events/summer_2018.sql
+++ sql/data/events/summer_2018.sql
@@ -1,17 +1,51 @@
 INSERT INTO EVENT_DATA (EVENT_TIME, EVENT_TYPE_ID, EVENT_TITLE, EVENT_LOCATION, EVENT_FACEBOOK_ID, EVENT_UNIONE_URL) VALUES
-		("2018-01-05 16-30-00", 0, "Summer Sessions", 0, 0, "0")
+		("2018-01-05 11-30-00", 0, "Summer Sessions", 0, 0, "0")
+	,	("2018-01-12 11-30-00", 0, "Summer Sessions", 0, 0, "0")
+	,	("2018-01-19 11-30-00", 0, "Summer Sessions", 0, 0, "0")
+	
+	,	("2018-01-05 16-30-00", 0, "Summer Sessions", 0, 0, "0")
 	,	("2018-01-12 16-30-00", 0, "Summer Sessions", 0, 0, "0")
 	,	("2018-01-19 16-30-00", 0, "Summer Sessions", 0, 0, "0")
+	
+	,	("2018-01-05 19-30-00", 0, "Summer After Dark", 0, 0, "0")
+	,	("2018-01-12 19-30-00", 0, "Summer After Dark", 0, 0, "0")
+	,	("2018-01-19 19-30-00", 0, "Summer After Dark", 0, 0, "0")
+	
 	,	("2018-01-26 11-30-00", 0, "Summer Sessions", 0, 0, "0")
-	,	("2018-02-02 11-30-00", 0, "Summer Sessions", 0, 0, "0")
-	,	("2018-02-09 11-30-00", 0, "Summer Sessions", 0, 0, "0")
+	,	("2018-01-02 11-30-00", 0, "Summer Sessions", 0, 0, "0")
+	,	("2018-01-09 11-30-00", 0, "Summer Sessions", 0, 0, "0")
+	
+	,	("2018-01-05 16-30-00", 0, "Summer Sessions", 0, 0, "0")
+	,	("2018-01-12 16-30-00", 0, "Summer Sessions", 0, 0, "0")
+	,	("2018-01-19 16-30-00", 0, "Summer Sessions", 0, 0, "0")
+	
+	,	("2018-01-05 19-30-00", 0, "Summer After Dark", 0, 0, "0")
+	,	("2018-01-12 19-30-00", 0, "Summer After Dark", 0, 0, "0")
+	,	("2018-01-19 19-30-00", 0, "Summer After Dark", 0, 0, "0")
 ;
 
 INSERT INTO EVENT_ANIME_DATA (EVENT_TIME, ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER) VALUES
-		("2018-01-05 16-30-00", "Yurikuma Arashi", 0, 1)
+		("2018-01-05 11-30-00", "Mawaru Penguindrum", 0, 1)
+	,	("2018-01-12 11-30-00", "Mawaru Penguindrum", 0, 2)
+	,	("2018-01-19 11-30-00", "Mawaru Penguindrum", 0, 3)
+	
+	,	("2018-01-05 16-30-00", "Yurikuma Arashi", 0, 1)
 	,	("2018-01-12 16-30-00", "Yurikuma Arashi", 0, 2)
 	,	("2018-01-19 16-30-00", "Yurikuma Arashi", 0, 3)
+	
+	,	("2018-01-05 19-30-00", "Plastic Memories", 0, 1)
+	,	("2018-01-12 19-30-00", "Plastic Memories", 0, 2)
+	,	("2018-01-19 19-30-00", "Plastic Memories", 0, 3)
+	
 	,	("2018-01-26 11-30-00", "Assassination Classroom Season 2", 0, 1)
 	,	("2018-02-02 11-30-00", "Assassination Classroom Season 2", 0, 2)
 	,	("2018-02-09 11-30-00", "Assassination Classroom Season 2", 0, 3)
+	
+	,	("2018-01-26 16-30-00", "Anime_Title", 0, 1)
+	,	("2018-02-02 16-30-00", "Anime_Title", 0, 2)
+	,	("2018-02-09 16-30-00", "Anime_Title", 0, 3)
+	
+	,	("2018-01-26 19-30-00", "Queen's Blade 2: The Evil Eye", 0, 1)
+	,	("2018-02-02 19-30-00", "Queen's Blade 2: The Evil Eye", 0, 2)
+	,	("2018-02-09 19-30-00", "Queen's Blade 2: The Evil Eye", 0, 3)
 ;
--- sql/data/full.php
+++ sql/data/full.php
@@ -7,9 +7,16 @@
 	include('event_data_core.sql');
 	
 	// Anime Data
-	include('anime/madman/ben-to_data.sql');
-	include('anime/madman/yurikuma_arashi_data.sql');
+	include('anime/hanabee/plastic_memories_data.sql');
+
+	include('anime/madman/assassination_classroom_data.sql');
 	include('anime/madman/assassination_classroom_season_2_data.sql');
+	include('anime/madman/ben-to_data.sql');
+	include('anime/madman/highschool_dxd_data.sql');
+	include('anime/madman/highschool_dxd_season_2_data.sql');
+	include('anime/madman/yurikuma_arashi_data.sql');
+	
+	include('anime/siren/mawaru_penguindrum_data.sql');
 	
 	// Events Data
 	include('events/summer_2018.sql');
