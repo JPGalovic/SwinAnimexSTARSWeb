aba6858610dc88c6d7b7421cfaab622443d9b39e
C1and00j <34433447+C1and00j@users.noreply.github.com>
2017-12-11 18:41:25 +1100

    Add: Highschool dxd SQL file

---
+++ sql/data/anime/madman/Highschool_dxd_new_data.sql
@@ -0,0 +1,94 @@
+-- Anime Data
+INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
+		("HighSchool Dxd New",
+		 12,
+		 "The devilish haremking Issei Hyoudou is back for more outrageous Oppai Battle Entertainment in the scintillating second season of High School DxD! Fans who like their hardcore action spiked with a double dose of fan service won't be able to pry their eyes off all the scorching supernatural schoolgirls waiting for them in Season Two. So what are you waiting for? Plant yourself in front of the boob tube and watch a harem of devil hotties show you how much fun it is to be bad!",
+		 "The devilish haremking Issei Hyoudou is back for more outrageous Oppai Battle Entertainment in the scintillating second season of High School DxD!",
+		 "Madman",
+		 "&copy;Tetsuya Yanagisawa, Takao Yoshioka / TNK"
+		)
+;
+
+INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
+		("HighSchool Dxd New",
+		 4,
+		 1,
+		 "MA15+ - Strong sexual references and frequent animated nudity",
+		 12,
+		 "https://www.madman.com.au/catalogue/view/30434"
+		)
+		("HighSchool Dxd New",
+		 3,
+		 1,
+		 "MA15+ - Strong sexual references and frequent animated nudity",
+		 12,
+		 "https://www.madman.com.au/catalogue/view/30433"
+		)
+;
+
+INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SYNOPSYS) VALUES
+		("HighSchool Dxd New",
+		 1,
+		 "Another Disquieting Premonition!",
+		 "After a hectic morning with Rias and Asia, Issei's Dragon arm awakes forcing Issei to have Akeno remove the Dragon Power. While looking through photos, Kiba finds a Holy Sword among the photos of Issei and his friend while playing at that friend's house. Later, while on the way home after Issei completed a contract with his new client, Issei and his friends are ordered to hunt down a Stray Devil at an abandoned factory. The team manages to defeat the Stray Devil. Kiba is oblivious to Rias' questions about Kiba's actions. Kiba reveals to Issei that he plots revenge against the Holy Sword Excalibur. Later at his house, Issei finds out that Kiba was once part of a project called &quot;The Holy Sword Project&quot; being the only survivor of it. On his way back, Kiba witnesses a priest being killed by Freed being further enraged by the Excalibur Freed was wielding, leading to a battle between the two. Meanwhile, two strangers arrive in Issei's town, one of which holds a copy of the photo in Issei's album depicting Issei playing with her. "
+		)
+		("HighSchool Dxd New",
+		 2,
+		 "The Holy Sword Is Here!",
+		 "Ddraig tells Issei about the &quot;Vanishing Dragon&quot;, Albion. Meanwhile on her way to school with Tsubaki, Sona notices the strangers standing outside the school gate and the Holy Sword that one of them is wielding. Later at the Occult Research Club, Issei finds out about the &quot;Holy Sword Project&quot;, where he was one of the failed products who fled from his death. Heading home, Issei and Asia find the strangers talking to his mother, and to the latter's surprise, one of them is his childhood friend Irina Shidou. The next day Irina and the other stranger, Xenovia, arrive at the Occult Research Club to tell them that they are to retrieve the stolen Excalibur swords without any intervention from the devils. While leaving, Xenovia realizes that Asia has been condemned as a witch and threatens to kill Asia. Enraged, Issei challenges Xenovia and Irina to a duel with Kiba following in pursuit. "
+		)
+		("HighSchool Dxd New",
+		 3,
+		 "I'll Destroy the Holy Sword!",
+		 "Issei and Kiba are overpowered by Xenovia and Irina. Before leaving, Rias asks the two about the Fallen Angel who stole the Excaliburs learning that the one who was responsible was the Fallen Angel Leader, Kokabiel. Back at the Occult Research Club, Kiba leaves the building with extreme frustration over his loss to Xenovia. Issei decides to help Kiba exact his revenge on the Excalibur swords. Coercing Saji into his plans with Koneko's help, the three start to search for Xenovia and Irina, eventually finding them in a street. The trio then form an alliance with the two, helping them to retrieve the Excalibur swords while handing them an Excalibur sword so that Kiba can destroy it. Kiba, touched by Issei and Koneko's action, agrees to accept the help of the two, thanking them for their help. Later at night, Issei meets up with Kiba and the others to start their plan to destroy the Excalibur. Meanwhile, a young man appears beside Issei's client. "
+		)
+		("HighSchool Dxd New",
+		 4,
+		 "A Strong Enemy Appeared!",
+		 "Issei and the others start their operation to search for Freed and the Excalibur, later finding the ex-priest at the abandoned warehouse previously used by Stray Devil Viser. Kiba engages Freed in a one-on-one battle until Issei, Saji and Koneko intervene giving Kiba the advantage. However, Valper Galilei advises Freed to control his Excalibur better, and manages to reverse the situation until the timely arrival of Xenovia and Irina. Valper and Freed flee, forcing Kiba, Xenovia and Irina to chase after the two. However, Rias, Sona, Akeno and Tsubaki intervene and Issei and Saji are punished by their respective masters for taking action without permission. Back at the Hyodo Residence, Asia greets Rias and Issei in a naked apron as Rias decides to change into one as well. The next day, Rias, Akeno and Koneko send their familiars to search for Kiba, Xenovia and Irina throughout the city, finding an injured Irina lying in the outskirts of town. After healing Irina with Asia's help, Tsubaki takes Irina to the Sitri residence for further treatment. Freed reveals that he now has four Excalibur swords with him after stealing Irina's Excalibur Mimic under Kokabiel's orders as part of his plan to start another Great War using all seven Excaliburs. Disappointed that Michael only sent exorcists, Kokabiel decides to target the Devils of Kuoh Academy to force the Satans to take action. After the Fallen Angel leader uses a small attack as a diversion, Issei and the others head back to the school in preparation for their upcoming battle with Kokabiel. Meanwhile at Kuoh Academy, Valper and Freed are shown to be inside a magic circle undergoing an unknown ceremony, with Kiba and Xenovia realizing the situation from afar. "
+		)
+		("HighSchool Dxd New",
+		 5,
+		 "Decisive Battle at Kuoh Academy!",
+		 "Sona and her team form a barrier surrounding the school while Rias and her team prepare for their battle against Kokabiel, Freed and Valper. The battle begins with Kokabiel summoning several Cerberus demons to battle the Gremory Team, which the protagonists successfully defeats with the help of Xenovia. During that time, Valper successfully combines the four Excaliburs while triggering a self-destruct magic circle that will activate in 20 minutes as a stranger looks on from afar. Rias and Akeno then attempt to battle Kokabiel but prove useless against his powers. Meanwhile, Kiba confronts Valper and realizes the truth of the Holy Sword Project. With the help of his friends, Kiba successfully attains his Balance Breaker, and together with Xenovia who summons her Durandal they successfully destroy the Fused Excalibur. Kiba attempts to kill Valper but Kokabiel kills him and reveals the Occult Research Club that Issei can transfer his power to anyone as Issei prepares to transfer his powers to Rias. "
+		)
+		("HighSchool Dxd New",
+		 6,
+		 "Go! Occult Research Club!",
+		 "Issei transfers his boosted powers to Rias, but both Rias and Akeno are overpowered by Kokabiel. Realizing Akeno is the daughter of the Fallen Angel Cadre, Baraqiel, Kokabiel mocks Rias saying that she has a habit of taking in weird people like her brother. Enraged, Issei plans to attack Kokabiel. Kiba, Xenovia and Koneko attack Kokabiel to buy time for Issei to boost his powers, but Kokabiel overpowers them. He then reveals that in the previous Great War, both the Four Great Satans and God were killed, much to Xenovia and Asia's horror. Despite this, Issei states his dream of becoming a Harem King and attacks Kokabiel, reigniting his friends' fighting spirit. However, the barrier surrounding the school field is destroyed by the stranger, calling himself the White Dragon Emperor Albion, he ultimately defeats Kokabiel and negates the self destruct spell on the school, before taking Kokabiel and Freed away from the field. With Kokabiel gone, the academy's student council repairs the school while Kiba vows once more to serve Rias along with her other servants. In the aftermath, Xenovia is reincarnated as a Devil as Rias' Knight after being declared a heretic by the Church and joins the Occult Research Club before reconciling with Asia. Issei later plays a video game with his client, who reveals himself to be Fallen Angel Leader Azazel. "
+		)
+		("HighSchool Dxd New",
+		 7,
+		 "Summer! Bathing Suits! I'm in Trouble!",
+		 "At the Occult Research Club, Issei informs the group that his client is Azazel much to Rias's dismay. The next day, the Occult Research Club is placed in charge of cleaning the pool, but Issei's Dragon arm acts up again requiring Akeno to suck out the Dragon power. Having cleaned the pool, the Occult Research Club fills it and starts swimming. Rias then attempts to seduce Issei by having him apply sunscreen on her back. However, Akeno interrupts and tries to seduce Issei herself, resulting in a battle between the two girls. Fleeing the battle, Issei meets Xenovia who wants to have a child with him but Rias and the other girls intervene. Back at the Occult Research Club, Sirzechs and Grayfia arrive to greet Rias and her peerage. Sirzechs reveals that he has come for Rias' class observation day and to investigate the location for the meeting of the three factions. Sirzechs and Grayfia then head to the Hyoudou Residence to greet Issei's parents and Sirzechs spends the night in Issei's room. The next day, Rias leaves the Hyoudou Residence early to guide Sirzechs and Grayfia while Asia goes to Xenovia's apartment leaving Issei to go to school alone. On his way he encounters the stranger he previously encountered, who reveals himself as Vali Lucifer, the White Dragon Emperor. "
+		)
+		("HighSchool Dxd New",
+		 8,
+		 "Open House Begins!",
+		 "Vali approaches Issei but Kiba and Xenovia intervene; Vali warns them that since they were unable to defeat Kokabiel, they do not have the strength to beat him. Vali asks Issei where he thinks he ranks, in terms of strength, in the Underworld, and tells Issei that he is someone important. Vali then advises Rias to nurture Issei properly before leaving without a fight. Resting at the sickroom after meeting Vali, Issei questions Ddraig on his relationship with Albion; Ddraig explains how the two Heavenly Dragons came to be sealed into their respective Sacred Gears. At Kuoh Academy, Issei is once again put into a comical situation due to Xenovia's lack of common sense. He runs into Rias, Akeno, Sona and Tsubaki discussing the earlier meeting with Vali, and their concern about their upcoming class observation. After finishing their classes, Issei, Rias and Akeno hear some noise about a cosplayer in the gymnasium; heading there, they meet Serafall Leviathan, Sona's older sister and the current Devil King with the title of Leviathan, whose presence and antics embarrass Sona. At the Hyodo Residence, Sirzechs, Issei's and Rias' fathers enjoy a video of Rias' class observation, but Rias gets embarrassed and leaves the room. Issei invites her to his room and the two share an intimate moment until Asia interrupts them followed by Sirzechs, who drops by to tell Rias that recent events and the expansion of her peerage have changed his mind about her ability to control her second Bishop thus giving her his permission to unseal him. "
+		)
+		("HighSchool Dxd New",
+		 9,
+		 "I Have a Junior!",
+		 "The Occult Research Club unseal the locked door in their club house, revealing a boy wearing a female school uniform whom Rias introduces as Gasper Vladi, who accidentally freezes time when Issei touches him. They learn that Gasper possesses the Sacred Gear, Forbidden Balor View, and was sealed due to his inability to control it. Rias, Akeno and Kiba then leave to help in the preparation for the meeting of the Three Factions, leaving the others to train Gasper. During the training, Azazel suddenly arrives at the school yard and gives Saji, Issei and Gasper advice on how to control Gasper's Sacred Gear before leaving. Issei and the others take Gasper to the gymnasium in an attempt to train him to focus his powers, however Gasper once again goes into seclusion after the training. Issei has a conversation with Gasper, managing to get the boy to open up and talk with him, until Kiba, Koneko, Asia and Xenovia arrive. The next day, Issei goes to the shrine with Akeno and meets another new figure who introduces himself as Michael, the leader of the Angels. "
+		)
+		("HighSchool Dxd New",
+		 10,
+		 "Various Three-way Deadlocks!",
+		 "After meeting Michael, Issei receives the Holy Sword, Ascalon, from the Archangel and proceeds assimilate it into his Boosted Gear and Michael leaves shortly afterwards. After Michael leaves, Issei and Akeno discuss Akeno's fallen angel and human heritage. Issei is unfazed by her fallen angel heritage and so deepen his bonds with Akeno, who gives him a lap pillow until a jealous Rias appears and forces Issei to leave with her. Issei then heads back to the school to help Gasper in his training who improves in controlling his powers. On the day of the conference, Rias and her servants go to the meeting room, leaving Koneko and Gasper behind. At the meeting, they meet Azazel and Vali who are representing the Fallen Angels, Michael who represents the Angels and Sirzechs and Serafall who are representing the Devils as the Three Factions starts the meeting. Meanwhile, a group of mysterious people approaches Gasper and Koneko who are standing by at the Occult Research Clubroom. "
+		)
+		("HighSchool Dxd New",
+		 11,
+		 "The Leaders' Summit Begins!",
+		 "During the meeting between the leaders of the Three Factions, Azazel asks both Issei and Vali their opinions about the peace treaty. Issei then asks Michael why Asia was exiled and is told of the current situation in Heaven and both Asia and Xenovia reveal that they are happy with their current lifestyle. Azazel then comments about his subordinate who killed Asia to Issei's irritation. However, time freezes and the members of the Occult Research Club realize that Gasper has been kidnapped. Several Magicians of the Khaos Brigade's Old Satan Faction appear and they ambush the meeting. Issei volunteers to accompany Rias as she goes to rescue Gasper. Vali attacks the Magicians outside and easily kills them. Rias and Issei are transported to the old school building through castling using Rias' unused Rook piece. Before leaving, Azazel gives Issei two bracelets to control his and Gasper's Sacred Gears. Cattleya Leviathan, a General of the Old Satan Faction, arrives and fights the leaders. Azazel battles with Katerea after insulting her. Kiba, Xenovia and Irina also volunteer to help in battling the Magicians while Grayfia is analyzing the magic circle being used by the Magicians. Issei and Rias arrive at the room where Gasper and Koneko are being held captive. Issei lets Gasper drink his blood, allowing Gasper to properly control his powers. Issei then gives him his bracelet and the four of them head out. "
+		)
+		("HighSchool Dxd New",
+		 12,
+		 "Clash of the Twin Heavenly Dragons!",
+		 "The battle between the Magicians and the allied Three Factions continues as Gasper destroys the transportation circle used by the Magicians. Azazel kills Cattleya by impaling her in the head with his Artificial Sacred Gear, the Downfall Dragon Spear, after he severs his left arm. Vali reveals his treachery and laments that Issei is his rival. When he proposes killing Issei's parents to motivate him to become stronger, Issei furiously enters his Balance Breaker and battles Vali. Issei then takes a huge risk by absorbing in the power of the Divine Dividing into his Boosted Gear. Prompted by Azazel's comment Issei's power increases drastically and inflicts damage to Vali. As Vali prepares for a counterattack, Bikou intervenes and flees with Vali. In the aftermath, Issei asks Michael to allow Asia and Xenovia to pray to God without pain. Sometime later, in response to Sirzechs's request to help Issei, Kiba and Gasper master their Sacred Gear, Azazel is fitted with an artificial arm and becomes the new advisor of the Occult Research Club. He also tells them that Sirzechs has ordered the girls in Rias's peerage to move into the Hyodo Residence. As the girls move in, Rias asks Sirzechs to renovate the house. "
+		)
+;
+
+INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
+		("HighSchool Dxd New", 0, 4, 4)
+;
