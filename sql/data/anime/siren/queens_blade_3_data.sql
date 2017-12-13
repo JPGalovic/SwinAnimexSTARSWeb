73db61845c4e2e582312c279cb3d3d0f1e1a838c
James P. Galovic <jpgalovic@gmail.com>
2017-12-13 17:11:56 +1100

    Fix: Nameing of Siren

--- sql/data/anime/siren/queens_blade_3_data.sql
+++ sql/data/anime/siren/queens_blade_3_data.sql
@@ -3,7 +3,7 @@ INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIP
 		 12,
 		 "Power corrupts, and it when it appears that the once noble Queen Claudette's ways have turned to oppression and heretical persecution, it's up to a new generation of warriors to step up to the plate armor to bear arms and bare their naked fury in open rebellion! The odds may seem unfairly stacked in favor of the Amazonian ranks of the queen, doubly supported by her power of writ and assassins. But the incredible wits and assets of the dazzling array of daring damsels willing to risk their gorgeous skins and put their lithesome bodies on line against her, might just expose a few unexpected weaknesses in the queen's support! It's the wildest fantasy ever as an unprecedented force of fabulous female fighters. from elven alchemist to half-demon berserker, from an undead pirate captain to a dragon maid, they all come together with the goal of stripping the Queen from her throne in QUEEN'S BLADE: REBELLION! &quot;Quality fan-service!&quot; - Anime News Network",
 		 "FIVE OUT OF FIVE REBELS AGREE: It's time for this Queen to go DOWN!",
-		 "Siren Visual",
+		 "Siren",
 		 "Kinji Yoshimoto"
 		)
 ;
