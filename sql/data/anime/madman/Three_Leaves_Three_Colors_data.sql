60fcce2c6ff7ebbdda7ecb6f041aa882ac375938
C1and00j <34433447+C1and00j@users.noreply.github.com>
2017-12-20 13:19:40 +1100

    Add: three leaves, three colors data, REMOVED: FALSE file

---
+++ sql/data/anime/madman/Three_Leaves_Three_Colors_data.sql
@@ -0,0 +1,89 @@
+-- Anime Data
+INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
+		("Three Leaves, Three Colors",
+		 12,
+		 "Yoko Nishikawa lived a life of privilege until her father’s company went under. From princess to pauper, she faces life with a non-existent budget and is stuck eating bread crusts. Alone and distraught, everything changes when glutton Futaba and scary class rep Teru stumble into her life—literally. Together, they’ll discover a new friendship and survive the day-to-day trials of being in high school.",
+		 "Yoko Nishikawa lived a life of privilege until her father’s company went under. From princess to pauper, she faces life with a non-existent budget and is stuck eating bread crusts.",
+		 "Madman"
+		 "&copy;Â&copy 2016 arai-cherry / Houbunsha, Three leaves, three colors Project"
+		)
+;
+
+INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
+		("Three Leaves, Three Colors",
+		 3,
+		 1,
+		 "PG - Mild themes",
+		 12,
+		 "https://www.madman.com.au/catalogue/view/36925"
+		)
+;
+
+INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SYNOPSYS) VALUES
+		("Three Leaves, Three Colors",
+		 1,
+		 "These Are Bread Crusts",
+		 "While on lunch break, Odagiri Futaba, who decides to take a shortcut to her classroom, and Hayama, who is chasing a cat, stumble upon Nishikawa Yoko eating alone. When Futaba asks Yoko what's on her hand, she immediately says that they are bread crusts. Then, the three of them end up having lunch together and introduce themselves. Futaba asks Yoko whether she has no friends, where she replies that Futaba is too direct in asking it. That night, Yoko tries to deep fry the break crusts."
+		)
+	,	("Three Leaves, Three Colors",
+		 2,
+		 "Nothing Good for You is Tasty",
+		 "While lunch, Hayama offers Yoko her rolled egg, which the latter refuse. Futaba asks both of them being so polite to each other, and they should call each other by using nicknames. Suddenly, a cat appears out of nowhere, which makes Hayama shows her soft side for animals. She tells them that she has a pet cat named 'Beelzebub' at home, which she nicknamed 'Bel'."
+		)
+	,	("Three Leaves, Three Colors",
+		 3,
+		 "A Taste of Maid",
+		 "When Yoko goes to school, it is revealed that Yamaji always inspects whether she locked properly after she goes out. A week later, Futaba reveals that she is running out of money due to her accidentally use her allowance to eat the expensive grilled sea urchin. When Futaba decided she probably should work part-time, Yoko plans to do the same as well, while asking what should be a good place for her to work."
+		)
+	,	("Three Leaves, Three Colors",
+		 4,
+		 "Actually, This Is What I Live For",
+		 "When Yoko-sama invited her friends to her home, they all play pretend to be a married woman, wife of a chairman and wife of a real estate magnate, as Yamaji tells Futaba and Teru a tale from Yoko's childhood. In Futaba's case, it is revealed that she has been a walking black hole since she was in primary school, eating a loaf of bread (with jam) for snacks. When they ask about Teru's past, she tells them that Santa didn't give her none of what she wanted back then (either a white tiger, a puma, a snow leopard or a sabre-toothed cat). Suspecting it is because she has been a bad girl, she vows to be a good girl and study hard, even though it is all just for show. Both Futaba and Sonobe are initially jealous of Teru who has a good older sister, but retract their statement after seeing her act in the present."
+		)
+	,	("Three Leaves, Three Colors",
+		 5,
+		 "Your Birthmarks Have Faded",
+		 "During a very hot day in summer, Futaba comes to Teru's house, shouting &quot;Teru-chan, let's play!&quot; until she comes out angry and points out that she should just ring the chime. After annoyed by hearing that Futaba just come to play Old Maid with her, Teru suggests that they go to somewhere cooler. In a restaurant named Jonafull, as the three friends sit down, Yoko-sama dozes off, and hit her head quite hard on the table. They learn that Yoko is exhausted from working."
+		)
+	,	("Three Leaves, Three Colors",
+		 6,
+		 "Veggies, Meat, Meat, Meat, Meat, Meat, Meat, and Fish",
+		 "During August, Yoko, Futaba and Teru arrive at the beach... not to play, but to work with Sonobe by managing her beach hut, Himitsu no Hanazono (秘密の花園, literally Secret Garden), Beach Hut Version. (Not) surprisingly, Yoko-sama don't know what a beach hut is, and Sonobe's attempt on explaining it to her end in failure. An overprotective Yamaji brings along a metal bat, with the intention of swinging it to anyone who stare at Yoko-sama."
+		)
+	,	("Three Leaves, Three Colors",
+		 7,
+		 "Ten Yen Short",
+		 "Autumn has arrived, as Yoko, Futaba and Teru return home from school. Futaba only thinks about food, and says that it is an autumn of eating, while adding that she can eat five times as much in autumn. They meet with a blue-haired boy, who shouts &quot;Odagiri Futaba!&quot;, and runs before anyone can say anything else."
+		)
+	,	("Three Leaves, Three Colors",
+		 8,
+		 "You've Got Them Hooked",
+		 "In a Cat Cafe named &quot;Nekonimashi&quot; (猫にまし), Serina is playing with the cats there happily. Suddenly, a new customer enters, and Teru sits besides Serina. Both of them are surprised to see each other there. The story then flashbacks to that morning. Kou asks Teru to go out with her, and when she points to Nekonimashi, Teru reveals that she has a coupon to there already, which is the reason of Serina's encounter with Teru."
+		)
+	,	("Three Leaves, Three Colors",
+		 9,
+		 "Curry Tastes Better on the Second Day",
+		 "It is the time for Cultural Festival. Class 1-3 students are thinking of an event to do, and Futaba suggests curry shop. When they ask about costumes, Sonobe Shino appears out of nowhere and suggests that they just use school uniforms, and make the theme to be &quot;ordinary curry made by high school girls&quot;, before disappearing without anyone else but Asako, Serina, Futaba and Teru notices her real identity."
+		)
+	,	("Three Leaves, Three Colors",
+		 10,
+		 "A Day to Gorge on Chicken and Cake",
+		 "It's almost Christmas season. Yoko claims that it is the season for her heating bill to be high, since it is cold. Yamaji, who happens to be passing flyers, hearing that Yoko is going to cut cost by being in a blanket, says that he will buy a heater for Yoko-sama. The three leaves ignore him and walk away."
+		)
+	,	("Three Leaves, Three Colors",
+		 11,
+		 "A Day to Gorge on Chocolate",
+		 "It's almost the season for Valentine Day. Yoko claims that it is the season that she want winter to end so that she can save on heating bills. Teru points out that they seems to have this conversation before. Futaba claims that Valentine Day is the day to gorge on chocolate, while Yoko disagrees, and Teru once again feel a déjà vu."
+		)
+	,	("Three Leaves, Three Colors",
+		 12,
+		 "I Am Graduating from Bread Crusts",
+		 "Yoko goes to school, as Odagiri Futaba greets her with &quot;Yoko-san&quot; (and wears a green katyusha), which Yoko feels very weird. Teru lazily greets them as well (and she doesn't wear any spectacles), and Futaba calls her &quot;Teru-san&quot; (too polite of a namecall), as Yoko feels weird and weirder. Yoko thought that it was all an act, as Futaba points out that Yoko is acting weird."
+		)
+;
+
+INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
+		("Three Leaves, Three Colors", 0, 1, 4)
+	,	("Three Leaves, Three Colors", 0, 2, 4)
+	,	("Three Leaves, Three Colors", 0, 3, 4)
+;
