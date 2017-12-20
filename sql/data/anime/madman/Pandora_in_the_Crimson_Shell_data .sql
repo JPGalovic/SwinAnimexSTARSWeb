bb61f0597c642f02fe2e73d49e4d7e14b6261876
C1and00j <34433447+C1and00j@users.noreply.github.com>
2017-12-20 12:58:11 +1100

    ADD:pandora in the crimson shell data

---
+++ sql/data/anime/madman/Pandora_in_the_Crimson_Shell_data .sql
@@ -0,0 +1,89 @@
+-- Anime Data
+INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
+		("Pandora in the Crimson Shell",
+		 12,
+		 "From Shirow Masamune, the creator of Ghost in the Shell, comes a lighthearted story of two cyborg girls who fight to save their home. Nene Nanakorobi is your typical girl next door except for that fact that she’s a rare type of full-body cyborg. She’s thrilled to finally meet another cyborg named Clarion and hopes they’ll be best friends. This is the beginning of their adventures together.",
+		 "From Shirow Masamune, the creator of Ghost in the Shell, comes a lighthearted story of two cyborg girls who fight to save their home. Nene Nanakorobi is your typical girl next door except for that fact that she’s a rare type of full-body cyborg.",
+		 "Madman"
+		 "&copy;Shirow Masamune, Koushi Rikudou / KADOKAWA, Benipan"
+		)
+;
+
+INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
+		("Pandora in the Crimson Shell",
+		 3,
+		 1,
+		 "MA15+ - Strong sexual innuendo",
+		 13,
+		 "https://www.madman.com.au/catalogue/view/35729"
+		)
+;
+
+INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SYNOPSYS) VALUES
+		("Pandora in the Crimson Shell",
+		 1,
+		 "Adepter",
+		 "In an age of advanced technology, a cyborg named Nene Nanakorobi meets an inventor named Uzal Delilah and her cyborg pet, Clarion, on their way to Cenancle Island. Shortly after the girls arrive on the island, the city comes under attack by an army of robots controlled by a group of rebels who despise Uzal. In order to battle against them, Uzal gives Nene authority to use the Pandora Device inside of Clarion, updating her operating system to temporarily give her the skill to use a gun. After the robots are stopped, Uzal brings Nene to her laboratory where she gives Nene an optical camouflage for when she uses the Pandora Device, before the rebels ask for Uzal's help in stopping a robot that had gone berserk. "
+		)
+	,	("Pandora in the Crimson Shell",
+		 2,
+		 "Geofront",
+		 "Nene and Clarion are tasked with stopping BUER, a giant boring robot belonging to Uzal's company that went berserk after her underlings tried to use it against her. After the underlings trick BUER into firing its laser cannon, Clarion aims to reach its main console while Nene acts as her bodyguard. However, a large piece of rubble interrupts Clarion before she complete her mission, so she instead operates some smaller robots to allow Nene to reach the console and shut BUER down. With forces approaching on the laboratory, Uzal detaches BUER's Central Nervous Unit before having Nene and the others escape from the lab as it self-destructs, leaving Clarion in Nene's care. Upon reaching the surface, Nene and the others find themselves surrounded by Cenancle's Defense Force."
+		)
+	,	("Pandora in the Crimson Shell",
+		 3,
+		 "Terrarium",
+		 "Thanks to Uzal's efforts, Nene and Clarion are treated like rescued hostages and are escorted to the residence of Nene's aunt, Takumi Korobase, who is acquaintances with Uzal. Later, as Takumi takes a dangerous interest in Clarion's ears, Nene manages to see through a simulation Takumi had placed them in when they arrived and stop her. The next day, Nene and Clarion are approached by one of Uzal's underlings, Bunny, who asks for BUER's authentication key in order to help her comrades. As Bunny delivers the key to a mysterious man named Ian Kurtz, who plans to use it for his own gain, Clarion digs up BUER's central nervous unit. "
+		)
+	,	("Pandora in the Crimson Shell",
+		 4,
+		 "Kitchen Drudge",
+		 "After Nene manages to enrol in a prestigious online school, Takumi, wanting to have some time alone to examine BUER, sends Nene and Clarion to observe some of her companies as part of her homework. With the companies being too aware of Takumi's status to provide any useful information, Nene goes to a charity event where a TV reporter named Prosperina allows her to observe her work. When some thugs suddenly show up and start wrecking the place, Nene and Clarion step up against them, encouraging the other volunteers to stand up against them too. With most of the food ruined by the attack, Nene uses the Pandora Device to learn cooking skills and cook up some replacement meals."
+		)
+	,	("Pandora in the Crimson Shell",
+		 5,
+		 "System Down",
+		 "While pondering if it is right to keep borrowing Clarion's power to achieve world peace, Nene meets an old woman named Anna who is adjusting to a robotic arm. Just then, a network disturbance occurs across the entire island, causing almost all of its systems and transportation to shut down. Nene and Clarion soon come across a man named Robert, who helps out everyone they comes across and manages to stop a pair of thieves, before coming across Anna, whose life is in danger because of the disturbance affecting her artificial organs. Using the Pandora Device, Nene becomes a paramedic to treat Anna's symptoms and take her to the hospital. After the network is restored, Nene is thanked by Anna for saving her, while Robert, who is part of the island's defense force, is given a task by Kurtz. "
+		)
+	,	("Pandora in the Crimson Shell",
+		 6,
+		 "Central Nervous Unit",
+		 "Nene goes in for a medical checkup at hospital, where a girl named Amy Gilliam is also visiting to have a new leg installed, only to find her cat Lean has sneaked in as well. While searching for Lean, Amy comes across BUER, who joins her in her search. The two are soon found by Nene and Clarion while Anna manages to find Leon and reunite her with Amy. When Amy rips her dress before her appointment, Nene uses the Pandora Device to become a couturier and sew it back together, allowing her to go to her appointment with a smile. When they return to the park, they find it has been completely torn down, with its residents evacuated by the CDF. As they pass by Kurtz, Clarion is cautious of a cyborg following behind him. "
+		)
+	,	("Pandora in the Crimson Shell",
+		 7,
+		 "Puppeteer",
+		 "A family of robot thieves known as the Chicken Brothers catch Clarion off guard and capture Nene. As Clarion tracks the thieves down to their hideout, Nene comes across all the other androids the thieves had taken and, upon reuniting with Clarion, uses the Pandora Device to take control of them and stop the thieves. "
+		)
+	,	("Pandora in the Crimson Shell",
+		 8,
+		 "Inferno",
+		 "Nene and Clarion go to the Herme's department store, where government chairman Janus North is serving as manager-for-a-day. Following another network disturbance, North and his secretary become stuck in an elevator with Nene and Clarion. The group manage to find their way out of the elevator, only to discover a fire has broken out across the entire department store. Wanting to help North and the secretary escape, Nene uses her Pandora Device skills to create a fireproof cart and lead them to the emergency stairs. Spotting a boy still trapped in the fire, Nene goes back to rescue him despite Clarion's warnings, using what little oxygen she has to keep him alive. Putting Nene's wishes over her own common sense, Clarion manages to catch up to her and get both Nene and the boy out safely. "
+		)
+	,	("Pandora in the Crimson Shell",
+		 9,
+		 "Assault",
+		 "The fire at the department store is revealed to have been caused by a laser fired from BUER's body after Kurtz tries to hack into it. Deducing this herself and deciding not to get Nene involved, Clarion approaches Takumi about BUER going out of control again and borrows some of her robots for an assault on Uzal's base in order to stop it. Making her way past Bunny and her robot defenses, Clarion soon comes up against Kurtz' cyborg bodyguard, Fear. "
+		)
+	,	("Pandora in the Crimson Shell",
+		 10,
+		 "Fear",
+		 "Nene wakes up to find Clarion has gone, leaving behind a note saying not to involved, and begins feeling lonely without her around. Meanwhile, Robert receives a confidential message from Uzal warning him about Kurtz' plans to use BUER. As Nene struggles to try and do things by herself, she receives some helpful advice from Robert before learning from BUER about what Clarion is actually involved in. Meanwhile, Clarion brings out her strongest weapons against Fear, who is revealed to be an android, but is caught off guard when it targets Bunny. Wanting to help Clarion, Nene sneaks out with BUER's help to go and find her. "
+		)
+	,	("Pandora in the Crimson Shell",
+		 11,
+		 "Ghost Urn",
+		 "Nene infiltrates the base, where she gets mistaken for a dummy robot and temporarily shut down by Kurtz. While BUER follows Kurtz, Nene finds herself in the disposal area with Bunny, who tasks her with diving into the base's system and finding a way out before they are disposed of. Remembering when she started out moving her cyborg, Nene manages to move about freely in cyberspace and unlock the emergency hatch. While Bunny heads off to save her allies, BUER attempts to stop Kurtz from hacking his main body only to be thwarted by Takumi's own hacking attempts. As Nene reaches the central room and is confronted by Fear, she is aided by the arrival of Clarion, who managed to survive Fear's attack. Wanting to help Clarion protect the island, Nene uses the Pandora Device and joins Clarion is facing up against Fear. "
+		)
+	,	("Pandora in the Crimson Shell",
+		 12,
+		 "Elpis",
+		 "Nene uses her ability to support Clarion as she fights against Fear, allowing her to destroy it. Meanwhile, BUER's defenses are taken down by Takumi, allowing Kurtz to activate it. Instead of gaining control, however, Kurtz is shocked to find that all he has done is leaked BUER's collection of panty shots across the world. As BUER goes on a rampage out of pure embarrassment, Uzal gets in contact with Nene, stating that BUER must be stopped by using Clarion to activate his self-destruct system. Not wanting to lose Clarion, Nene chooses a riskier alternative, going inside of BUER and managing to calm him down. After Uzal sinks BUER's body and seals off the geofront, Nene and Clarion resume their everyday life of seeking world peace together. "
+		)
+;
+
+INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
+		("Pandora in the Crimson Shell", 0, 1, 4)
+	,	("Pandora in the Crimson Shell", 0, 2, 4)
+	,	("Pandora in the Crimson Shell", 0, 3, 4)
+;
