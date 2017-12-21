6d70c157cc26bb89b7416942aa60d3988615ea97
James P. Galovic <jpgalovic@gmail.com>
2017-12-21 19:47:27 +1100

    Change: sql file builder to use seperate arrays for each component, makes additions of titles and events simpler

--- sql/build_sql.php
+++ sql/build_sql.php
@@ -17,46 +17,87 @@
 			
 
 	// Data Collation
-	$data_array = array(
-			0 =>  'data/drop_data.sql'
-		,	1 =>  'data/company_data.sql'
-		,	2 =>  'data/classification_data.sql'
-		,	3 =>  'data/anime_data_core.sql'
-		,	4 =>  'data/game_data_core.sql'
-		,	5 =>  'data/event_data_core.sql'
+	$core_data_array = array(
+			0  =>  'data/drop_data.sql'
+		,	1  =>  'data/company_data.sql'
+		,	2  =>  'data/classification_data.sql'
+		,	3  =>  'data/anime_data_core.sql'
+		,	4  =>  'data/game_data_core.sql'
+		,	5  =>  'data/event_data_core.sql'
+	);
 		
-		,	6 =>  'data/anime/hanabee/little_busters_refrain_data.sql'
-		,	7 =>  'data/anime/hanabee/plastic_memories_data.sql'
-		
-		,	8 =>  'data/anime/madman/assassination_classroom_season_2_data.sql'
-		,	9 =>  'data/anime/madman/ben-to_data.sql'
-		,	10 => 'data/anime/madman/danganronpa_data.sql'
-		,	11 => 'data/anime/madman/death_note_data.sql'
-		,	12 => 'data/anime/madman/food_wars_data.sql'
-		,	13 => 'data/anime/madman/highschool_dxd_data.sql'
-		,	14 => 'data/anime/madman/highschool_dxd_season_2_data.sql'
-		,	15 => 'data/anime/madman/one_punch_man_data.sql'
-		,	16 => 'data/anime/madman/pandora_in_the_crimson_shell_data.sql'
-		,	17 => 'data/anime/madman/samurai_jack_data.sql'
-		,	18 => 'data/anime/madman/school_live_data.sql'
-		,	19 => 'data/anime/madman/three_leaves_three_colors_data.sql'
-		,	20 => 'data/anime/madman/vividred_operation_data.sql'
-		,	21 => 'data/anime/madman/yurikuma_arashi_data.sql'
-		
-		,	22 => 'data/anime/siren/canaan_data.sql'
-		,	23 => 'data/anime/siren/mawaru_penguindrum_data.sql'
-		,	24 => 'data/anime/siren/queens_blade_2_data.sql'
-		,	25 => 'data/anime/siren/queens_blade_3_data.sql'
-		,	26 => 'data/anime/siren/welcome_to_irabus_office_data.sql'
-		
-		,	27 => 'data/events/summer_2018.sql'
+	$madman_anime_data_array = array(
+			0  => 'data/anime/madman/assassination_classroom_season_2_data.sql'
+		,	1  => 'data/anime/madman/ben-to_data.sql'
+		,	2  => 'data/anime/madman/danganronpa_data.sql'
+		,	3  => 'data/anime/madman/death_note_data.sql'
+		,	4  => 'data/anime/madman/food_wars_data.sql'
+		,	5  => 'data/anime/madman/highschool_dxd_data.sql'
+		,	6  => 'data/anime/madman/highschool_dxd_season_2_data.sql'
+		,	7  => 'data/anime/madman/one_punch_man_data.sql'
+		,	8  => 'data/anime/madman/pandora_in_the_crimson_shell_data.sql'
+		,	9  => 'data/anime/madman/samurai_jack_data.sql'
+		,	10 => 'data/anime/madman/school_live_data.sql'
+		,	11 => 'data/anime/madman/three_leaves_three_colors_data.sql'
+		,	12 => 'data/anime/madman/vividred_operation_data.sql'
+		,	13 => 'data/anime/madman/yurikuma_arashi_data.sql'
+	);
+
+	$hanabee_anime_data_array = array(
+			0  => 'data/anime/hanabee/little_busters_refrain_data.sql'
+		,	1  => 'data/anime/hanabee/plastic_memories_data.sql'
+	);
+
+	$siren_anime_data_array = array(
+			0  => 'data/anime/siren/canaan_data.sql'
+		,	1  => 'data/anime/siren/mawaru_penguindrum_data.sql'
+		,	2  => 'data/anime/siren/queens_blade_2_data.sql'
+		,	3  => 'data/anime/siren/queens_blade_3_data.sql'
+		,	4  => 'data/anime/siren/welcome_to_irabus_office_data.sql'
+	
+	);
+
+	$event_data_array = array(
+			0  => 'data/events/summer_2018.sql'
 	);
 
 	//,	n => ''
 
 	// Write Data to File
 
-	foreach ($data_array as $data_file)
+	foreach ($core_data_array as $data_file)
+	{
+		$read_handle = fopen($data_file, 'r') or die("Cannot open file: ".$data_file);
+		$data = fread($read_handle, filesize($data_file));
+		fwrite($write_handle, $data);
+		fclose($read_handle);
+	}
+
+	foreach ($madman_anime_data_array as $data_file)
+	{
+		$read_handle = fopen($data_file, 'r') or die("Cannot open file: ".$data_file);
+		$data = fread($read_handle, filesize($data_file));
+		fwrite($write_handle, $data);
+		fclose($read_handle);
+	}
+
+	foreach ($hanabee_anime_data_array as $data_file)
+	{
+		$read_handle = fopen($data_file, 'r') or die("Cannot open file: ".$data_file);
+		$data = fread($read_handle, filesize($data_file));
+		fwrite($write_handle, $data);
+		fclose($read_handle);
+	}
+
+	foreach ($siren_anime_data_array as $data_file)
+	{
+		$read_handle = fopen($data_file, 'r') or die("Cannot open file: ".$data_file);
+		$data = fread($read_handle, filesize($data_file));
+		fwrite($write_handle, $data);
+		fclose($read_handle);
+	}
+
+	foreach ($event_data_array as $data_file)
 	{
 		$read_handle = fopen($data_file, 'r') or die("Cannot open file: ".$data_file);
 		$data = fread($read_handle, filesize($data_file));
@@ -65,9 +106,6 @@
 	}
 	
 	fclose($write_handle);
-
-	$read_handle = fopen($full_sql, 'r') or die("Cannot open file: ".$full_sql);
-	$data = fread($read_handle, filesize($full_sql));
 	
 	echo('<p>SQL File Written.</p>');
 	echo('<a href="data/swin_anime_database.sql">Get SQL File</a>')
