d5a6e8c7672316788d258e90cc9ec90ae0af9729
James P. Galovic <jpgalovic@gmail.com>
2017-12-25 01:46:50 +1100

    Add: Missing Classification

--- sql/data/classification_data.sql
+++ sql/data/classification_data.sql
@@ -80,5 +80,6 @@ INSERT INTO CLASSIFICATION (CLASSIFICATION) VALUES
 	,	("MA15+ - Strong sexual references, animated nudity and violence")
 	,	("MA15+ - Strong sexual innuendo")
 	,	("MA15+ - Strong supernatural themes and animated violence")
+	,	("MA15+ - Strong animated violence and sexual references")
 ;
 
