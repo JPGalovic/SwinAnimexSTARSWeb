9b7fb548121851209f14175623f40a493f8a93c7
icy <18729501+Icy-of-Melbourne@users.noreply.github.com>
2017-12-30 00:07:32 +1100

    Add: Tokyo Magnitude 8.0

--- sql/data/anime/siren/tokyo_magnitude_8_data.sql
+++ sql/data/anime/siren/tokyo_magnitude_8_data.sql
@@ -1,51 +1,82 @@
--- Anime Data
 INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
-		("Title",
-		 n,
-		 "Synopsys",
-		 "Description",
-		 "Company",
-		 "Copyright"
+		("Tokyo Magnitude 8.0",
+		 11,
+		 "Imagine if an earthquake as powerful as magnitude 8.0 struck Tokyo, the capital city of Japan, today... Who would we think about and what should we do at that moment? This is a tremendous human drama that portrays the connections and human bonds in the most extreme situations.",
+		 "Imagine if an earthquake as powerful as magnitude 8.0 struck Tokyo, the capital city of Japan, today... Who would we think about and what should we do at that moment? This is a tremendous human drama that portrays the connections and human bonds in the most extreme situations.",
+		 "Siren",
+		 "Masaki Tachibana"
 		)
 ;
 
 INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
-		("Title",
-		 n,
-		 m,
-		 "Classification",
-		 x,
-		 "URL"
+		("Tokyo Magnitude 8.0",
+		 1,
+		 1,
+		 "PG - Mild Themes and Infrequent Coarse Language",
+		 11,
+		 "http://sirenvisual.com.au/index.php/shop/tokyo-magnitude-8-0-dvd.html"
 		)
 ;
 
 INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SYNOPSYS) VALUES
-		("Title",
-		 n,
-		 "Episode_Title",
-		 "Episode_Synopsys"
+		("Tokyo Magnitude 8.0",
+		 1,
+		 "Odaiba Stinks",
+		 "Mirai Onozawa and her younger brother Yūki are visiting a robot exposition at Odaiba, Tokyo, when a massive earthquake strikes the city."
 		)
-;
+	,	("Tokyo Magnitude 8.0",
+		 2,
+		 "The World is Broken",
+		 "After the earthquake Mirai looks desperately for Yūki inside a collapsing building along with Mari Kusakabe, a female motorcycle courier who offered to help her."
+		 )
+	,	("Tokyo Magnitude 8.0",
+		 3,
+		 "Burning Bridge",
+		 "Mirai, Yūki and Mari decide to reach their homes together, but just after starting their journey, they find that there are far more dangers in the collapsing city than they imagined."
+		 )
+	,	("Tokyo Magnitude 8.0",
+		 4,
+		 "The Promise Between the Three",
+		 "Having finally left Odaiba, the trio finds a place to gather supplies and prepare themselves for their long walk, but after having a discussion, Mirai and Yūki stray away from Mari and end up in serious trouble."
+		 )
+	,	("Tokyo Magnitude 8.0",
+		 5,
+		 "The School is Wailing",
+		 "The group takes shelter at her school's premises during the night, but their stay there becomes far from peaceful as they are constantly plagued by aftershocks and depressed by the sight of those who lost their relatives to the earthquake."
+		 )
+	,	("Tokyo Magnitude 8.0",
+		 6,
+		 "Choice to Abandon",
+		 "The group reaches Mari's workplace, where she loses consciousness because of anemia. During that time the kids are suddenly frightened by Aya, a colleague of Mari, who noisily enters the building carrying a baseball bat."
+		 )
+	,	("Tokyo Magnitude 8.0",
+		 7,
+		 "Summer's Dusk",
+		 "Rescue robots fill the scene, fascinating Yūki and leading him to meet a new friend, who shares his interest and is the same age as Mirai. However the journey so far seems to have taken its toll on Yūki." 
+		 )
+	,	("Tokyo Magnitude 8.0",
+		 8,
+		 "Pure White Morning",
+		 "Yūki collapses suddenly and is taken to the hospital."
+		)
+	,	("Tokyo Magnitude 8.0",
+		 9,
+		 "Farewell Today",
+		 "Mari finally reaches her home to find it was destroyed. Mirai and Yūki learn that residents are being cared for at a nearby elementary school. There, they find two unknown bodies that Mari believes are her mother and daughter, and mourns their deaths. However, Yūki believes they are still alive and with his sister, helps finds Mari's daughter at another school. After reuniting with her daughter, Mari finds her mother at a local hospital. Mirai and Yūki leave to continue their search for their parents. Mirai leaves Mari a note to thank her for all she did for them, stating that she hopes they will meet again. Confused and surprised, Mari says &quot;Yūki and I?&quot;"
+		)
+	,	("Tokyo Magnitude 8.0",
+		 10,
+		 "Mirai, You Know...",
+		 "While on a truck going towards their hometown, Yūki tries to talk to Mirai about his death, but she tells him to quit talking nonsense as it would make her furious, very worried and very upset within shame of sadness if he died. In their home town, Mirai runs into a friend some school who tells her that her parents are staying at one of the school shelters, and that her father was hospitalized from an injury. Yūki runs off when Mirai runs into his best friend Itsuki. Itsuki takes her to her parents, but finds that her mom has gone back to the house to get clothes for her father. Outside, they see the marronier Yūki and Itsuki had planted before the earthquake. Itsuki and Mirai return to his house to get a journal he wanted to give Yūki, while Yūki continues to avoid being seen by Itsuki. Half the house collapses while they are there. At the end of the episode, Yūki finally is able to tell Mirai that he died at the hospital earlier."
+		 )
+	,	("Tokyo Magnitude 8.0",
+		 11,
+		 "Dear Yūki...",
+		 "Mirai and Yūki return home where Mirai is reunited with her parents and Yūki's spirit leaves. One month later, Tokyo is continuing to recover from the earthquake. Mari visits Mirai who returns her cell phone and Yūki's backpack. Mirai finds an email from Yūki that he sent before his death from Mari's phone saying that he hoped he could get a phone too so they could talk even when they were apart. Mirai gives her mother the present Yūki had purchased for her for the birthday. While watering the tree Yūki planted, Mirai resolves to move on with her life, knowing that Yūki will always be in her heart."
+		 );
 
 INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
-		("Title", x, y, z)
+		("Tokyo Magnitude 8.0", 0, 1, 4)
+	,	("Tokyo Magnitude 8.0", 0, 2, 4)
+	,	("Tokyo Magnitude 8.0", 0, 3, 3)
 ;
-		
--- General Anime Volume Data
-	,	("Title",
-		 n,
-		 m,
-		 "Classification",
-		 x,
-		 "URL"
-		)
-		
--- General Anime Episode Data
-	,	("Title",
-		 n,
-		 "Episode_Title",
-		 "Episode_Synopsys"
-		)
-		
--- General Anime Session Data
-	,	("Title", x, y, z)
