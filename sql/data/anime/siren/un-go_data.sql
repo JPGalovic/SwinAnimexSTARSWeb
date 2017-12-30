3dc8da838292fdd60cad6c853c0a65875158ae71
icy <18729501+Icy-of-Melbourne@users.noreply.github.com>
2017-12-30 22:20:37 +1100

    Add: Ungo
    Please Note: Siren lists it as 'To Be Classified', though I noticed over in classification_data.sql there was an entry for 'check the classification' so I gave it that instead.

--- sql/data/anime/siren/un-go_data.sql
+++ sql/data/anime/siren/un-go_data.sql
@@ -1,51 +1,82 @@
--- Anime Data
 INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
-		("Title",
-		 n,
-		 "Synopsys",
-		 "Description",
-		 "Company",
-		 "Copyright"
+		("Un-Go",
+		 11,
+		 "He pursues his job as the &quot;Last Great Detective&quot;. Others call him the &quot;Defeated Detective&quot;. What's certain is that he's Shinjurou Yuuki, a young man with a passion for mysteries and a talent for solving them which has made him the target of dark forces now stirrin within the sinister underworld of a near future Tokyo. That could prove lethal, given that not even the other &quot;good guys&quot;, including the police, are exactly on Shinjouro's side. Fortunately Yuuki's not completely on his own, and with the aid of his uniquely talented associate Inga, he's ready to cut a swath through the veils of secrecy that have been laid before him. Get ready for mind against matter and a lot of cloak and dagger as the ultimate battle of clue and deduction begins in UN-GO!",
+		 "He pursues his job as the &quot;Last Great Detective&quot;. Others call him the &quot;Defeated Detective&quot;. What's certain is that he's Shinjurou Yuuki, a young man with a passion for mysteries and a talent for solving them which has made him the target of dark forces now stirrin within the sinister underworld of a near future Tokyo.",
+		 "Siren",
+		 "Seiji Mizushima"
 		)
 ;
 
 INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
-		("Title",
-		 n,
-		 m,
-		 "Classification",
-		 x,
-		 "URL"
+		("Un-Go",
+		 1,
+		 1,
+		 "Check the Classification",
+		 11,
+		 "http://sirenvisual.com.au/index.php/shop/un-go.html"
 		)
 ;
 
 INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SYNOPSYS) VALUES
-		("Title",
-		 n,
-		 "Episode_Title",
-		 "Episode_Synopsys"
+		("Un-Go",
+		 1,
+		 "Murder at the Ball",
+		 "Kaoru Kanou, a corporate president who is under suspicion for arrest, holds a ballroom event. Among the invitees are detective Shinjuurou Yuuki; his strange partner, Inga; and Rie, the daughter of JJ Systems Chairman Rinroku Kaishou. During the event, Kanou walks onto the podium to profess his innocence, and falls dead just as he walks into the spotlight. Koyama finds that Kanou was murdered by a knife in his back. Kanou's wife blames the government for killing him, blaming a female SP officer, who was found shot dead in an elevator, for the assassination. However, Shinjuurou debunks this, questioning why the SP officer had her jacket next to her and a bullet in her chest. After deducing that Kanou's wife was the killer, Inga asks her, "What was your husband to you?" Compelled to answer, Kanou's wife states that Kanou was a hero for supporting Japan during the war. Not wanting to let the government arrest him, Kanou's wife killed the SP officer, impersonated her, and killed Kanou to ensure that he would die as a hero."
 		)
-;
-
+	,	("Un-Go",
+		 2,
+		 "Pitiless Song",
+		 "A wealthy woman named Hisako Osada is found murdered in a briefcase that was delivered to her home. While the police suspect Hisako's lover to be the culprit, her daughter, An, believes someone else is behind the murder and calls on Shinjuurou to investigate. Their investigation leads them to look into an idol group Hisako managed named Yonagahime 3+1, one of its members of which, Eri, was allegedly killed in a terrorist attack. Inga uses her powers on one of Yonagahime's members, who reveals that Eri never existed and was just used by Hisako to skyrocket the band's popularity during the war. After analyzing Eri's vocal data, Shinjuurou deduces the culprit to be An, who wasn't allowed to publicly sing due to her vocal data being used for Eri."
+		 )
+	,	("Un-Go",
+		 3,
+		 "Masked Mansion",
+		 "Rie calls on Shinjuurou and Inga to accept a request from Mitsuko Sasa, whose adopted brother Kazamori caught on fire and died, allegedly related to a curse from her father, Komamori, who died in a similar manner seven years ago. As Izumi's group conducts their own investigations on the cause of Kazamori's death, Inga uses her powers to reveal that Kazamori not human. Shinjuurou prompts Kazamori to show "himself", revealing that "he" is actually a computer program."
+		 )
+	,	("Un-Go",
+		 4,
+		 "House, Unmasked",
+		 "Kazamori is revealed to be an R.A.I., one of the last created by Komamori before they were outlawed. As Kazamori is accused of killing the corpse that was assumed to be him, Shinjuurou and Inga download Kazamori into a panda toy before he is taken into custody. After discovering that the corpse was Dr. Taku, and that the Dr. Taku who was injured was a robot puppet, Shinjuurou and others soon discover the culprit is Komamori. He'd faked his death using the body of the real Dr. Taku so that his R.A.I. wouldn't be used by the JSDF. After Komamori is arrested by Izumi, Kazamori moves her consciousness to her main body."
+		 )
+	,	("Un-Go",
+		 5,
+		 "The Phantom Statue",
+		 "At the unveiling of a statue created in memory of three youths who sacrificed themselves to dispose of a bomb in a press conference, two people who were snuck in to try and unveil a purported scam behind the story are found murdered inside the statue. Shinjuurou initially suspects the statue's commissioner, Hakuro Shimada, as the murderer, but Inga's question proves his innocence. After a brief depression and a pep talk from Kazamori, Shinjuurou deduces the culprit to be the statue's creator, Youko, who killed the men before they were transported, before later revealing the location of gold bricks that Hakuro had hidden."
+		 )
+	,	("Un-Go",
+		 6,
+		 "The Code Too Simple",
+		 "Shinjuurou is approached by Yajima, an old friend of Rinroku, who found a piece of manuscript in a book given to him by an inmate while he was in prison. After discovering Yajima's wife Takiko's fingerprints on the paper, Shinjuurou informs Rie, who investigates how the book relates to Rinroku. Shinjuurou later hears from Yajima about how his children disappeared and his wife went blind, allegedly having an affair with Rinroku. Shinjuurou looks at all the books Rinroku bought from the Yajima family, finding more hidden codes which are revealed to be from Yajima's children. As it turns out, Rinroku had helped the children and placed them in child care to avoid Takiko's neglect, and they are soon reunited with Yajima. Afterward, Shinjuurou confronts the prisoner who gave Yajima the book, learning something shocking."
+		 )
+	,	("Un-Go",
+		 7,
+		 "Daydream",
+		 "As the prisoner explains how he is 'a novelist who writes in reality', a strange being accompanying him traps Shinjuurou in a vision where he is a cameraman shooting a film about a war that never happened. As Shinjuurou becomes curious about whether the director is involved with one of the actresses, he gets the nagging feeling that there's a mystery to be solved. As Shinjuurou loses grip on this reality, he discovers the director has been killed and believes himself to be the culprit."
+		 )
+	,	("Un-Go",
+		 8,
+		 "The King of Paradise",
+		 "After determining that Shinjuurou had been imprisoned, Rie accompanies Izumi to the prison, where they are both put under the novelist's spell and become characters in his 'novel' alongside Shinjuurou. As Inga determines this is the work of a deity known as Bettenou, Kazamori manages to make use of a pair of taser handcuffs to snap Shinjuurou back to reality. Shinjuurou investigates into the real murder, determining the culprit to be one of the 'actresses', who killed the 'director', who was actually a security guard trying to help her escape, because she believed she was being cut from the movie. Meanwhile, Inga gets the truth out of the novelist while Bettenou disappears."
+		)
+	,	("Un-Go",
+		 9,
+		 "Rinroku Kaishou's Crime",
+		 "As Shinjuurou wonders where Bettenou has disappeared to, a hacking group known as Full Circle is spreading leaks of Rinroku's company and lifestyle. As Rinroku makes an appearance on television show along with various people from rival companies, an explosion occurs when he exits the room, hospitalizing everyone including Rinroku, while another Rinroku appears at Rie's house. As Rie asks Shinjuurou to prove Rinroku's innocence the next day, Inga comes face to face with Bettenou. After Shinjuurou hears about the second Rinroku from Rie, he deduces that this is the work of Bettenou and believes Rinroku to be the one using her. He gets help from a rookie Diet member to have the hospitalized Rinroku provide a testimony on the leaks."
+		)
+	,	("Un-Go",
+		 10,
+		 "Rinroku Kaisho's Funeral",
+		 "As Rinroku is interviewed via a live video feed, Shinjuurou has Rie tell everyone about the Rinroku that appeared in her home at the time of the explosion. To Shinjuurou's surprise, Inga is called in as a witness to question Izumi, forcing her to reveal secrets about Rinroku's involvement in illegal software used prior to the war. As the hospitalized Rinroku is discharged, he is killed in a car explosion. After speaking with Rie, Shinjuurou suspects something is amiss and manages to learn more about Bettenou from the novelist, further founding his theory that Rinroku used Bettenou's power to fake his death."
+		 )
+	,	("Un-Go",
+		 11,
+		 "I'm Just Searching",
+		 "Shinjuurou, along with Rie, Izumi and some others, receive strange invitations supposedly sent by Rinroku. They are brought together at a table where they are presented with weapons, Shinjuurou believes that the imposter Rinroku was the one who appeared at Rie's home, deducing that the Diet member and Rinroku's bodyguard caused the explosion. It is at this point that Rinroku himself seemingly appears with Inga, only for Shinjuurou to deduce that he is actually Izumi's assistant, Hayami Seigen, using Bettenou's power, with the real Rinroku having disguised himself amongst the attendees, having previously faked his death. After a fierce battle, Inga defeats Bettenou and devours her. After the incident, Shinjuurou begins telling his story to Rie, including the origin of Inga's current body."
+		 )
+	
 INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
-		("Title", x, y, z)
+		("Un-Go", 0, 1, 4)
+	,	("Un-Go", 0, 2, 4)
+	,	("Un-Go", 0, 3, 3)
 ;
-		
--- General Anime Volume Data
-	,	("Title",
-		 n,
-		 m,
-		 "Classification",
-		 x,
-		 "URL"
-		)
-		
--- General Anime Episode Data
-	,	("Title",
-		 n,
-		 "Episode_Title",
-		 "Episode_Synopsys"
-		)
-		
--- General Anime Session Data
-	,	("Title", x, y, z)
