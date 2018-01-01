137ad664ee7aac584cca8e48c960166b6063323d
James P. Galovic <jpgalovic@gmail.com>
2018-01-01 16:55:41 +1100

    Fix image for assisination classroom 2, bd part 1

--- sql/data/swin_anime_database.sql
+++ sql/data/swin_anime_database.sql
@@ -4264,6 +4264,7 @@ INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_O
 	,	("Nadia: The Secret of Blue Water", 1, 3, 8)
 	,	("Nadia: The Secret of Blue Water", 1, 4, 15)
 ;
+-- Summer A
 INSERT INTO EVENT_DATA (EVENT_TIME, EVENT_TYPE_ID, EVENT_TITLE, EVENT_LOCATION, EVENT_FACEBOOK_ID, EVENT_UNIONE_URL) VALUES
 		("2018-01-05 11-30-00", 0, "Summer Sessions", 0, 0, "0")
 	,	("2018-01-12 11-30-00", 0, "Summer Sessions", 0, 0, "0")
@@ -4276,37 +4277,6 @@ INSERT INTO EVENT_DATA (EVENT_TIME, EVENT_TYPE_ID, EVENT_TITLE, EVENT_LOCATION, 
 	,	("2018-01-05 19-30-00", 0, "Summer After Dark", 0, 0, "0")
 	,	("2018-01-12 19-30-00", 0, "Summer After Dark", 0, 0, "0")
 	,	("2018-01-19 19-30-00", 0, "Summer After Dark", 0, 0, "0")
-	
-	,	("2018-01-26 11-30-00", 0, "Summer Sessions", 0, 0, "0")
-	,	("2018-02-02 11-30-00", 0, "Summer Sessions", 0, 0, "0")
-	,	("2018-02-09 11-30-00", 0, "Summer Sessions", 0, 0, "0")
-	
-	,	("2018-01-26 16-30-00", 0, "Summer Sessions", 0, 0, "0")
-	,	("2018-02-02 16-30-00", 0, "Summer Sessions", 0, 0, "0")
-	,	("2018-02-09 16-30-00", 0, "Summer Sessions", 0, 0, "0")
-	
-	,	("2018-01-26 19-30-00", 0, "Summer After Dark", 0, 0, "0")
-	,	("2018-02-02 19-30-00", 0, "Summer After Dark", 0, 0, "0")
-	,	("2018-02-09 19-30-00", 0, "Summer After Dark", 0, 0, "0")
-	
-	,	("2018-01-08 14-30-00", 0, "Summer Showcase", 0, 0, "0")
-	,	("2018-01-08 15-30-00", 0, "Summer Showcase", 0, 0, "0")
-	
-	,	("2018-01-15 14-30-00", 0, "Summer Showcase", 0, 0, "0")
-	,	("2018-01-15 15-30-00", 0, "Summer Showcase", 0, 0, "0")
-	
-	,	("2018-01-22 14-30-00", 0, "Summer Showcase", 0, 0, "0")
-	,	("2018-01-22 15-30-00", 0, "Summer Showcase", 0, 0, "0")
-	
-	,	("2018-01-29 14-30-00", 0, "Summer Showcase", 0, 0, "0")
-	,	("2018-01-29 15-30-00", 0, "Summer Showcase", 0, 0, "0")
-	
-	,	("2018-02-05 14-30-00", 0, "Summer Showcase", 0, 0, "0")
-	,	("2018-02-05 15-30-00", 0, "Summer Showcase", 0, 0, "0")
-	
-	,	("2018-01-10 10-00-00", 1, "Beach Episode", 2, 0, "0")
-	,	("2018-01-17 10-00-00", 1, "Arcade Episode", 1, 0, "0")
-	,	("2018-01-31 10-00-00", 1, "Pool Episode", 3, 0, "0")
 ;
 
 INSERT INTO EVENT_ANIME_DATA (EVENT_TIME, ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER) VALUES
@@ -4321,8 +4291,42 @@ INSERT INTO EVENT_ANIME_DATA (EVENT_TIME, ANIME_TITLE, SESSION_TYPE_ID, SESSION_
 	,	("2018-01-05 19-30-00", "Plastic Memories", 0, 1)
 	,	("2018-01-12 19-30-00", "Plastic Memories", 0, 2)
 	,	("2018-01-19 19-30-00", "Plastic Memories", 0, 3)
+;
+
+-- Summer Showcase A
+INSERT INTO EVENT_DATA (EVENT_TIME, EVENT_TYPE_ID, EVENT_TITLE, EVENT_LOCATION, EVENT_FACEBOOK_ID, EVENT_UNIONE_URL) VALUES
+		("2018-01-08 14-30-00", 0, "Summer Showcase", 0, 0, "0")
+	,	("2018-01-08 15-30-00", 0, "Summer Showcase", 0, 0, "0")
 	
-	,	("2018-01-26 11-30-00", "Assassination Classroom Season 2", 0, 1)
+	,	("2018-01-15 14-30-00", 0, "Summer Showcase", 0, 0, "0")
+	,	("2018-01-15 15-30-00", 0, "Summer Showcase", 0, 0, "0")
+;
+
+INSERT INTO EVENT_ANIME_DATA (EVENT_TIME, ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER) VALUES	
+		("2018-01-08 14-30-00", "Vividred Operation", 3, 1)
+	,	("2018-01-08 15-30-00", "Magical Warfare", 3, 1)
+	
+	,	("2018-01-15 14-30-00", "Cowboy Bebop", 3, 1)
+	,	("2018-01-15 15-30-00", "Deadman Wonderland", 3, 1)
+;
+
+-- Summer B
+INSERT INTO EVENT_DATA (EVENT_TIME, EVENT_TYPE_ID, EVENT_TITLE, EVENT_LOCATION, EVENT_FACEBOOK_ID, EVENT_UNIONE_URL) VALUES
+		("2018-01-26 11-30-00", 0, "Summer Sessions", 0, 0, "0")
+	,	("2018-02-02 11-30-00", 0, "Summer Sessions", 0, 0, "0")
+	,	("2018-02-09 11-30-00", 0, "Summer Sessions", 0, 0, "0")
+	
+	,	("2018-01-26 16-30-00", 0, "Summer Sessions", 0, 0, "0")
+	,	("2018-02-02 16-30-00", 0, "Summer Sessions", 0, 0, "0")
+	,	("2018-02-09 16-30-00", 0, "Summer Sessions", 0, 0, "0")
+	
+	,	("2018-01-26 19-30-00", 0, "Summer After Dark", 0, 0, "0")
+	,	("2018-02-02 19-30-00", 0, "Summer After Dark", 0, 0, "0")
+	,	("2018-02-09 19-30-00", 0, "Summer After Dark", 0, 0, "0")
+;
+
+INSERT INTO EVENT_ANIME_DATA (EVENT_TIME, ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER) VALUES
+		("2018-01-26 11-30-00", "Assassination Classroom Season 2", 0, 1)
 	,	("2018-02-02 11-30-00", "Assassination Classroom Season 2", 0, 2)
 	,	("2018-02-09 11-30-00", "Assassination Classroom Season 2", 0, 3)
 	
@@ -4333,20 +4337,65 @@ INSERT INTO EVENT_ANIME_DATA (EVENT_TIME, ANIME_TITLE, SESSION_TYPE_ID, SESSION_
 	,	("2018-01-26 19-30-00", "Queens Blade 2 The Evil Eye", 0, 1)
 	,	("2018-02-02 19-30-00", "Queens Blade 2 The Evil Eye", 0, 2)
 	,	("2018-02-09 19-30-00", "Queens Blade 2 The Evil Eye", 0, 3)
-	
-	,	("2018-01-08 14-30-00", "Vividred Operation", 3, 1)
-	,	("2018-01-08 15-30-00", "Magical Warfare", 3, 1)
-	
-	,	("2018-01-15 14-30-00", "Cowboy Bebop", 3, 1)
-	,	("2018-01-15 15-30-00", "Deadman Wonderland", 3, 1)
-	
-	,	("2018-01-22 14-30-00", "", 3, 1)
-	,	("2018-01-22 15-30-00", "", 3, 1)
-	
-	,	("2018-01-29 14-30-00", "", 3, 1)
-	,	("2018-01-29 15-30-00", "", 3, 1)
-	
-	,	("2018-02-05 14-30-00", "", 3, 1)
-	,	("2018-02-05 15-30-00", "", 3, 1)
 ;
 
+-- Summer Showcase B
+INSERT INTO EVENT_DATA (EVENT_TIME, EVENT_TYPE_ID, EVENT_TITLE, EVENT_LOCATION, EVENT_FACEBOOK_ID, EVENT_UNIONE_URL) VALUES
+		("2018-01-22 14-30-00", 0, "Summer Showcase", 0, 0, "0")
+	,	("2018-01-22 15-30-00", 0, "Summer Showcase", 0, 0, "0")
+	
+	,	("2018-01-29 14-30-00", 0, "Summer Showcase", 0, 0, "0")
+	,	("2018-01-29 15-30-00", 0, "Summer Showcase", 0, 0, "0")
+	
+	,	("2018-02-05 14-30-00", 0, "Summer Showcase", 0, 0, "0")
+	,	("2018-02-05 15-30-00", 0, "Summer Showcase", 0, 0, "0")
+;
+	
+-- INSERT INTO EVENT_ANIME_DATA (EVENT_TIME, ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER) VALUES
+-- 		("2018-01-22 14-30-00", "", 3, 1)
+-- 	,	("2018-01-22 15-30-00", "", 3, 1)
+-- 	
+-- 	,	("2018-01-29 14-30-00", "", 3, 1)
+-- 	,	("2018-01-29 15-30-00", "", 3, 1)
+-- 	
+-- 	,	("2018-02-05 14-30-00", "", 3, 1)
+-- 	,	("2018-02-05 15-30-00", "", 3, 1)
+-- ;
+	
+-- Summer Episodes
+INSERT INTO EVENT_DATA (EVENT_TIME, EVENT_TYPE_ID, EVENT_TITLE, EVENT_LOCATION, EVENT_FACEBOOK_ID, EVENT_UNIONE_URL) VALUES
+		("2018-01-10 10-00-00", 1, "Beach Episode", 2, 0, "0")
+	,	("2018-01-17 10-00-00", 1, "Arcade Episode", 1, 0, "0")
+	,	("2018-01-31 10-00-00", 1, "Pool Episode", 3, 0, "0")
+;
+
+-- A
+INSERT INTO EVENT_DATA (EVENT_TIME, EVENT_TYPE_ID, EVENT_TITLE, EVENT_LOCATION, EVENT_FACEBOOK_ID, EVENT_UNIONE_URL) VALUES
+		("2018-03-02 11-30-00", 0, "Screening Sessions", 0, 0, "0")
+	,	("2018-03-09 11-30-00", 0, "Screening Sessions", 0, 0, "0")
+	,	("2018-03-16 11-30-00", 0, "Screening Sessions", 0, 0, "0")
+	
+	,	("2018-03-02 16-30-00", 0, "Screening Sessions", 0, 0, "0")
+	,	("2018-03-09 16-30-00", 0, "Screening Sessions", 0, 0, "0")
+	,	("2018-03-16 16-30-00", 0, "Screening Sessions", 0, 0, "0")
+	
+	,	("2018-03-02 19-30-00", 0, "Screening Sessions", 0, 0, "0")
+	,	("2018-03-09 19-30-00", 0, "Screening Sessions", 0, 0, "0")
+	,	("2018-03-16 19-30-00", 0, "Screening Sessions", 0, 0, "0")
+;
+
+INSERT INTO EVENT_ANIME_DATA (EVENT_TIME, ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER) VALUES
+		("2018-03-02 11-30-00", "Muv-Luv Alternative Total Eclipse", 0, 1)
+	,	("2018-03-09 11-30-00", "Muv-Luv Alternative Total Eclipse", 0, 2)
+	,	("2018-03-16 11-30-00", "Muv-Luv Alternative Total Eclipse", 0, 3)
+	
+	,	("2018-03-02 16-30-00", "Canaan", 0, 1)
+	,	("2018-03-09 16-30-00", "Canaan", 0, 2)
+	,	("2018-03-16 16-30-00", "Canaan", 0, 3)
+	
+	,	("2018-03-02 19-30-00", "Danganronpa", 0, 1)
+	,	("2018-03-09 19-30-00", "Danganronpa", 0, 2)
+	,	("2018-03-16 19-30-00", "Danganronpa", 0, 3)
+;
+
+-- A Showcase
