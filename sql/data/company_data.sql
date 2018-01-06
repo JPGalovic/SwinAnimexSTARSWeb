91cfdabe3568626705232b2529405bd48a42da5d
James P. Galovic <jpgalovic@gmail.com>
2018-01-06 23:44:35 +1100

    Update: Database to include, is company sponsor bool, missing sponsors

--- sql/data/company_data.sql
+++ sql/data/company_data.sql
@@ -2,14 +2,19 @@
 CREATE TABLE IF NOT EXISTS COMPANY (
 		COMPANY_NAME			VARCHAR(50)
 	,	COMPANY_URL				VARCHAR(250)
+	,	IS_SPONSOR				BOOLEAN 			NOT NULL
 	,	PRIMARY KEY				(COMPANY_NAME)
 );
 
 -- Data for Sponsors
-INSERT INTO COMPANY (COMPANY_NAME, COMPANY_URL) VALUES
-		("Madman", "https://madman.com.au/")
-	,	("AnimeLab", "https://animelab.com")
-	,	("Hanabee", "https://hanabee.com.au/")
-	,	("Siren", "https://sirenvisual.com.au/")
+INSERT INTO COMPANY (COMPANY_NAME, COMPANY_URL, IS_SPONSOR) VALUES
+		("Madman", "https://madman.com.au/", TRUE)
+	,	("AnimeLab", "https://animelab.com", TRUE)
+	,	("Hanabee", "https://hanabee.com.au/", TRUE)
+	,	("Siren", "https://sirenvisual.com.au/", TRUE)
+	,	("Rewardle", "https://patrons.rewardle.com/", TRUE)
+	,	("Grain and Nori", "http://www.grainandnori.com.au/", TRUE)
+	,	("Resistance Bar and Cafe", "https://www.facebook.com/TheResistanceBarandCafe/", TRUE)
+	,	("Glenferrie Crepe Cafe", "https://www.facebook.com/GlenferrieCrepeCafe/", TRUE)
 ;
 
