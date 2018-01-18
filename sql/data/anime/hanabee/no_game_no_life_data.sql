333db6c1c402e9b984268809372f9d7674e84823
C1and00j <34433447+C1and00j@users.noreply.github.com>
2018-01-18 18:15:36 +1100

    Add no game no life sql files

--- sql/data/anime/hanabee/no_game_no_life_data.sql
+++ sql/data/anime/hanabee/no_game_no_life_data.sql
@@ -1,51 +1,96 @@
 -- Anime Data
 INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
-		("Title",
-		 n,
-		 "Synopsys",
-		 "Description",
-		 "Company",
-		 "Copyright"
+		("No game No life",
+		 12,
+		 "Just as their urban legend suggests the siblings Sora and Shiro are gods when it comes to the gaming. Blazing across the internet they are known as NEET (Not in Education, Employment, or Training) gamers. Until one day a boy calling himself &quot;God&quot; introduces them to a parallel fantasy world where everything is decided by games. In this world, all armed conflict and violence have been banned and the races must resolve their disagreements through gaming. Can the siblings find be as brilliant as their reputation to be the salvation of all humanity? The odds aren't in their favour, but Shiro and Sora are going to duel till the end in No Game, No Life.",
+		 "Just as their urban legend suggests the siblings Sora and Shiro are gods when it comes to the gaming. Blazing across the internet they are known as NEET (Not in Education, Employment, or Training) gamers.",
+		 "Hanabee",
+		 "&copy;Madhouse"
 		)
 ;
 
 INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
-		("Title",
-		 n,
-		 m,
-		 "Classification",
-		 x,
-		 "URL"
+		("No game No life",
+		 4,
+		 1,
+		 "MA15+ - Strong sexual themes",
+		 12,
+		 "https://hanabee.com.au/products/no-game-no-life-blu-ray"
+		)
+	,	("No game No life",
+		 3,
+		 1,
+		 "MA15+ - Strong sexual themes",
+		 12,
+		 "https://hanabee.com.au/products/no-game-no-life"
 		)
 ;
 
 INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SYNOPSYS) VALUES
-		("Title",
-		 n,
-		 "Episode_Title",
-		 "Episode_Synopsys"
+		("No game No life",
+		 1,
+		 "Beginner",
+		 "Sora and Shiro are two siblings and are known in online games as the undefeated group named The Blank. In real life, they are hikikomori who are ostracized by the world. One day, they receive a challenge from Tet to a game of chess and are victorious. In response he offers to send them to a world which revolves around games and they accept, believing it to be a joke. They are then summoned to a reality known as Disboard where a spell, known as the Ten Pledges, prevents violence and enforces the rules and outcomes of games. They travel to Elkia, the nation inhabited by humans, where Sora plays poker against a woman for her money and wins by out-cheating her. He and Shiro rest at an inn and decide their next goal is to find a home."
+		)
+	,	("No game No life",
+		 2,
+		 "Challenger",
+		 "Stephanie Dola, granddaughter of Elkia's deceased king, loses against Kurami Zell in a contest to decide the next ruler; she then confronts Sora for not revealing how her opponent was cheating. Sora tricks her into a game of rock-paper-scissors and after winning, uses the Pledges to make her fall in love with him. Sora and Shiro then move into the palace with Stephanie where they learn more about world and decide to help Stephanie revitalize Elkia. "
+		)
+	,	("No game No life",
+		 3,
+		 "Expert",
+		 "During Kurami's coronation, Sora reveals she was using magic from the elf, Fil Nilvalen, to win her games. Kurami salvages the situation but is forced to play a game against Sora and Shiro. Originally, they believe the game is chess, but Sora deduces it is a war simulator with chess themed pieces; Sora's charisma rallies his army, giving them an advantage and forcing Kurami to cheat by corrupting Sora's soldiers. In return, Sora persuades Kurami's queen to join his army by declaring her king to be corrupt. "
+		)
+	,	("No game No life",
+		 4,
+		 "Grandmaster",
+		 "Kurami's army begin defecting to her former queen; eventually her king is assassinated due to her tyrannical rule and Sora is victorious. Sora denounces Kurami's plan in assimilating humans with the elves, declaring humanity is stronger than she thinks. Sora and Shiro are crowned the king and queen of humanity and begin solving Elkia's political and economical problems. They later attend the formal coronation and receive humanity's race piece, a chess piece which represents humanity's rights and free-will. Tet visits Sora and Shiro and explains that whoever gathers all sixteen chess pieces, one for each species, will earn the right to challenge him for his title of god; in response Sora and Shiro accept his challenge, declaring they will be the winner. "
+		)
+	,	("No game No life",
+		 5,
+		 "Weak Square",
+		 "Sora and Shiro leave Elkia's political responsibilities to Stephanie until she is challenged to a game by nobles who refuse the reform. She challenges Sora to blackjack in hopes of binding him to the Pledges to perform his duties; she attempts to use false shuffling but loses due to card counting. Understanding her predicament, Sora trumps the nobles and uses the previous game to have Stephanie act like a dog. Stephanie tries several more games on Sora to take responsibility as Elkia's king but fails. Sora and Shiro reveal they were not slacking, but are researching ways to conquer the other nations; in response Stephanie redirects them to Elkia's library which is owned by the flügel Jibril. "
+		)
+	,	("No game No life",
+		 6,
+		 "Interesting",
+		 "Sora and Shiro challenge Jibril by betting their tablet computer full of books, considering it of high value, Jibril offers her freedom in exchange. They begin a game of materialization shiritori, where the words they use will materialize or disappear from the environment. From the game, Sora and Shiro deduce Disboard's scientific research is lacking, and uses this knowledge to their advantage; they succeed after removing coulomb's law and inducing a hypernova, removing Jibril from the game. Impressed by the two, Jibril solemnly pledges her loyalty to them."
+		)
+	,	("No game No life",
+		 7,
+		 "Sacrifice",
+		 "Jibril shares her knowledge about the werebeasts, revealing they have been undefeated for decades and always take the memories of the game away from the opponents. When Sora learns Elkia's king challenged the werebeasts eight times and lost a large amount of land, he denounces the king and upsets Stephanie. Upon further investigation, Sora questions the king's motives and eventually learns about the king's will from Stephanie. Using the key from the will, Sora and Shiro find a hidden library in the king's chambers; there, they discover information regarding the werebeasts and their choice of games. "
+		)
+	,	("No game No life",
+		 8,
+		 "Fake End",
+		 "Sora and Shiro enter the embassy of the Eastern Federation, nation of the werebeasts, to issue a formal challenge. Sora and Shiro reveals the werebeasts use video games and threatens to leak that information to the elves if their challenge is not accepted; Sora bets humanity's chess piece in return for all of the land on the continent. As the Eastern Federation makes preparations for the game, Sora waits for the missing piece needed for victory; the next day, Shiro realizes everyone has forgotten about Sora. "
+		)
+	,	("No game No life",
+		 9,
+		 "Sky Walk",
+		 "Believing Shiro's memories have been tampered with, Jibril arranges a chess game to remove the memories regarding Sora; Shiro decides her memories are real and wins the match. After investigating, Shiro recalls Sora arranged a game of reversi with Kurami; the game's pieces consist of the components making up Sora and Kurami's identity. Having lost most of his pieces, Sora was nearly erased from existence but the last three pieces allow Shiro to keep her memories of him. Shiro finds the pieces in her room and places them on an invisible board, winning the game and restoring Sora. From his victory, Sora shares his memories with Kurami, allowing them to form an alliance, and gains the right to manipulate Fil's memories. "
+		)
+	,	("No game No life",
+		 10,
+		 "Blue Rose",
+		 "Sora and friends bond with Kurami and Fil as they make the final preparations to conquer the Eastern Federation. Per Sora's demands, the match against the werebeasts is made public in order to prevent them from cheating. The game begins and Sora, Shiro, Stephanie, Jibril, and their opponent Izuna Hatsuse, are sent to a virtual reality world which resembles Tokyo. Sora and Shiro's agoraphobia acts up and they enter a devoid state. "
+		)
+	,	("No game No life",
+		 11,
+		 "Killing Giant",
+		 "Recalling they are in virtual reality, Sora and Shiro return to normal; the game is a form of cops and robbers where they use guns that fire heart-shaped bullets. While Stephanie remains clueless about the game, Sora, Shiro, and Jibril battle against Izuna and deduce that her grandfather, Ino Hatsuse, is relaying their location and state of mind to her. Sora and friends regroup as Shiro makes the proper adjustments to their plans. Executing the unspoken plan, Sora and Shiro corner Izuna, forcing her to use an ability called Blood Destruction to augment her abilities. "
+		)
+	,	("No game No life",
+		 12,
+		 "Rule Number 10",
+		 "Sora and Shiro are defeated; as Izuna drops her guard, she is shot by Stephanie riding a NPC. Sora reveals he bound Stephanie to the Pledges to hide her intent while Shiro calculated the NPC's movements, allowing Stephanie to hide her location. While Izuna laments the werebeasts' fate, Sora assures her of his goodwill. Sora forces political pressure on the Eastern Federation leader, the nameless miko, to challenge him to a game. They have a coin flip where Sora has it land on its edge. He convinces the miko to declare they both win and as a result, Elkia can share resources with the Eastern Federation while the werebeasts maintain their self-rule; the allied nation is named the Elkia Federation. In the aftermath, Sora and friends have the miko channel an old deus for a game. "
 		)
 ;
 
 INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
-		("Title", x, y, z)
+		("No game No life", 0, 1, 4)
+	,	("No game No life", 0, 2, 4)
+	,	("No game No life", 0, 3, 4)
 ;
-		
--- General Anime Volume Data
-	,	("Title",
-		 n,
-		 m,
-		 "Classification",
-		 x,
-		 "URL"
-		)
-		
--- General Anime Episode Data
-	,	("Title",
-		 n,
-		 "Episode_Title",
-		 "Episode_Synopsys"
-		)
-		
--- General Anime Session Data
-	,	("Title", x, y, z)
