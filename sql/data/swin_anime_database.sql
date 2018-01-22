06825837d32a3c57025279afba5f903b977194b2
James P. Galovic <jpgalovic@gmail.com>
2018-01-22 14:11:16 +1100

    Add: Missing classification marking, Update Database

--- sql/data/swin_anime_database.sql
+++ sql/data/swin_anime_database.sql
@@ -5636,33 +5636,3 @@ INSERT INTO EVENT_DATA (EVENT_TIME, EVENT_TYPE_ID, EVENT_TITLE, EVENT_LOCATION, 
 	,	("2018-01-31 10-00-00", 1, "Pool Episode", 3, 0, "0")
 ;
 
--- A
-INSERT INTO EVENT_DATA (EVENT_TIME, EVENT_TYPE_ID, EVENT_TITLE, EVENT_LOCATION, EVENT_FACEBOOK_ID, EVENT_UNIONE_URL) VALUES
-		("2018-03-02 11-30-00", 0, "Screening Sessions", 0, 0, "0")
-	,	("2018-03-09 11-30-00", 0, "Screening Sessions", 0, 0, "0")
-	,	("2018-03-16 11-30-00", 0, "Screening Sessions", 0, 0, "0")
-	
-	,	("2018-03-02 16-30-00", 0, "Screening Sessions", 0, 0, "0")
-	,	("2018-03-09 16-30-00", 0, "Screening Sessions", 0, 0, "0")
-	,	("2018-03-16 16-30-00", 0, "Screening Sessions", 0, 0, "0")
-	
-	,	("2018-03-02 19-30-00", 0, "Screening Sessions", 0, 0, "0")
-	,	("2018-03-09 19-30-00", 0, "Screening Sessions", 0, 0, "0")
-	,	("2018-03-16 19-30-00", 0, "Screening Sessions", 0, 0, "0")
-;
-
-INSERT INTO EVENT_ANIME_DATA (EVENT_TIME, ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER) VALUES
-		("2018-03-02 11-30-00", "Muv-Luv Alternative Total Eclipse", 0, 1)
-	,	("2018-03-09 11-30-00", "Muv-Luv Alternative Total Eclipse", 0, 2)
-	,	("2018-03-16 11-30-00", "Muv-Luv Alternative Total Eclipse", 0, 3)
-	
-	,	("2018-03-02 16-30-00", "Canaan", 0, 1)
-	,	("2018-03-09 16-30-00", "Canaan", 0, 2)
-	,	("2018-03-16 16-30-00", "Canaan", 0, 3)
-	
-	,	("2018-03-02 19-30-00", "Danganronpa", 0, 1)
-	,	("2018-03-09 19-30-00", "Danganronpa", 0, 2)
-	,	("2018-03-16 19-30-00", "Danganronpa", 0, 3)
-;
-
--- A Showcase
