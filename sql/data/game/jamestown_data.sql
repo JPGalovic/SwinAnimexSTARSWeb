d0d7dffafa2e2ea0e8133a89213fd9ab50e63500
James P. Galovic <jpgalovic@gmail.com>
2018-01-25 00:27:17 +1100

    Fix: Errors with Template Mastercode
    Fix: Numbering on Platform ID's

--- sql/data/game/jamestown_data.sql
+++ sql/data/game/jamestown_data.sql
@@ -1,6 +1,6 @@
 -- Jamestown Data, Version 1.0, JAN18, JPGalovic
-INSERT INTO GAME (GAME_TITLE, GAME_DESCRIPTION, MIN_NUMBER_OF_PLAYERS, MAX_NUMBER_OF_PLAYERS, RECOMMENDED NUMBER OF PLAYERS, COMPANY_NAME, CLASSIFICATION) VALUES
-		("Jamestown: Legend of the Lost Colony",
+INSERT INTO GAME (GAME_TITLE, GAME_DESCIRPTION, MIN_NUMBER_OF_PLAYERS, MAX_NUMBER_OF_PLAYERS, RECOMMENDED_NUMBER_OF_PLAYERS, COMPANY_NAME, CLASSIFICATION) VALUES
+		("Jamestown&#58; Legend of the Lost Colony",
 		 "<p>Jamestown is an old-school shooter with a new-school heart. It features all the intensity, depth, and lovingly handcrafted pixels of a classic arcade shooter, with a modern twist: deeply-integrated cooperative gameplay.</p><p>Classically, top-down shooters give multiple players a shared pool of lives or continues; this inevitably results in one of the players “failing out” and being left to watch their more skilled (or lucky) friend continue to have fun. In Jamestown, so long as any player survives, the entire team can be brought back to life. By giving even novice players the power to rescue their team from the jaws of death, Jamestown creates an exhilarating cooperative thrill-ride where everyone’s contribution truly matters. Win or lose, your team will do it together.</p><p>This innovative take on co-op enhances an already rock-solid arcade shooter experience. With its addictive “Vaunt” shield mechanic, richly rewarding scoring system, and 5 difficulty levels ranging from “first-time player” to “masochistic bullet junkie,” Jamestown delivers an engaging core experience no matter how you want to play it.</p><p>The game features 4 playable ships, each with a unique set of abilities. Over the course of the game’s campaign mode, players will experience 5 screen-filling boss battles, over 40 different enemies, 20 rule-bending bonus levels, lush hand-pixeled visuals, and an original orchestral score by classically-trained Chilean composer Francisco Cerda.</p>",
 		 1,
 		 4,
@@ -11,8 +11,8 @@ INSERT INTO GAME (GAME_TITLE, GAME_DESCRIPTION, MIN_NUMBER_OF_PLAYERS, MAX_NUMBE
 ;
 
 INSERT INTO GAME_PLATFORM (GAME_TITLE, PLATFORM_ID) VALUES
-		("Jamestown: Legend of the Lost Colony", 1)
-	,	("Jamestown: Legend of the Lost Colony", 2)
-	,	("Jamestown: Legend of the Lost Colony", 3)
-	,	("Jamestown: Legend of the Lost Colony", 23)
+		("Jamestown&#58; Legend of the Lost Colony", 0)
+	,	("Jamestown&#58; Legend of the Lost Colony", 1)
+	,	("Jamestown&#58; Legend of the Lost Colony", 2)
+	,	("Jamestown&#58; Legend of the Lost Colony", 23)
 ;
