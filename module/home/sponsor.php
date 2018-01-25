e7c779663ebc1e4197c2ab9b6b50066582e717f9
James P. Galovic <jpgalovic@gmail.com>
2018-01-26 02:49:29 +1100

    Fix: Sponsors section eliminates non sponsor companies

--- module/home/sponsor.php
+++ module/home/sponsor.php
@@ -9,7 +9,10 @@
 
 		while($company_row = $get_company_data->fetch_assoc())
 		{
-			echo('<a href="'.$company_row['COMPANY_URL'].'"><img src="image/sponsor/'.remove_illegal_char(strtolower($company_row['COMPANY_NAME'])).'.png" alt="'.$company_row['COMPANY_NAME'].'"/></a>');
+			if($company_row['IS_SPONSOR'] == true)
+			{
+				echo('<a href="'.$company_row['COMPANY_URL'].'"><img src="image/sponsor/'.remove_illegal_char(strtolower($company_row['COMPANY_NAME'])).'.png" alt="'.$company_row['COMPANY_NAME'].'"/></a>');
+			}
 		}
 
 		echo('</span></article>');
--- sql/misc/get_company_data.php
+++ sql/misc/get_company_data.php
@@ -3,9 +3,9 @@
 	// Flags: company_name: sting.
 
 	if(isset($company_name)) // Company Name set, select details for given company
-		$get_company_query = 'SELECT COMPANY_NAME, COMPANY_URL FROM COMPANY WHERE COMPANY_NAME = "'.$company_name.'"';
+		$get_company_query = 'SELECT COMPANY_NAME, COMPANY_URL, IS_SPONSOR FROM COMPANY WHERE COMPANY_NAME = "'.$company_name.'"';
 	else // Company Name not set, select details for all companies
-		$get_company_query = 'SELECT COMPANY_NAME, COMPANY_URL FROM COMPANY';
+		$get_company_query = 'SELECT COMPANY_NAME, COMPANY_URL, IS_SPONSOR FROM COMPANY';
 
 	if(!$get_company_data = $DB->query($get_company_query))
 	{
