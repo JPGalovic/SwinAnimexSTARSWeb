fe07b7adc0d3cb34697da2773cc9330be8d43dff
James P. Galovic <jpgalovic@gmail.com>
2018-01-26 02:43:04 +1100

    Add CTC Marking
    Add Basic Card Formation

--- module/event_card/video_game_event_card.php
+++ module/event_card/video_game_event_card.php
@@ -8,12 +8,27 @@
 	if(!$get_game_event_data->num_rows == 0)//Check for game data
 	{
 		$game_event_row = $get_game_event_data->fetch_assoc();
-		echo('<img src="image/game/'.remove_illegal_char(remove_illegal_char(strtolower($game_event_row['GAME_TITLE']))).'/event_image.png" alt="'.remove_illegal_char(remove_illegal_char(strtolower($game_event_row['GAME_TITLE']))).'">');
-		echo('<h4>'.$event_row['EVENT_TITLE'].' - '.$game_event_row['GAME_TITLE'].' - [TORNIMENT/MARATHON/LAN]</h4>');
-		echo('<p>'.date('l jS F Y - g:ia', strtotime($event_row['EVENT_TIME'])).'</p>');
-		echo('<p>'.$event_row['CAMPUS'].' - '.$event_row['ROOM'].'</p>');
-		echo('<img src="image/classification/[CLASSIFICATION]" alt="[CLASSIFICATION]" id="classification">
-');
+		$game_title = $game_event_row['GAME_TITLE'];
+		// Get Game Specific Data
+		include('sql/games/get_game_classification_data.php');
+		
+		if(!$get_game_classification_data->num_rows == 0)
+		{
+			$game_classification_row = $get_game_classification_data->fetch_assoc();
+			
+			echo('<img src="image/game/'.remove_illegal_char(remove_illegal_char(strtolower($game_event_row['GAME_TITLE']))).'/event_image.png" alt="'.remove_illegal_char(remove_illegal_char(strtolower($game_event_row['GAME_TITLE']))).'">');
+			echo('<h4>'.$event_row['EVENT_TITLE'].' - '.$game_event_row['GAME_TITLE']);
+			if(!$game_event_row['TYPE_ID'] == 1)
+				echo(' - '.$game_event_row['DESCRIPTION'].'</h4>');
+			else
+				echo('</h4>');
+
+			echo('<p>'.date('l jS F Y - g:ia', strtotime($event_row['EVENT_TIME'])).'</p>');
+			echo('<p>'.$event_row['CAMPUS'].' - '.$event_row['ROOM'].'</p>');
+
+
+			echo('<img src="image/classification/'.strtolower($game_classification_row['CLASSIFICATION']).'.png" alt="'.$game_classification_row['CLASSIFICATION'].'" id="classification">');
+		}
 	}
 	else
 	{
