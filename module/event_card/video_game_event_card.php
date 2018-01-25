2c6231f2cf3d66a40b5c4bcddf9a8a7ff557bce0
James P. Galovic <jpgalovic@gmail.com>
2018-01-26 02:04:09 +1100

    Fix: need to us : in sted of html ascii code as code does not work

--- module/event_card/video_game_event_card.php
+++ module/event_card/video_game_event_card.php
@@ -2,12 +2,39 @@
 	// Card for display of basic Video Game Event, Version 1.0, JAN18, JPGalovic
 	// Get Game Event Data
 	include('sql/events/get_game_event_data.php');
+
+	echo('<section class="quater" id="event_card">');
+
+	if(!$get_game_event_data->num_rows == 0)//Check for game data
+	{
+		$game_event_row = $get_game_event_data->fetch_assoc();
+		echo('<img src="image/game/'.remove_illegal_char(remove_illegal_char(strtolower($game_event_row['GAME_TITLE']))).'/event_image.png" alt="'.remove_illegal_char(remove_illegal_char(strtolower($game_event_row['GAME_TITLE']))).'">');
+		echo('<h4>'.$event_row['EVENT_TITLE'].' - '.$game_event_row['GAME_TITLE'].' - [TORNIMENT/MARATHON/LAN]</h4>');
+		echo('<p>'.date('l jS F Y - g:ia', strtotime($event_row['EVENT_TIME'])).'</p>');
+		echo('<p>'.$event_row['CAMPUS'].' - '.$event_row['ROOM'].'</p>');
+		echo('<img src="image/classification/[CLASSIFICATION]" alt="[CLASSIFICATION]" id="classification">
+');
+	}
+	else
+	{
+		echo('<img src="image/event/'.$event_row['EVENT_TITLE'].'.png" alt="'.$event_row['EVENT_TITLE'].'">');
+		echo('<h4>'.$event_row['EVENT_TITLE'].'</h4>');
+		echo('<p>'.date('l jS F Y - g:ia', strtotime($event_row['EVENT_TIME'])).'</p>');
+		echo('<p>'.$event_row['CAMPUS'].' - '.$event_row['ROOM'].'</p>');
+	}
+
+	echo('</section>');
 ?>
 
-<section class="quater" id="event_card">
-	<img src="image/game/[GAME_TITLE]/event_image.png" alt="[GAME_TITLE]">
-	<h4>[EVENT_TITLE] - [GAME_TITLE] - [TORNIMENT/MARATHON/LAN]</h4>
-	<p>[EVENT_DATETIME]</p>
-	<p>[EVENT_CAMPUS] - [EVENT-ROOM]</p>
-	<img src="image/classification/[CLASSIFICATION]" alt="[CLASSIFICATION]" id="classification">
-</section>
+
+	
+	
+	
+	
+	
+
+
+	
+	
+	
+	
