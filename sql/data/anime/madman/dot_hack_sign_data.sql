c8fa319393f4543a65daa9776c104d6cbea7e901
James P. Galovic <jpgalovic@gmail.com>
2018-01-29 04:16:33 +1100

    Add: data for dot hack sign

--- sql/data/anime/madman/dot_hack_sign_data.sql
+++ sql/data/anime/madman/dot_hack_sign_data.sql
@@ -1,51 +1,158 @@
 -- Anime Data
 INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
-		("Title",
-		 n,
-		 "Synopsys",
-		 "Description",
-		 "Company",
-		 "Copyright"
+		(".hack//SIGN",
+		 26,
+		 "Login to the most successful online game in history! &quot;The World&quot; is being played by millions of people around the world, making it the most popular game ever. Players can go online and trade items, form parties, and go on quests. But none of that concerns Tsukasa. Trapped within &quot;The World&quot;, a virtual prisoner - His journey from the digital world to the real world has just begun.",
+		 "Login to the most successful online game in history! &quot;The World&quot; is being played by millions of people around the world, making it the most popular game ever.",
+		 "Madman",
+		 "&copy; Project .hack All Rights Reserved"
 		)
 ;
 
 INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
-		("Title",
-		 n,
-		 m,
-		 "Classification",
-		 x,
-		 "URL"
+		(".hack//SIGN",
+		 0,
+		 1,
+		 "PG",
+		 26,
+		 "https://www.animelab.com/shows/hacksign"
 		)
 ;
 
 INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SYNOPSYS) VALUES
-		("Title",
-		 n,
-		 "Episode_Title",
-		 "Episode_Synopsys"
+		(".hack//SIGN",
+		 1,
+		 "Role Play",
+		 "Tsukasa wakes up to find himself in The World, but he has no idea why he is there, and is not sure who he is. The situation worsens when he discovers he is trapped in the game."
+		)
+	,	(".hack//SIGN",
+		 2,
+		 "Guardian",
+		 "Mimiru and Bear are concerned about Tsukasa's inability to log out. Meanwhile, the Crimson Knights treat him as an illegal character. Tsukasa decides to meet Mimiru and Bear to show them his guardian monster."
+		)
+	,	(".hack//SIGN",
+		 3,
+		 "Folklore",
+		 "BT suggests Tsukasa is related to the legendary item, the Key of the Twilight and Sora suspects the same. Bear tells Subaru that Tsukasa may instead be a victim of a bigger problem."
+		)
+	,	(".hack//SIGN",
+		 4,
+		 "Wanted",
+		 "Tsukasa's guardian has been uncontrollably attacking characters. Wanting to talk with him, Subaru gives the order to make him a 'wanted' on all servers of The World."
+		)
+	,	(".hack//SIGN",
+		 5,
+		 "Captured",
+		 "Tsukasa, now in hiding, learns to control his guardian. Silver Knight and Sora plot a double-cross to lure Tsukasa out, and have him trapped by the Knights."
+		)
+	,	(".hack//SIGN",
+		 6,
+		 "Encounter",
+		 "In captivity, Subaru talks with Tsukasa and she finds out just how real The World has become to him. She decides to release him, but they are interrupted as Sora arrives to 'rescue' him."
+		)
+	,	(".hack//SIGN",
+		 7,
+		 "Reason",
+		 "Mimiru meets an inexperienced player, A20, and helps her to learn the ways of The World. Through her, Mimiru answers her own question of why she plays the game."
+		)
+	,	(".hack//SIGN",
+		 8,
+		 "Promise",
+		 "Wanting to meet up with Tsukasa once more, Mimiru sends him mail asking him to meet at a certain place. She intends to wait there, for however long, until he appears. BT meets Crim and ask for his help in searching for the Key of the Twilight."
+		)
+	,	(".hack//SIGN",
+		 9,
+		 "Epitaph",
+		 "The investigation into the Key of the Twilight begins, and it seems they will get the help of the hacker Helba. Tsukasa encounters a player who asks him to watch her Puchiguso, (or Grunty in the English version) which turns out to be sick."
+		)
+	,	(".hack//SIGN",
+		 10,
+		 "Compensation",
+		 "Bear deals with being an inadequate father when BT tells him that may explain his concern for Tsukasa. Subaru and Tsukasa become closer while BT and Crim delve further into BBS clues concerning the Key of the Twilight."
+		)
+	,	(".hack//SIGN",
+		 11,
+		 "Party",
+		 "Mimiru and Bear decide to take on a non-fighting quest, forming a party with Tsukasa. Sora decides to come after Tsukasa."
+		)
+	,	(".hack//SIGN",
+		 12,
+		 "Entanglement",
+		 "Tsukasa is shocked to see Aura covered in vines as Morganna asks him if he really wants to return to 'that filthy world'. Bear and Subaru discuss what will happen to Tsukasa's player when and if he wakes up."
+		)
+	,	(".hack//SIGN",
+		 13,
+		 "Twilight Eye",
+		 "The players have figured out how to open the Twilight Eye, and to follow the path it reveals. BT and Crim are going in, but BT is plotting something with Sora. With the help of Helba, Bear, Mimiru, and Tsukasa are able to follow."
+		)
+	,	(".hack//SIGN",
+		 14,
+		 "Castle",
+		 "After opening the Eye, BT, and Crim find a hidden area and begin their search for the key. Bear, Mimiru and Tsukasa arrive too. They find not the Key, but a fragmented AI version of &quot;Harald Hoerwick&quot;. After speaking to him the place begins to break apart and Morganna attacks Tsukasa. After Bear falls down in the cliff, Macha orders the guardian to kill Mimiru, but instead kills Tsukasa as he sacrifices himself to save her and immediately logs out after that."
+		)
+	,	(".hack//SIGN",
+		 15,
+		 "Evidence",
+		 "An overview of the series so far using a series of flashbacks."
+		)
+	,	(".hack//SIGN",
+		 16,
+		 "Depth",
+		 "After the attack, Tsukasa suddenly logs in and screaming at the same time at Morganna's domain. He learns he cannot die in this place and begins to doubt his own existence. Tsukasa finds Subaru and she reassures him of his situation."
+		)
+	,	(".hack//SIGN",
+		 17,
+		 "Conflict",
+		 "BT asks Crim to meet outside the game and is rejected. She vents her anger on Subaru. Later, she joins some characters who want her help only to find out they have tricked her. She is saved by Tsukasa and the meaning of her name is revealed."
+		)
+	,	(".hack//SIGN",
+		 18,
+		 "Declaration",
+		 "Silver Knight foils every attempt Subaru makes to meet Tsukasa and his friends, believing that someone of her stature and responsibility should have nothing to do with an illegal character like Tsukasa. When Subaru becomes aware of this, and realizes how far they have deviated from their original purpose, she decides to disband the Crimson Knights."
+		)
+	,	(".hack//SIGN",
+		 19,
+		 "Recollection",
+		 "Subaru wanders The World, recalling her first steps in the game and how she and Crim founded the Crimson Knights. Unprotected, she is attacked by another character, and Tsukasa finds her badly beaten. They bond even closer and we see a glimpse of the real life Subaru."
+		)
+	,	(".hack//SIGN",
+		 20,
+		 "Tempest",
+		 "Tsukasa takes Subaru to Aura's domain but when they get there Aura is missing. Meanwhile, the others follow Bear's suggestion to finalize the search for the Key of the Twilight. Subaru meets Mimiru and tells her about Aura but thinks she shouldn't have left Tsukasa alone there. Morganna attacks Tsukasa and he is left lifeless on the bed."
+		)
+	,	(".hack//SIGN",
+		 21,
+		 "Despair",
+		 "Tsukasa is left catatonic by Morganna and Macha begins to feel sorry for him. Unbeatable, distorted monsters begin to appear in The World and the others decide to seek Helba's help to talk again with the AI Harald."
+		)
+	,	(".hack//SIGN",
+		 22,
+		 "Phantom",
+		 "Bear, Mimiru, and Subaru travel to another fragment of the AI Harald, encounter another unbeatable monster, but are saved by Balmung. From Harald, they learn about Aura and her connection with Tsukasa."
+		)
+	,	(".hack//SIGN",
+		 23,
+		 "The Eve",
+		 "All the characters but Tsukasa meet to pass through Helba's gate. Tsukasa is scared to go, but Crim tells him of Subaru's condition in real life. The gate appears and they enter, except for Subaru who is determined to wait for Tsukasa."
+		)
+	,	(".hack//SIGN",
+		 24,
+		 "Net Slum",
+		 "Tsukasa arrives and the party reaches Net Slum, Helba's domain. Helba reveals Morganna's plan, and Tsukasa reveals to Subaru that he is a girl in real life. The party realize they must defend Subaru from Morganna, since Morganna wants Tsukasa to be in despair. Tsukasa decides that he wants to meet Subaru in real life."
+		)
+	,	(".hack//SIGN",
+		 25,
+		 "Catastrophe",
+		 "The party retreats to the Cathedral to guard Subaru and Tsukasa. Morganna sends Sora and four guardians after them. During the battle, Tsukasa finds courage. Macha disobeys Morganna, and sacrifices herself to save Tsukasa and Subaru. Enraged, Morganna transports Tsukasa, Subaru, and Mimiru to an unknown area."
+		)
+	,	(".hack//SIGN",
+		 26,
+		 "Return",
+		 "Despite Morganna's tricks, Tsukasa, Subaru and Mimiru reach Aura. With Subaru as his strength, Tsukasa finds the courage to defy Morganna and awakens Aura, allowing him to log out. Tsukasa, now confirmed to be a girl, wakes up from her coma. Leaving the hospital, she is met by a girl in a wheelchair, who is Subaru's player."
 		)
 ;
 
 INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
-		("Title", x, y, z)
+		(".hack//SIGN", 3, 1, 2)
 ;
-		
--- General Anime Volume Data
-	,	("Title",
-		 n,
-		 m,
-		 "Classification",
-		 x,
-		 "URL"
-		)
-		
--- General Anime Episode Data
-	,	("Title",
-		 n,
-		 "Episode_Title",
-		 "Episode_Synopsys"
-		)
-		
--- General Anime Session Data
-	,	("Title", x, y, z)
+
