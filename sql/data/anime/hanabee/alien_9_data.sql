a79ac09d070ec60eb0d6b25739e34428231ee1b7
C1and00j <34433447+C1and00j@users.noreply.github.com>
2018-01-30 19:39:05 +1100

    Add: alien 9

--- sql/data/anime/hanabee/alien_9_data.sql
+++ sql/data/anime/hanabee/alien_9_data.sql
@@ -1,51 +1,50 @@
 -- Anime Data
 INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
-		("Title",
-		 n,
-		 "Synopsys",
-		 "Description",
-		 "Company",
-		 "Copyright"
+		("ALIEN 9",
+		 4,
+		 "In sixth grade Yuri just wants to end her last year in elementary school on a quiet note...not as a member of the 'Alien Party'. The objective of the 'Alien Party' is to capture any rogue aliens that wonder on to the school premises. However to capture these aliens you need the help of one, called a "Borg". A symbiotic life-form that offers the ability to protect, incapacitate and ultimately capture other aliens in compensation for being able to feed off their wastes by licking their back.",
+		 "In sixth grade Yuri just wants to end her last year in elementary school on a quiet note...not as a member of the 'Alien Party'.",
+		 "hanabee",
+		 "&copy; J.C. STAFF"
 		)
 ;
 
 INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
-		("Title",
+		("ALIEN 9",
 		 n,
 		 m,
-		 "Classification",
-		 x,
-		 "URL"
+		 "M - fantasy themes and violence",
+		 4,
+		 "https://hanabee.com.au/products/alien-9-dvd"
 		)
 ;
 
 INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SYNOPSYS) VALUES
-		("Title",
-		 n,
-		 "Episode_Title",
-		 "Episode_Synopsys"
+		("ALIEN 9",
+		 1,
+		 "9th Elementary Anti-Alien Squad",
+		 ""
+		)
+	,	("ALIEN 9",
+		 2,
+		 "Boredom, Spaceship and Overgrowth",
+		 ""
+		)
+	,	("ALIEN 9",
+		 3,
+		 "Summer Vacation, Borg and Death",
+		 ""
+		)
+	,	("ALIEN 9",
+		 4,
+		 "The End of the Beginning",
+		 ""
 		)
 ;
 
 INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
-		("Title", x, y, z)
+		("ALIEN 9", 0, 1, 1)
+	,	("ALIEN 9", 0, 2, 1)
+	,	("ALIEN 9", 0, 3, 2)
+	,	("ALIEN 9", 2, 1, 4)
 ;
-		
--- General Anime Volume Data
-	,	("Title",
-		 n,
-		 m,
-		 "Classification",
-		 x,
-		 "URL"
-		)
-		
--- General Anime Episode Data
-	,	("Title",
-		 n,
-		 "Episode_Title",
-		 "Episode_Synopsys"
-		)
-		
--- General Anime Session Data
-	,	("Title", x, y, z)
