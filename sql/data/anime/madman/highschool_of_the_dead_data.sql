86ac8b0c33ef31606085bd2a7312d3210dce2aab
C1and00j <34433447+C1and00j@users.noreply.github.com>
2018-02-04 10:40:08 +1100

    Add:highschool of the dead

--- sql/data/anime/madman/highschool_of_the_dead_data.sql
+++ sql/data/anime/madman/highschool_of_the_dead_data.sql
@@ -1,51 +1,98 @@
 -- Anime Data
 INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
-		("Title",
-		 n,
-		 "Synopsys",
-		 "Description",
-		 "Company",
-		 "Copyright"
+		("Highschool of the dead",
+		 12,
+		 "A deadly new disease is ravaging the world, turning the populace into mindless zombies, and the number of infected is skyrocketing by the second. As their fellow classmates and teachers succumb to the infection around them, a small group of students at Fujimi High School are fighting for their lives after surviving the initial outbreak. It is up to Takashi Komuro to unite the group of weary survivors and try to escape the horrors that surround them. In this new world of the living dead…will they escape? ",
+		 "A deadly new disease is ravaging the world, turning the populace into mindless zombies, and the number of infected is skyrocketing by the second.",
+		 "madman",
+		 "&copy;SATO DAISUKE - SATO SHOJI &#47; FUJIMI SHOBO &#47; H.O.T.D Project"
 		)
 ;
 
 INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
-		("Title",
-		 n,
-		 m,
-		 "Classification",
-		 x,
-		 "URL"
+		("Highschool of the dead",
+		 4,
+		 1,
+		 "MA15+ - Strong animated violence and sexual references",
+		 12,
+		 "https://www.madman.com.au/catalogue/view/15152"
 		)
+	,	("Highschool of the dead",
+		 3,
+		 1,
+		 "MA15+ - Strong animated violence and sexual references",
+		 12,
+		 "https://www.madman.com.au/catalogue/view/14227/high-school-of-the-dead-collection"
+		)
+	
 ;
 
 INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SYNOPSYS) VALUES
-		("Title",
-		 n,
-		 "Episode_Title",
-		 "Episode_Synopsys"
+		("Highschool of the dead",
+		 1,
+		 "Spring of the Dead",
+		 "At the beginning of the semester, Takashi Komuro has a social separation with his childhood friend Rei Miyamoto. When he goes outside to mope, he witnesses a zombie, known as &quot;them&quot;, attack at the front of the school. Takashi goes to warn Rei and his best friend Hisashi Igo, who is dating Rei, and escapes with them just before news of the attack spreads across the school, causing panic. The three run into Mr. Wakisaka, a teacher turned zombie, that bites Hisashi in the arm before Takashi kills it by smashing its head. They barricade themselves on the rooftop to witness Self-Defense Force helicopters flying in to combat the zombie outbreak in the city, oblivious to the carnage in the school. Hisashi begins succumbing to his injury and Takashi is forced to kill him, despite Rei's objections. Rei insults Takashi, and he almost leaves to face them alone. After she apologizes, Rei and Takashi embrace briefly. "
+		)
+	,	("Highschool of the dead",
+		 2,
+		 "Escape from the Dead",
+		 "As two other survivors, Saya Takagi and Kohta Hirano, try to avoid the zombies, Rei tries to contact her father, who tells her the city is in complete chaos before being cut off. Meanwhile, kendo club president Saeko Busujima saves the school nurse, Shizuka Marikawa, from more zombies. Kohta manages to piece together a nail gun, while Takashi and Rei manage to stop the zombies on their end. As Kohta runs out of ammunition and Saya starts to buckle under the pressure, the other survivors join them to take out the zombies. As they make plans to journey to Takashi's house to check on the safety of their families, they learn that the outbreak is much worse than they thought. "
+		)
+	,	("Highschool of the dead",
+		 3,
+		 "Democracy under the Dead",
+		 "The survivors are shocked to learn that the zombie pandemic has spread worldwide. After encountering another group of survivors, Takashi tests a theory on avoiding the zombies based on how they can only react to sound. However, when one of the survivors accidentally makes a sound, they have to fight through a horde on the way to the bus, losing some survivors in the process. When they spot another group of survivors approaching, Rei recognizes one of the teachers, Koichi Shido, and is hesitant about letting him on board. After breaking out of the school, Rei becomes defiant about Shido appointing himself as the leader and runs off the bus. As Takashi goes after her, a bus filled with zombies crashes and traps them in a tunnel, forcing them to meet up with the other survivors later. After defeating a zombie motorcyclist, Takashi and Rei find its motorcycle and head towards the city. "
+		)
+	,	("Highschool of the dead",
+		 4,
+		 "Running in the Dead",
+		 "A recapitulation of the past events are shown from Takashi's perspective. Takashi and Rei continue moving through the city, until they encounter a police car. With the occupants dead, Rei searches the vehicle and gathers an expandable baton, a revolver, and some bullets. They later stop at a gas station to refill the gas fuel tank. As Takashi goes to a cash register nearby to operate the gas pump, Rei is attacked by an insane survivor, who holds her hostage and sexually gropes her. Takashi quickly shoots him point-blank, allowing Rei to break free. With their motorcycle refueled, Takashi and Rei begin moving again as they leave the survivor to die in the hands of the incoming zombies. "
+		)
+	,	("Highschool of the dead",
+		 5,
+		 "Streets of the Dead",
+		 "At Kansai International Airport, Rika Minami and her squad from the Special Assault Team helps in the evacuation of civilians by clearing zombies at the runway. Meanwhile, after escaping from a warzone between mad survivors and zombies, Takashi and Rei learn that all of the bridges have been blocked by the police and military to prevent infected civilians from coming in. Realizing nothing good is gained by staying with Shido, it is then that Saya, Kohta, Saeko and Shizuka leaves the bus after Kohta forcefully persuades him and heads to Onbetsu Bridge, where they encounter more zombies and are rescued by the timely arrival of Takashi and Rei. Needing a place to stay for the night, Shizuka suggests they stay at the apartment where her friend Rika lives, seeing a Humvee parked outside. With zombies infesting around the building, the group heads inside and fights them. "
+		)
+	,	("Highschool of the dead",
+		 6,
+		 "In the Dead of the Night",
+		 "With the apartment cleared, the girls refresh themselves in the bath while the boys check the weapons and ammunition in the household. Meanwhile, the police at the blockade are coming under pressure from both the zombies and the civilians trying to escape them, including a group of protesters led by a conspiracy theorist who believes the epidemic is a biological weapon of the US and Japanese governments. With no help coming from headquarters, the police are ordered to use deadly force to maintain order at the blockade. Back at the apartment, Takashi has to deal with Rei and Shizuka, who are both drunk. After an argument between them, Rei starts to come onto Takashi, but he holds her back. Just then, a dog, later recognized as Zero, starts barking outside, which starts to attract the zombies. "
+		)
+	,	("Highschool of the dead",
+		 7,
+		 "Dead Night and the Dead Ruck",
+		 "While Takashi and others keep their lights low while surveying the situation, a man tries to find shelter for his daughter, Alice Maresato, but is killed by paranoid survivors. When Alice's screams attract the zombies, Takashi decides to go to rescue her while Kohta provides sniper support. Meanwhile, the girls, deciding they can no longer stay at the apartment, prepare to load up to move on. With the streets filled with zombies, Takashi, along with Alice and Zero, carefully crosses the top of the fence until the others arrive to help him. Once reunited, they all decide to head towards the other side of the river. "
+		)
+	,	("Highschool of the dead",
+		 8,
+		 "The Dead Way Home",
+		 "The zombies manage to infiltrate Air Force One. With the Chairman and the President being infected, The Chairman begs the President to launch their ICBMs in his belief that the other countries will launch theirs at the US, as he is finished off before turning into a zombie. Meanwhile, Takashi's group reaches the other side of the river, void of any civilians or zombies. After changing and getting familiar with new weapons, they decide to head towards Saya's house first since it is nearby. They start to find more zombies as they approach their destination, and an unexpected wire barrier causes Rei to fall off the Humvee. The others have to deal with unfamiliar weapons and their own courage in order to fend off the zombies. Outnumbered, Takashi and Saeko try to lead them away, but they are still drawn towards the direction of the others. As things look bad, they are assisted by a group of firefighters led by Saya's mother, Yuriko Takagi, while Takashi and Saeko take a different route. "
+		)
+	,	("Highschool of the dead",
+		 9,
+		 "The Sword and Dead",
+		 "Takashi and Saeko acquire an amphibious vehicle to help evade the zombies and set up on a sand bank in the middle of the river. Later, they ditch their vehicle in a park fountain to lure the zombies while Saeko attacks them. However, Saeko freezes when she comes across some zombie children, forcing Takashi to help her. They decide to camp out at a shrine, where Saeko reveals that she almost killed a rapist four years ago yet enjoyed it, saying she has not changed since that day and does not deserve love. Takashi responds by kissing her however. The next morning, as zombies start to surround them, Saeko, hating the sadistic side of herself, gives in and refuses to fight back. Takashi tells her that he has always admired her, giving her the motivation to defeat the zombies with a real sword. Accepting their dark sides, Takashi and Saeko safely arrive at Saya's house. "
+		)
+	,	("Highschool of the dead",
+		 10,
+		 "The Dead's House Rules",
+		 "Having spent a day at Saya's mansion which has been heavily fortified, the group needs to decide if they should stick with her family and staff and be treated as kids or go off on their own and retain their independence. Saya is mad at her father for presuming she was dead during the outbreak while taking action to safe guard the neighborhood, but Takashi snaps at her since her parents are proven to be alive, which calms her down. Saya's father, Soichiro Takagi, a Japanese right-wing nationalist politician, arrives and executes his zombie friend in front of the survivors at the mansion to show them their current situation. Kohta, not wanting to lose his guns and become useless, refuses to give up his guns to Soichiro's men. When Soichiro arrives to question Kohta, the others come to his defense. Meanwhile, one of the students who stayed with Shido reports the situation at the mansion to Shido himself, who is presiding over an orgy between the students. "
+		)
+	,	("Highschool of the dead",
+		 11,
+		 "Dead Storm Rising",
+		 "Saya tries to calm down a group of survivors who refuse to accept their current situation. Takashi is appointed the leader by the others, much to his surprise. Soichiro later gives Saeko a prized katana as thanks for protecting his daughter and gratitude to Saeko's father for teaching him. As the group makes plans to go and rescue their remaining families, Shido and his followers arrive at the mansion, to which an angry Rei charges up to him with her bayonet. Rei reveals she hates Shido because his father, a corrupt politician, asked him to force Rei to repeat a year as revenge against her father, who was investigating him for corruption. Soichiro arrives and tells Rei that Shido's fate is in her hands, but Rei decides he is not worth killing which Soichiro has Shido and his followers leave as they have become corrupt. Elsewhere, a US submarine has been given orders by the succeeding President to launch a controversial preemptive nuclear strike at China, North Korea, and Russia, while the crew at the International Space Station watches in horror as both the US and Russia launch their ICBMs at each other. "
+		)
+	,	("Highschool of the dead",
+		 12,
+		 "All Dead's Attack",
+		 "The US and Japanese naval forces team up to shoot down some of the retaliatory missiles. However, one missile manages to penetrate through the joined US and Japanese air defenses, exploding somewhere near Japan. Due to the EMP burst from the nuclear explosion, machines and electronics near the blast break down which causes Shido's bus to crash and destroy one of the barricades. With a giant horde of zombies coming to the mansion, Soichiro orders the main gate to be closed but the overwhelming zombies breach the gate, causing panic among the survivors. With the mansion no longer safe, the Takagis order their followers and the remaining survivors to fight and head to another safe area that has yet to be breached. The Takagis leave Saya's safety to Takashi and the group as they fight to their Humvee which was not affected by the EMP. With their ride fixed, the group leaves the mansion while the survivors fight off the horde. Despite the situation, the group remain optimistic as they finally arrive at a mall by foot. "
 		)
 ;
 
 INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
-		("Title", x, y, z)
+		("Highschool of the dead", 0, 1, 4)
+	,	("Highschool of the dead", 0, 2, 4)
+	,	("Highschool of the dead", 0, 3, 4)
+	,	("Highschool of the dead", 3, 1, 2)
 ;
-		
--- General Anime Volume Data
-	,	("Title",
-		 n,
-		 m,
-		 "Classification",
-		 x,
-		 "URL"
-		)
-		
--- General Anime Episode Data
-	,	("Title",
-		 n,
-		 "Episode_Title",
-		 "Episode_Synopsys"
-		)
-		
--- General Anime Session Data
-	,	("Title", x, y, z)
