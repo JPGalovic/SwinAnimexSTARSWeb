e0c6fa109afd654eb854ffdf58f0706ceef5cd41
C1and00j <34433447+C1and00j@users.noreply.github.com>
2018-02-06 07:45:07 +1100

    add:gangsta

--- sql/data/anime/madman/gansta_data.sql
+++ sql/data/anime/madman/gansta_data.sql
@@ -1,51 +1,97 @@
 -- Anime Data
 INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
-		("Title",
-		 n,
-		 "Synopsys",
-		 "Description",
-		 "Company",
-		 "Copyright"
+		("Gangsta",
+		 12,
+		 "In the city of Ergastulum, a shady ville filled with made men and petty thieves, whores on the make and cops on the take, there are some deeds too dirty for even its jaded inhabitants to touch. Enter the &quot;Handymen,&quot; Nic and Worick, who take care of the jobs no one else will handle. Until the day when a cop they know on the force requests their help in taking down a new gang muscling in on the territory of a top Mafia family. It seems like business &#40;and mayhem&#41 as usual, but the Handymen are about to find that this job is a lot more than they bargained for.",
+		 "In the city of Ergastulum, a shady ville filled with made men and petty thieves, whores on the make and cops on the take, there are some deeds too dirty for even its jaded inhabitants to touch.",
+		 "madman",
+		 "&copy;Kohske/SHINCHOSHA, GANGSTA. COMMITTEE"
 		)
 ;
 
 INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
-		("Title",
-		 n,
-		 m,
-		 "Classification",
-		 x,
-		 "URL"
+		("Gangsta",
+		 4,
+		 1,
+		 "MA15 - Strong animated violence",
+		 12,
+		 "https://www.madman.com.au/catalogue/view/39078"
+		)
+	,	("Gangsta",
+		 3,
+		 1,
+		 "MA15 - Strong animated violence",
+		 12,
+		 "https://www.madman.com.au/catalogue/view/34331/gangsta-complete-season-1"
 		)
 ;
 
 INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SYNOPSYS) VALUES
-		("Title",
-		 n,
-		 "Episode_Title",
-		 "Episode_Synopsys"
+		("Gangsta",
+		 1,
+		 "Naughty Boys",
+		 "The episode opens with Nic watching as Alex is berated by Barry, her boss, for not earning enough money. Later, while Nic and Worick are dealing with some thugs, they see her being abused by an irate customer. As she walks away, one of them &#40later revealed to be Nic&#41; tosses her a handkerchief to wipe blood off her face. The two Handymen then meet up with the police officer Chad, who hires them to eliminate Barry's gang, which has been encroaching on mafia turf. Before they leave, Nic asks if they can take a trophy. Worick then meets up with Alex, who asks him to thank Nic for the handkerchief, and Worick tells her that they have been hired to kill her boss. Nic is then seen attacking the gang, and Worick shows up in time to finish off Barry. Alex shows up and takes Worick's gun, firing several shots into Barry's body in a fit of despair as she's relieved to no longer be under his control but is now rudderless. Speaking with Chad afterward, Worick reveals that they have chosen Alex as their trophy. Chad protests, but Nic cows him into acceptance with a display of his superhuman strength. At the end of the episode, Worick asks Alex if she can answer their phone for them while he is out. "
+		)
+	,	("Gangsta",
+		 2,
+		 "Hundemarken",
+		 "Nina arrives at the Handymen's office to deliver Nic's medicine. She tells them that Dr. Theo is being harassed by a &quot;Tag&quot;, one of the people like Nic. He and Nina leave for the clinic, but Worick spots some suspicious characters and stays behind. Using Alex as a distraction, he manages to dispose of them. Meanwhile, the Tag and some other men are pressuring Dr. Theo to join their organization. They begin to threaten Nina, and are surprised when she arrives unharmed with Nic. Nic and the Tag then begin to fight, with Nic claiming that he can take him down in three minutes. Worick begins to explain the concept of Tags to Alex, but they are interrupted by Nic's fight as he defeats his rival. Afterward, Chad arrives, and berates them for the problems they have caused. Alex asks Nina if she is afraid of Nic, and Nina says she is not: She says Alex is because she doesn't know about Twilights. Elsewhere, the wounded Twilight that Nic fought is killed by an unknown person for failing to complete his job. "
+		)
+	,	("Gangsta",
+		 3,
+		 "Ergastulum",
+		 "Worick wakes up from a dream of Nic killing his father. Since Worick has to work his other job, Alex and Nic leave to make deliveries for Dr. Theo. Nic explains to Alex that Worick is a gigolo. While making a delivery to Joel, Alex sees a newspaper featuring an article about the killing of the Arcangelo family twenty-two years ago and the kidnapping of their youngest son. Worick then joins them after finishing with his customer. They make a delivery to Monroe, who asks Nic to come back to the mafia, but Nic refuses. Next they deliver a batch of Twilight drugs to the madam of the brothel where Worick used to work. Before they leave, Alex sees Nic in a room with a mysterious woman. The next day, Worick and Alex talk about his past and his job as a gigolo. The episode ends with both Nic and Worick having a flashback to their childhood. "
+		)
+	,	("Gangsta",
+		 4,
+		 "Nonconformist",
+		 "Chad summons Nic and Worick to the police station to help identify a pile of mangled bodies found in District 6. Worick identifies them using his photographic memory. Chad then receives a call from Monroe, who asks him to send the Handymen over to help in a gang war he is involved in. Monroe and his men are then attacked by a knife-wielding Twilight of the same rank as Nic, who kills many of them. Nic arrives to fight the Twilight, and Monroe asks his men if they want to place bets on who will win. Worick and Nic's backstory is interspersed with the events of the episode, showing Nic being hired as a mercenary bodyguard for Worick, who is at first outraged that his bodyguard is disabled while the rest of his family's bodyguards are not. However, he soon comes to empathize with Nic. Meanwhile, Alex hallucinates Barry's bloody and animate corpse when faced with the dilemma of staying with the Handymen or living on her own. "
+		)
+	,	("Gangsta",
+		 5,
+		 "Sanctions",
+		 "The episode begins with a flashback to young Worick with a client, juxtaposed with a newscaster talking about the state of affairs in Ergastulum and the Three Laws which are about to take effect. Worick runs back to Nic to share his earnings and to tell him about a café that serves Twilights where they can eat, but is stunned to find Nic crouched and bleeding in an alley. Chad tends to Nic's wounds. In the present, Ginger, assistant to Gina Paulklee of the Paulklee Guild, reports to her boss that Doug has taken a job outside of the Guild's jurisdiction. Worick interrogates a mercenary behind the attack on Monroe. Doug meets his match in Nic, who has overdosed on Celebrer to dull pain. Monroe recalls an attack by a C/3 Twilight (possibly a young Nic) who destroyed his estate and decimated his staff. Worick reports that the Corsicans are making a move. Delico, Monroe's D/0 Twilight, saves Monroe from Doug. Worick tries to stop Nic to keep him from overextending himself, but before he can, Ginger and Gina show up. Gina explains to Nic that the Three Laws have to be observed or Ergastulum will fall apart. She shoots Nic and Doug with Celebrer &quot;downers&quot;. Alex, who was studying the sign language book and watching the rain, sees a shadowy figure, which is revealed to be Barry. The sound of heavy footsteps on the stairs has her cowering on the floor wondering how it could be that he survived being shot. Worick drags Nic and Doug to Dr. Theo's clinic and calls Alex, but she fails to answer. "
+		)
+	,	("Gangsta",
+		 6,
+		 "Thorn",
+		 "While interrogating a henchman, Worick learns that Uranos Corsica doesn't want to kill Monroe; he only wants access to the Celebrer-supplying Cristiano Family that he protects. Monroe holds a funeral for the men killed by Doug. Meanwhile, Nina and Worick discuss Nic's injuries as he lies unconscious at the clinic. When Nic awakens, Worick tells him that the body dump discovered in episode four were all Twilights; one of them was the Tag that Nic had fought in episode two. When Worick returns to the office, he finds Dr. Theo there; Theo tells him that Alex is likely suffering from withdrawal from drugs given to her by Barry, which have caused her hallucinations. Searching for Alex, Worick finds her in the alley, where she fails to recognize him and mistakes him for a customer. When she continues to hallucinate Barry, Worick head-butts her to bring her to her senses. In a flashback to their childhood, Worick begins to teach Nic to write. When he asks how Nic got a bruise on his arm, Nic replies that he fell down. Later, Nic and the other mercenaries are dispensed to put down anti-Twilight riots outside Ergastulum. While retrieving the sign language book that Worick gave him, Nic is almost hit by a hand grenade. He returns the damaged book to Worick. Noticing an injury that Worick's father gave him, Nic asks what happened, and Worick says that he fell down. "
+		)
+	,	("Gangsta",
+		 7,
+		 "Birth",
+		 "Worick wakes up from a dream about the time that he learned that Nic is a Twilight, and Alex comforts him. Meanwhile, Chad arrives at Theo's clinic and informs Doug that he is in trouble with the Guild. Theo tells Chad that the Twilight bodies found in District 6 were likely killed by other Twilights. The Four Fathers gather to discuss the recent string of Twilight killings, including another batch of bodies that has recently been discovered. Loretta is suspicious of Uranos Corsica, but he vehemently denies his involvement, despite expressing pleasure at the outcome. In a flashback, Worick reveals to Nic that he has purchased Nic's contract from Gaston Brown. Worick's father finds him smoking, and presses a lit cigarette into his left eye. Nic sees this and, outraged, kills Worick's father and the rest of the family. Nic is about to kill himself too, but Worick forcibly stops him, telling him that he will have to suffer for what he did before he lets him die. In the present, Worick and Delico meet, both being excluded from the Four Fathers meeting, and Worick contemplates that being killed in the Twilight hunt wouldn't be a bad way for Nic to die. In the end of the episode, Nic and Alex pass the two Twilight killers in an alleyway, and Nic, seemingly recognizing them, grins. "
+		)
+	,	("Gangsta",
+		 8,
+		 "Evening Dress",
+		 "The episode begins with the handymen performing repairs on Constance Raveau's shop after a shoot-out, and Worick invites Alex to a soiree hosted by the Cristianos. While talking with Constance, Alex suddenly remembers that she has a younger brother who she had forgotten, and wonders how that could be. She then begins to hallucinate Barry, before being calmed by Nic. Meanwhile, an unknown man arrives at the East Gate of Ergastulum. The Handymen arrive at the Cristiano nightclub, where they are greeted by Loretta and her two bodyguards, Marco Adriano and Galahad Woeher. As the party gets under way, Nic and Worick leave to help Twilights who are being chased by anti-Twilight rioters. The party is revealed to be an attempt to gather the Twilights together in order to protect them. Meanwhile, Galahad and Loretta coerce Alex into singing for the gathered Twilights. The mysterious man hears her singing, but is turned away at the door. As the song ends, Nic and Worick successfully escort the last of the Twilights to safety. Meanwhile, the two Twilight killers from the previous episode prepare to take action. "
+		)
+	,	("Gangsta",
+		 9,
+		 "Siblings",
+		 "Ivan Glaziev, one of Monroe's henchmen and secretly an anti-Twilight sympathizer, blackmails one of the Twilights at the party into attacking Loretta by holding his child hostage. When she attempts to question the Twilight, he is killed by Mikhail, one of the two Twilight killers, who arrive and begin slaughtering the guests. Galahad and Marco engage Mikhail, and manage to defeat him before the other Twilight killer, Erica, arrives and turns the tables. Meanwhile, Worick fires a signal flare to summon help. Erica threatens Loretta, but Nic arrives just in time. While fighting her, he realizes that she is Delico's sister. While talking with Alex, Galahad reveals that Nic is what is known as a &quot;faker&quot;, a Twilight who overdoses on Celebrer to attain a level higher than they would naturally have. As Erica is about to kill Nic, she is stopped by Ginger from the Paulklee Guild, who was brought by Worick's signal. Erica and Mikhail, realizing they are outclassed, decide to retreat. They meet up with Ivan and Uranos Corsica, who is revealed to be part of the group behind the Twilight killings, Esminets. As the episode ends, Corsica welcomes the mysterious man from the East Gate into their ranks, and he is revealed to be Alex's younger brother, Emilio. "
+		)
+	,	("Gangsta",
+		 10,
+		 "Land of Confusion",
+		 NULL
+		)
+	,	("Gangsta",
+		 11,
+		 "Absence",
+		 NULL
+		)
+	,	("Gangsta",
+		 12,
+		 "Odds and Ends",
+		 NULL
 		)
 ;
 
 INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
-		("Title", x, y, z)
+		("Gangsta", 0, 1, 4)
+	,	("Gangsta", 0, 2, 4)
+	,	("Gangsta", 0, 3, 4)
+	,	("Gangsta", 3, 1, 2)
 ;
-		
--- General Anime Volume Data
-	,	("Title",
-		 n,
-		 m,
-		 "Classification",
-		 x,
-		 "URL"
-		)
-		
--- General Anime Episode Data
-	,	("Title",
-		 n,
-		 "Episode_Title",
-		 "Episode_Synopsys"
-		)
-		
--- General Anime Session Data
-	,	("Title", x, y, z)
