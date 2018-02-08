07edc0917c1a508f99d6deaac36fc1c70ab1bbe2
C1and00j <34433447+C1and00j@users.noreply.github.com>
2018-02-08 16:28:36 +1100

    Add: berserk, classification Fix: file name

---
+++ sql/data/anime/madman/berserk_data.sql
@@ -0,0 +1,169 @@
+-- Anime Data
+INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
+		("Berserk",
+		 25,
+		 "Experience all twenty-five episodes of the Berserk Series in a collector’s box set! In the castle town of Midland, a new king has come to power through treachery and violence. His agents victimize the citizens, until one night when a soldier known as the Black Swordsman destroys them. Yet, between the strength of a young mercenary named Guts and the charismatic Captain Griffith, their country will never be the same.",
+		 "Experience all twenty-five episodes of the Berserk Series in a collector’s box set! In the castle town of Midland, a new king has come to power through treachery and violence.",
+		 "Madman",
+		 "&copy;NTV / VAP"
+		)
+;
+
+INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
+		("Berserk",
+		 4,
+		 1,
+		 "MA15+ - Strong animated violence and adult theme",
+		 25,
+		 "https://www.madman.com.au/catalogue/view/37380/berserk-complete-series-bluray-limited-edition"
+		)
+	,	("Berserk",
+		 3,
+		 1,
+		 "MA15+ - Strong animated violence and adult theme",
+		 25,
+		 "https://www.madman.com.au/catalogue/view/3213/berserk-collection"
+		)
+	,	("Berserk",
+		 0,
+		 1,
+		 "MA15+ - Strong animated violence and adult theme",
+		 25,
+		 "https://www.animelab.com/shows/berserk"
+		)
+;
+
+INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SYNOPSYS) VALUES
+		("Berserk",
+		 1,
+		 "The Black Swordsman",
+		 "A girl is being assaulted in a bar when Guts walks in. He kills all the assailants but one and orders him to tell his master, the Baron, that the Black Swordsman is coming. Guts later finds and kills the Baron, and removes a demonic relic called a Behelit from around his neck. Soon after, Guts remembers the events in his past of when he fought the knight Bazuso. "
+		)
+	,	("Berserk",
+		 2,
+		 "Band of the Hawk",
+		 "In a flashback, Guts remembers when as a mercenary he killed the knight Bazuso. Later he encounters the group who would change his destiny: the Band of the Hawk. Corkus attempts to rob Guts and Griffith sends Casca to help him, but Guts defeats her. Guts then challenges Griffith, who grievously wounds him. Griffith orders Casca to lie with Guts during his convalescence. When Guts recovers days later, Griffith asks the swordsman to join his group but again he challenges Griffith to a sword fight. "
+		)
+	,	("Berserk",
+		 3,
+		 "First Battle",
+		 "Griffith defeats Guts, and makes him a member of the Band of the Hawk – an alliance between the philosopher and the berserker. The Hawks go on a midnight raid and Griffith chooses Guts for the most important position of rear guard; Guts succeeds and the band hails him as a hero. "
+		)
+	,	("Berserk",
+		 4,
+		 "The Hand of God",
+		 "Guts settles in with the Band of the Hawk, and Griffith shows him his Behelit. Guts remembers his childhood, being raised by his adoptive father and mentor Gambino. The young Guts was forced to kill Gambino when he tried to kill the boy he blamed for his wife's death. Three years pass and the Band of the Hawk grow in power and numbers. "
+		)
+	,	("Berserk",
+		 5,
+		 "A Wind of Swords",
+		 "Guts is now a commander in Griffith's army. Guts is taking risks in battle, and although he succeeds, Casca takes him to task for it. Eventually, Griffith is knighted as a viscount in the Midland court to the nobles' dismay. Guts saves the Hawks in battle, to Casca's chagrin. "
+		)
+	,	("Berserk",
+		 6,
+		 "Zodd the Immortal",
+		 "Guts leads his men to raid a castle under Chuder occupation, but his men are wiped out by Nosferatu Zodd the Immortal, a fearsome giant warrior. Guts enters the castle keep and engages Zodd in battle. When Guts manages to stab him, Zodd transforms into his 300-year-old demon form. Zodd nearly kills Guts before Griffith and a group of Hawks comes to his aid, but they are ineffective. When Zodd is about to kill Griffith he sees the Behelit and ceases his attack. He then sprouts wings and flies off, telling Guts that should Griffith's dream die, it will be his doom."
+		)
+	,	("Berserk",
+		 7,
+		 "The Sword's Owner",
+		 "Though he and Guts are still injured from their encounter with Nosferatu Zodd, Griffith uses it to get closer to the King and his daughter Charlotte. Guts and some of the Hawks feel that Griffith is pulling away from them."
+		)
+	,	("Berserk",
+		 8,
+		 "Conspiracy",
+		 "Griffith and Guts continue to have success in battle and Griffith is made a count. The king's brother, General Julius, is furious about this as he and Minister Foss plot Griffith's murder with a poison arrow during an upcoming hunt. "
+		)
+	,	("Berserk",
+		 9,
+		 "Assassination",
+		 "During the hunt, Griffith and Charlotte grow close before the former is shot in the chest with a poisoned arrow. Luckily, Griffith's Behelit takes the arrow and he survives unscathed. Griffith deduces Julius to be the culprit and recruits Guts to secretly kill the noble. "
+		)
+	,	("Berserk",
+		 10,
+		 "Noble Man",
+		 "Guts assassinates Julius, but is emotionally scarred when forced to kill Julius' son Adonis to conceal the deed. Later, he hears Griffith tell Charlotte that he would consider a true friend to be his equal, someone with his own dreams and who follows his own path. Guts realises that although he is close to Griffith, he is not his friend. "
+		)
+	,	("Berserk",
+		 11,
+		 "The Battle",
+		 "Griffith and the Hawks lead the march into battle against the Blue Whale Knights. Casca has a fever, and after fighting with Adon Coborlwitz she falls unconscious off a cliff, Guts goes after her. They are washed downriver, and Guts finds a cave where he must keep Casca warm with his body. "
+		)
+	,	("Berserk",
+		 12,
+		 "Together",
+		 "Guts tends to Casca but she still resents him. She tells her story of how Griffith helped her save herself from being raped, and give her a choice in life so she followed him. She watched him pragmatically go with a pederast lord to earn money to create his army and fulfill his dream. When Guts came, she knew that she would not be Griffith's chosen friend."
+		)
+	,	("Berserk",
+		 13,
+		 "Prepared for Death",
+		 "Adon Coborlwitz and his Blue Whale Knights find Guts and Casca. Though completely outnumbered, Guts tells Casca to run and stays to fight. Some men go after Casca, catching her and holding her down in order to rape her. Guts continues fighting. "
+		)
+	,	("Berserk",
+		 14,
+		 "Bonfire of Dreams",
+		 "Casca defeats her attackers, and she and Guts end up back at the camp. Guts and Casca share a tender moment, where Guts explains that everyone in the camp has a dream but all he has is his sword, causing Casca to think he's leaving. "
+		)
+	,	("Berserk",
+		 15,
+		 "The Decisive Battle",
+		 "Griffith volunteers the Hawks to reclaim the impregnable Fortress of Doldrey from a Chuder governor named Gennon and the Purple Rhino Knights in his service. Guts learns that Griffith's unsavory history was with Gennon when he was a noble. The Hawks begin their assault before pulling back. Their boats are burned and there is nowhere to retreat to as the Chuder army leaves the fortress in pursuit. "
+		)
+	,	("Berserk",
+		 16,
+		 "The Conqueror",
+		 "The retreat was a ruse and Casca takes the fortress, defeating Adon and the few remaining guards. Guts fights with the Rhino Knights' command Boscogn before his sword breaks, only to gain a sword that was provided by Zodd from a great distance. Guts uses the sword to kill Boscogn while Griffith kills a wounded Gennon to assure the Hawks' victory. "
+		)
+	,	("Berserk",
+		 17,
+		 "Moment of Glory",
+		 "The Hawks' reclaiming of Doldrey marks the end of the Hundred-Year War between Midland and Chuder. The King holds a ball for the mercenaries in their honor. The Queen, Charlotte's stepmother and Julius's secret lover, hatches a scheme with Foss to poison Griffith during the banquet. However, before the event, Foss receives a disturbing letter. During the toast, Griffith falls to the floor. "
+		)
+	,	("Berserk",
+		 18,
+		 "Tombstone of Flames",
+		 "The Queen and the conspirators believe they succeeded in poisoning Griffith and that he is dead. However, while they are celebrating, their room is locked and the tower is engulfed in flames. Griffith is still alive and watches them burn, having forced Foss to aid him by kidnapping his daughter. Griffith has again used Guts to remove his competition. Guts decides to leave and tells a pleading Casca that he does not want to be caught up in Griffith's dream anymore. "
+		)
+	,	("Berserk",
+		 19,
+		 "Separation",
+		 "As Guts leaves, he is challenged by Griffith and manages to defeat him and earn his freedom. However, Guts' departure affects Griffith psychologically and he spends a night of passion with Charlotte in a lapse of judgement. When Griffith leaves the next morning, he is apprehended and arrested by the guards. "
+		)
+	,	("Berserk",
+		 20,
+		 "Sparks",
+		 "A year has passed and Guts is living with Godo, a swordsmith, and his granddaughter Erica. Guts learns that the Band of the Hawk are now wanted outlaws and Casca is their captain. Guts goes to them and finds them battling foreign mercenaries. "
+		)
+	,	("Berserk",
+		 21,
+		 "Confession",
+		 "Guts helps the Hawks drive off the mercenaries. Casca has a plan to rescue Griffith from the Tower of Rebirth where he is being held. Casca blames Guts for Griffith's problems, but realizes it is not his fault. They make love and Guts asks her to go away with him after the rescue. "
+		)
+	,	("Berserk",
+		 22,
+		 "Infiltration",
+		 "Casca and Guts go into the dungeon under the Tower of Rebirth to rescue Griffith, finding him mute, crippled and horribly disfigured from a year of seemingly endless torture. Meanwhile, the members of the Hawk are slaughtered at their camp by monsters led by a green fairy. "
+		)
+	,	("Berserk",
+		 23,
+		 "Eve of the Feast",
+		 "Back at the camp, everyone is forlorn because of Griffith's terrible condition. The Raiders ask Guts to take them along with him. Casca feels she must take care of Griffith, and tells Guts to leave. Delirious and horrified at what he has been reduced to, Griffith takes off in a wagon and crashes into the river. He finds his Behelit and unintentionally activates it, taking everyone present to the Astral Realm. "
+		)
+	,	("Berserk",
+		 24,
+		 "The Eclipse",
+		 "The four archdemons of the God Hand awake and inform Griffith that he has been chosen to be their final member. For his baptism, Griffith must offer his friends as a sacrifice to the God Hand's Apostles. Griffith is shown visions of his past to convince him that the lives of his men are trivial in regard to the fulfillment of his dream. Guts climbs a mountain of flesh to try and stop Griffith from becoming a demon, but Griffith accepts the God Hand's offer. The entire Band of Hawk are branded as sacrificial offerings for the unborn child of darkness, Griffith. "
+		)
+	,	("Berserk",
+		 25,
+		 "Time of Eternity",
+		 "The Apostles assume their true forms and commence the Eclipse feasting by devouring those branded as offerings. Griffith is reborn as the fifth God Hand, Femto, and sexually violates Casca in front of Guts. Guts is forced to cut off his left forearm to stop his former friend while his right eye is gouged out. A post-credits scene shows Guts in the physical world with his wounds healed as he leaves Godo's house to exact his revenge on Griffith and the Apostles ending the series on a cliffhanger. "
+		)
+;
+
+INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
+		("Berserk", 0, 1, 8)
+	,	("Berserk", 0, 2, 8)
+	,	("Berserk", 0, 3, 9)
+	,	("Berserk", 3, 1, 2)
+;
