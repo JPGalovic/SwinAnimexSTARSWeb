acf2d4c165d5e5db43786f3a88a09eb8061d1818
James P. Galovic <jpgalovic@gmail.com>
2018-02-12 15:28:13 +1100

    Add: Magic the Gathering Data

--- sql/data/game/game_company/game_company.sql
+++ sql/data/game/game_company/game_company.sql
@@ -1,4 +1,5 @@
 -- Game Company Data, Version 1.0, JAN18, JPGalovic
 INSERT INTO COMPANY (COMPANY_NAME, COMPANY_URL, IS_SPONSOR) VALUES
 		("Final Form Games", "http://www.finalformgames.com", FALSE)
+	,	("Wizards of the Coast", "http://company.wizards.com", FALSE)
 ;
---
+++ sql/data/game/magic_the_gathering_data.sql
@@ -0,0 +1,18 @@
+-- Magic: The Gathering Data, Version 1.0, FEB18, JPGalovic
+INSERT INTO GAME (GAME_TITLE, GAME_DESCIRPTION, MIN_NUMBER_OF_PLAYERS, MAX_NUMBER_OF_PLAYERS, RECOMMENDED_NUMBER_OF_PLAYERS, COMPANY_NAME, CLASSIFICATION) VALUES
+		("Magic: The Gathering",
+		 "Expansive worlds, explosive spells and extraordinary creatures await you. Magic is driven by riveting stories, populated with Planeswalkers - powerful mages with the ability to travel between worlds. Discover the many tales of Magic.",
+		 2,
+		 4,
+		 2,
+		 "Wizards of the Coast",
+		 NULL
+		)
+;
+
+INSERT INTO GAME_PLATFORM (GAME_TITLE, PLATFORM_ID) VALUES
+		("Magic: The Gathering",
+		 11
+		)
+;
+
