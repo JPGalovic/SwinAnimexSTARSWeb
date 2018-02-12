2910a9c5935e7c854a5e5e40ea775a6b2eec05d3
Thatretroaussie <Thatretroaussie@github.com>
2018-02-12 19:28:03 +1100

    Add Aquarian lagos data.
    to finnish

--- sql/data/anime/madman/aquarion_lagos_data.sql
+++ sql/data/anime/madman/aquarion_lagos_data.sql
@@ -1,48 +1,169 @@
 -- Anime Data
 INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
-		("Title",
-		 n,
-		 "Synopsys",
-		 "Description",
-		 "Company",
-		 "Copyright"
+		("Aquarion Logos",
+		 26,
+		 "Celebrating its 10th anniversary, Aquarion returns with a new series, new characters and a brand new adventure! Inspired by the classic mecha series of the ‘70s and ‘80s, Aquarion Logos is ready to serve up more high-flying robotic battles animated by Satelight, the same team that brought you Log Horizon, Fairy Tail and the first two installments in the Aquarion franchise.",
+		 "Celebrating its 10th anniversary, Aquarion returns with a new series, new characters and a brand new adventure! Inspired by the classic mecha series of the ‘70s and ‘80s, ",
+		 "Madman",
+		 "&copy;2015 SHOJI KAWAMORI,SATELIGHT/Project AQUARION LOGOS"
 		)
 ;
 
 INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
-		("Title",
-		 n,
-		 m,
-		 "Classification",
-		 x,
-		 "URL"
+		("Aquarion Logos",
+		 3,
+		 1,
+		 "Animated violence and sexual references",
+		 26,
+		 "https://www.madman.com.au/catalogue/view/34310"
 		)
 ;
 
 INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SYNOPSYS) VALUES
-		("Title",
-		 n,
+		("Aquarion Logos",
+		 1,
 		 "Episode_Title",
 		 "Episode_Synopsys"
 		)
 ;
 
 INSERT INTO ANIME_SESSION(ANIME_TITLE, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
-		("Title", x, y, z)
+		("Aquarion Logos ", x, y, z)
 ;
 		
 -- General Anime Volume Data
-	,	("Title",
-		 n,
-		 m,
+	,	("Aquarion Logos",
+		 3,
+		 1,
 		 "Classification",
-		 x,
-		 "URL"
+		 26,
+		 "https://www.madman.com.au/catalogue/view/34310"
 		)
+
 		
 -- General Anime Episode Data
-	,	("Title",
-		 n,
+	,	("Aquarion Logos",
+		 1,
+		 "Shout! Sousei Gattai!",
+		 "Aquarion Logos is ready to serve up more high-flying robotic battles animated by Satelight"
+		)
+	,	("Aquarion Logos",
+		 2,
+		 "Shine! Light of Thought",
+		 "Feeling guilty for risking the lives of her young squadron, Sakurako moves to infiltrate Nesta herself, only to find herself face to face with Subete. She soon learns that her past ties to the shadowy organization may not be fully severed. "
+		)
+	,	("Aquarion Logos",
+		 3,
+		 "Resound! A Dream for Me Alone",
+		 "A young, new member joins the Verbalism Club with high hopes, but something is amiss! "
+		)
+	,	("Aquarion Logos",
+		 4,
+		 "Let Fly! an Arrow That Pierces the Heart",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 5,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 6,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 8,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 9,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 10,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 11,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 12,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 13,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 14,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 15,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 16,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 17,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 18,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 19,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 20,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 21,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 22,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 23,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 24,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 25,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Aquarion Logos",
+		 26,
 		 "Episode_Title",
 		 "Episode_Synopsys"
 		)
