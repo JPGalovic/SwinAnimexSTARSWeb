88391b18c00c214e65ef08eb728a8f1ac9105db8
James P. Galovic <jpgalovic@gmail.com>
2018-02-13 07:17:47 +1100

    Change: event_card_location function now does both on and off campus events (based on data for event)

--- code_gen/module_elements/event_cards.php
+++ code_gen/module_elements/event_cards.php
@@ -1,13 +1,26 @@
 <?php
 	// Functions for Common elements of Event Cards, Version 1.0, FEB18, JPGalovic
-	function event_card_location($campus, $room)
+
+	// Prints out location based on values of received data.	
+	function event_card_location($campus, $room, $address, $lat, $lng)
 	{
-		// Event Location
-		echo('<p>'.$event_row['CAMPUS']);
-		if($event_row['ROOM'] != NULL)
-			echo(' - '.$event_row['ROOM'].'</p>');
+		// 1. On Campus Event
+		// [CAMPUS]
+		// [CAMPUS] - [ROOM]
+		if($campus != NULL)
+		{
+			echo('<p>'.$campus);
+			if($room != NULL)
+				echo(' - '.$room.'</p>');
+			else
+				echo('</p>');
+		}
+		// 2. Off Campus Event
+		// [ADDRESS]
 		else
-			echo('</p>');
+		{
+			echo($address);
+		}
 	}
 
 ?>
