7b32acb93b7141e25f9a2f49f04d85e507bcc967
Thatretroaussie <Thatretroaussie@github.com>
2018-02-13 23:09:12 +1100

    No-Rin episode data
    Added sql data for no-rin

---
+++ sql/data/anime/madman/No-Rin.sql
@@ -0,0 +1,110 @@
+-- Anime Data
+INSERT INTO ANIME(ANIME_title, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
+		("No-Rin",
+		 12,
+		 "The mood is right for falling in love with a superstar in the new series from the director of the Negima OVAs! Teenager Hata Kousaku is crushed when his favorite singing idol, Kusakabe Yuka, stuns the world by retiring from show business. Kousaku’s friends try to cheer him up, but nothing can chase away his gloomy mood until Yuka herself transfers into his class at Tamo Agriculture School&#33;",
+		 "Teenager Hata Kousaku is crushed when his favorite singing idol, Kusakabe Yuka, stuns the world by retiring from show business. Kousaku’s friends try to cheer him up, but nothing can chase away his gloomy mood until Yuka herself transfers into his class at Tamo Agriculture School&#33;",
+		 "Madman",
+		 "&#64; 2014 Shirow Shiratori-SB Creative/NO-RIN Project"
+		)
+;
+
+INSERT INTO ANIME_VOLUME(ANIME_title, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
+		("No-Rin",
+		 0,
+		 1,
+		 "M - Sexual themes, sexualised imagery, sexual references and coarse language",
+		 12,
+		 "https://www.animelab.com/shows/norin"
+		)
+		("No-Rin",
+		 1,
+		 1,
+		 "M - Sexual themes, sexualised imagery, sexual references and coarse language",
+		 12,
+		 "https://www.madman.com.au/catalogue/view/31743/norin-complete-series"
+		)
+;
+
+INSERT INTO ANIME_EPISODE(ANIME_title, EPISODE_NUMBER, EPISODE_No-Rin, EPISODE_SYNOPSYS) VALUES
+		("No-Rin",
+		 1,
+		 "Chuno Love Story",
+		 "Teen idol Yuka Kusakabe announces her retirement. Kosaku Hata, a student of the Tamo Agricultural School and her biggest fan, is devastated. Soon afterwards new girl Ringo Kinoshita joins their class, only she seems strangely familiar."
+		)
+		("No-Rin",
+		 2,
+		 "The King of Farmers",
+		 "Kosaku shows new girl Ringo around the school and introduces her to childhood friend Minori Nakazawa, best friend Kei Kamatori, pet wallaby Wakadana and large breasted classmate Kocho Yoshida. During a welcome party Ringo seems oddly jealous of how close Minori is to Kosaku, at least until an unfortunate laundry room encounter turns into a "best panties" competition."
+		)
+		("No-Rin",
+		 3,
+		 "Super Sub-Mom War: Training Stage",
+		 "Kosaku is devastated to find Minori has thrown away all his Yuka Kusakabe merchandise, even his favourite body pillow. Meanwhile it is Ringo's first day farming with the other students and Minori is determined not to be beaten by an ex idol from the big city."
+		)("No-Rin",
+		 4,
+		 "The Smile You Showed Me",
+		 "Everybody must work together to catch a vegetable thief who has been pilfering from the fields at night. A misunderstanding leads to some embarrassment in Minori's bedroom. Kosaku is concerned Ringo has not smiled once since arriving at the school and several attempts are made to remedy this. "
+		)("No-Rin",
+		 5,
+		 "Farming's Five Top Four",
+		 "Through a series of unfortunate events Ringo meets three of the schools top farming students, the Yaoi obsessed class rep of the Biotechnology department, Akari &quot;Bio&quot; Suzuku, the forever lonely class rep of the Forestry department, Rintaro &quot;Woodman&quot; Miyamoto, and the strangely effeminate class rep of the Landscaping department, Kaoru &quot;Rose&quot; Hanazono."
+		)("No-Rin",
+		 6,
+		 "Full-Moe Alchemist: Bounty",
+		 "EKosaku and his friends realise they could sell more produce by using cute girls in their advertising. Thus appears expert money making student Torao &quot;Money&quot; Kanegami. Before long they are swamped with orders for Moe Eggs and erotically shaped mushrooms. Unfortunately that is against school rules and success is quickly followed by suspensions from class. At least Ringo looks good in a yukata."
+		)("No-Rin",
+		 7,
+		 "Sob Salad",
+		 "A sudden influx of excess vegetables leads to a sudden increase in body weight among the class. The untimely arrival of mould ruins Ringo's first crop of Soy beans, and drought followed by intense rainfall almost ruins this years tomato harvest. Certain truths are also revealed about Minori's involvement with the vegetables Kosaku sent anonymously to Ringo back when she was an idol."
+		)("No-Rin",
+		 8,
+		 "Cooking Granny",
+		 "40 year old class teacher Natsumi Bekki is tired of being single and embarks on a lust fuelled rampage against crops and students alike but is soon foiled by the intervention of Kei and a goat. A great debate has arisen amongst the boys, are large breasts better than small. Meanwhile, Kosaku, Ringo and Miss Bekki host a cooking show for the class that ultimately ends with the inappropriately nyotaimori-clad Miss Bekki being ejected from the room by her exasperated students."
+		)("No-Rin",
+		 9,
+		 "School Swim Trunks",
+		 "Minori has never once been to the beach. And she's not likely to anytime soon as rain, floods and heavy winds threaten the fields. Nothing a school bus in the right place can't fix. Now if only Kosaku can focus on his driving instead of Ringo in a swimsuit Minori may get her day at the beach after all."
+		)("No-Rin",
+		 10,
+		 "Veggie Battle: Ultimate v. Supreme",
+		 "Legendary School Principal Kuwanosuke Naganawa, who once killed a bear using only his hands, is slowly going mad with longing for the perfect eggplant. Meanwhile Kei's wealthy father, Hajime Menjo, owner of the Hexa Tech Corporation, challenges Kei to a sales duel. Kei's all organic produce against his chemically enhanced produce. If Kei loses he must leave his friends and return home with his father to run the company."
+		)("No-Rin",
+		 11,
+		 "Cheerful Farming Village",
+		 "With Kei and Kocho travelling together abroad Kosaku, Minori and Ringo return to Kosaku and Minori's hometown for summer break, however, a fake pregnancy announcement ends with Kosaku engaged to Minori with the full support of her parents. Could this be a fiendish plot. Either way, Ringo is less than pleased. Meanwhile, where has Wakadana been hiding all this time."
+		)("No-Rin",
+		 12,
+		 "Everybody's No-Rin",
+		 "Kosaku is coerced into going along with a fake wedding for the sake of the future of the village, however, Wakadana's hiding place is revealed at the crucial moment ruining the festive mood. An apologetic and heartfelt speech from Kosaku narrowly saves the day. Now if only he can choose between Ringo and Minori he might just avoid a punch to the face. Everybody returns to school where Ringo and Minori happily watch Kosaku being chased by the schools mad bull. "
+		)
+		
+		;
+
+INSERT INTO ANIME_SESSION(ANIME_title, SESSION_TYPE_ID, SESSION_NUMBER, NUMBER_OF_EPISODES) VALUES
+		("No-Rin", 3, 1, 2)
+		("No-Rin", 3, 2, 2)
+		("No-Rin", 3, 3, 2)
+		("No-Rin", 3, 4, 2)
+		("No-Rin", 3, 5, 2)
+		("No-Rin", 3, 6, 2)
+;
+		
+-- General Anime Volume Data
+	,	("title",
+		 n,
+		 m,
+		 "M - Sexual themes, sexualised imagery, sexual references and coarse language,
+		 x,
+		 "URL"
+		)
+		
+-- General Anime Episode Data
+	,	("title",
+		 n,
+		 "Episode_No-Rin",
+		 "Episode_Synopsys"
+		)
+		
+-- General Anime Session Data
+	,	("title", x, y, z)
