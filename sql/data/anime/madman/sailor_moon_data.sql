31c08a39ac7ddcbed705fb0439e4737891b01361
redtitan15 <36127845+redtitan15@users.noreply.github.com>
2018-02-16 22:35:48 +1100

    added episodes, volumes

--- sql/data/anime/madman/sailor_moon_data.sql
+++ sql/data/anime/madman/sailor_moon_data.sql
@@ -2,26 +2,279 @@
 INSERT INTO ANIME(ANIME_TITLE, NUMBER_OF_EPISODES, ANIME_SYNOPSIS, ANIME_DESCRIPTION, COMPANY_NAME, COPYRIGHT) VALUES
 		("Title",
 		 n,
-		 "Synopsys",
-		 "Description",
-		 "Company",
+		 "Usagi Tsukino is a cheerful 14-year-old schoolgirl who often finds herself in unwanted trouble. One day, she saves a talking cat named Luna from some mean kids, and her life is changed forever. Luna gives Usagi a magic brooch that transforms her into Sailor Moon, defender of love and justice! Now Usagi must work with Luna to find the other Sailor Guardians and the Moon Princess, whose Legendary Silver Crystal is Earth's only hope against the dark forces of the evil Queen Beryl!",
+		 "Usagi Tsukino is a cheerful 14-year-old schoolgirl who often finds herself in unwanted trouble. One day, she saves a talking cat named Luna from some mean kids, and her life is changed forever. Luna gives Usagi a magic brooch that transforms her into Sailor Moon, defender of love and justice! Now Usagi must work with Luna to find the other Sailor Guardians and the Moon Princess, whose Legendary Silver Crystal is Earth's only hope against the dark forces of the evil Queen Beryl!",
+		 "Madman Entertainment",
 		 "Copyright"
 		)
 ;
 
 INSERT INTO ANIME_VOLUME(ANIME_TITLE, VOLUME_TYPE_ID, VOLUME_NUMBER, CLASSIFICATION, NUMBER_OF_EPISODES, PURCHACE_URL) VALUES
-		("Title",
-		 n,
-		 m,
+		("Sailor Moon",
+		 1,
+		 1,
+		 "PG - Mild supernatural themes and animated violence",
+		 6,
+		 "URL"
+		)
+	,	("Title",
+		 2,
+		 2,
 		 "Classification",
 		 x,
 		 "URL"
 		)
+	,	("Title",
+		 3,
+		 3,
+		 "Classification",
+		 x,
+		 "URL"
+		)
+	,	("Title",
+		 4,
+		 4,
+		 "Classification",
+		 x,
+		 "URL"
+		)
+	,	("Title",
+		 5,
+		 5,
+		 "Classification",
+		 x,
+		 "URL"
+		)		
 ;
 
 INSERT INTO ANIME_EPISODE(ANIME_TITLE, EPISODE_NUMBER, EPISODE_TITLE, EPISODE_SYNOPSYS) VALUES
-		("Title",
-		 n,
+		("Sailor Moon",
+		 1,
+		 "The Crybaby: Usagi's Beautiful Transformation",
+		 "As the series begins as a slapstick comedy, a 14-year-old underachieving sailor-suited schoolgirl Usagi Tsukino saves a cat from being attacked by a group of little boys while on her way to school. After school, she and her friend Naru Osaka visit a local jewelry store owned by her mother. On her way home, Usagi encounters Mamoru Chiba and meets up with Motoki Furuhata. Upon returning to her home, Naru's mother reveals herself to be the youma Morga and captures her. The cat Usagi encountered earlier reveals herself as Luna and tells Usagi to search for the princess and destroy evil. Usagi transforms into Sailor Moon and, after hearing Naru in distress, confronts Morga, who intends to steal human energy by using the cursed jewelry on the girls. The youma tricks the brainwashed girls into attacking Usagi but a stranger wearing a tuxedo intervenes. Usagi weakens Morga with her screams, allowing her to use the Moon Tiara Action technique on Morga, killing her and restoring the girls to normal. Before departing, the stranger reveals himself to be Tuxedo Mask."
+		)
+	,	("Sailor Moon",
+		 2,
+		 "Punishment Awaits: the House of Fortune Is the Monster Mansion",
+		 "Having failed his previous mission, Jadeite informs Queen Beryl, the leader of the Dark Kingdom, that he is using another plan, this time involving fortune telling. Meanwhile, Usagi's classmate Umino Gurio is smitten with her. After confessing that he wants to go on a date, Naru has him consult a fortune teller but he decides to go to the House of Fortune. Luna tells Usagi to go to a fortune teller, in an attempt to check that Motoki is an admirer of her but decides to go to the House of Fortune. However, Umino and some of the other students are hypnotized by the tarot cards and cause trouble at school the next day. Realizing that the students were being hypnotized from the House of Fortune, Luna and Usagi infiltrate the house and encounter Balm, a youma disguised as the fortune teller. Balm orders her hypnotized subjects to attack Usagi, but Tuxedo Mask distracts her and, with his encouragement, Usagi kills Balm, returning the customers to normal. "
+		)
+	,	("Sailor Moon",
+		 3,
+		 "The Mysterious Sleeping Sickness: Protect the Girls in Love",
+		 "Homeroom teacher Haruna Sakurada and Naru submit love-letters to a late-night radio show. When they fall into a coma-like sleep, Usagi must investigate the radio station undercover."
+		)
+	,	("Sailor Moon",
+		 4,
+		 "Learn How to Be Skinny from Usagi",
+		 "Usagi gains a couple of pounds and starts to obsess about her weight. Jadeite starts a health club to drain energy out of people. Usagi and her friends sign up after seeing how much weight Ms. Haruna has lost. Usagi discovers tanks in the basement which are sucking the life energy out of her friends. "
+		)
+	,	("Sailor Moon",
+		 5,
+		 "Scent of a Monster: Chanela Will Steal Your Love",
+		 "Usagi wants her family to accept Luna as a "pet", but Shingo is afraid of cats. At a pet shop, he discovers a hypnotic creature called a Chanela and loses all interest in anything else. "
+		)
+	,	("Sailor Moon",
+		 6,
+		 "Protect the Melody of Love: Usagi Plays Cupid",
+		 "Jadeite creates a cassette tape that can drain life energy, but the youma he gives it to promptly loses it. It ends up in the possession of a jazz pianist, and Usagi must disguise herself as an adult to protect him. "
+		)
+	,	("Sailor Moon",
+		 7,
+		 "So You Want to Be a Superstar",
+		 "By making a youma possess a human celebrity, Jadeite draws a huge crowd of would-be superstars. Usagi tries to juggle her duties as Sailor Moon with her desire to be popular and famous. "
+		)
+	,	("Sailor Moon",
+		 8,
+		 "The Girl Genius Is a Monster: The Brainwashing Cram School of Horror",
+		 "Luna senses a strange aura around Usagi's genius classmate, Ami Mizuno, and thinks she might be a Dark Kingdom spy. Usagi befriends the girl and is not so sure. But she understands later that Ami Mizuno is a Guardian as Sailor Moon and their duties are the same. "
+		)
+	,	("Sailor Moon",
+		 9,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 10,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 11,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 12,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 13,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 14,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 15,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 16,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 17,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 18,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 19,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 20,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)	
+	,	("Sailor Moon",
+		 21,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 22,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 23,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 24,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 25,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 26,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 27,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 28,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 29,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 30,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)	
+	,	("Sailor Moon",
+		 31,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 32,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 33,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)	
+	,	("Sailor Moon",
+		 34,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 35,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 36,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 37,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 38,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 39,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 40,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 41,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 42,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 43,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 44,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 45,
+		 "Episode_Title",
+		 "Episode_Synopsys"
+		)
+	,	("Sailor Moon",
+		 46,
 		 "Episode_Title",
 		 "Episode_Synopsys"
 		)
