4c293ac8495492eff261c8e8d6fc5e6488eaa94f
James P. Galovic <jpgalovic@gmail.com>
2018-02-25 14:07:05 +1100

    Fix: Missing ; in friday_fill.sql, Add: misc article with we want you article

---
+++ sql/data/newsletter/misc_article.sql
@@ -0,0 +1,9 @@
+-- Misc Articles (commonly used or plugged), Verson 1.0.0, FEB18, JPGalovic
+INSERT INTO NEWS_ARTICLE (ARTICLE_DATE, ARTICLE_HEADLINE, ARTICLE_IMAGE_URL, ARTICLE_TEXT, ARTICLE_AUTHOR) VALUES
+		("2018-02-22 12-00-00",
+		 "SwinAnime x S.T.A.R.S. Wants You",
+		 "http://swinanime.net/image/news/volunteer_poster.png",
+		 "We are always looking for volunteers to help us run and plan our events, as such if you would like to help us to run or plan our events, simply visit <a href=&quot;volunteer.swinanime.net&quot; style=&quot;color: #FF6767; text-decoration: none;&quot;>volunteer.swinanime.net</a> and submit your interest today!",
+		 "J.P. Galovic"
+		)
+;
