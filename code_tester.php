f3a799bf5fdbe6610d22d70a877c245bb13f3e82
James P. Galovic <jpgalovic@gmail.com>
2018-02-25 21:51:11 +1100

    Change: Query now collects required data

--- code_tester.php
+++ code_tester.php
@@ -2,7 +2,7 @@
 	// Code Testing Platform, Used to test functionality of codegen.
 	include('code_gen/include.php'); // include base required files.2018-01-05 11:30:00
 
-	$data = get_newsletter_articles("2018-02-23 12-00-00");
+	$data = get_newsletter_articles("2018-02-23 12-00-00", "2");
 	//$data_row = $data->fetch_assoc();
 	//print_r(array_values($data_row));
 	
