46acbf3d93910d5ae1bb21ebc6ba4e5a99468670
James P. Galovic <jpgalovic@gmail.com>
2018-02-25 13:50:46 +1100

    Add: Friday Fill Newsletter Definintions

---
+++ sql/data/newsletter/friday_fill.sql
@@ -0,0 +1,43 @@
+-- Friday Fill Newletter Definitions, Version 1.0.0, FEB18, JPGalovic
+
+-- Semester 1, 2018
+INSERT INTO NEWSLETTER(PUBLICATION_DATE, PUBLICATION_TITLE, PUBLICATION_VOLUME) VALUES
+		("2018-02-23 12-00-00", "Friday Fill", 1)
+	,	("2018-03-02 12-00-00", "Friday Fill", 2)
+	,	("2018-03-09 12-00-00", "Friday Fill", 3)
+	,	("2018-03-16 12-00-00", "Friday Fill", 4)
+	,	("2018-03-23 12-00-00", "Friday Fill", 5)
+	,	("2018-03-30 12-00-00", "Friday Fill", 6)
+	,	("2018-04-06 12-00-00", "Friday Fill", 7)
+	,	("2018-04-13 12-00-00", "Friday Fill", 8)
+	,	("2018-04-20 12-00-00", "Friday Fill", 9)
+	,	("2018-04-27 12-00-00", "Friday Fill", 10)
+	,	("2018-05-04 12-00-00", "Friday Fill", 11)
+	,	("2018-05-11 12-00-00", "Friday Fill", 12)
+	,	("2018-05-18 12-00-00", "Friday Fill", 13)
+	,	("2018-05-25 12-00-00", "Friday Fill", 14)
+
+-- Winter Semester, 2018
+INSERT INTO NEWSLETTER(PUBLICATION_DATE, PUBLICATION_TITLE, PUBLICATION_VOLUME) VALUES
+		("2018-06-22 12-00-00", "Friday Fill", 15)
+	,	("2018-06-29 12-00-00", "Friday Fill", 16)
+	,	("2018-07-06 12-00-00", "Friday Fill", 17)
+	,	("2018-07-13 12-00-00", "Friday Fill", 18)
+	,	("2018-07-20 12-00-00", "Friday Fill", 19)
+	,	("2018-07-27 12-00-00", "Friday Fill", 20)
+
+-- Semester 2, 2018
+INSERT INTO NEWSLETTER(PUBLICATION_DATE, PUBLICATION_TITLE, PUBLICATION_VOLUME) VALUES
+		("2018-08-03 12-00-00", "Friday Fill", 21)
+	,	("2018-08-10 12-00-00", "Friday Fill", 22)
+	,	("2018-08-17 12-00-00", "Friday Fill", 23)
+	,	("2018-08-24 12-00-00", "Friday Fill", 24)
+	,	("2018-08-31 12-00-00", "Friday Fill", 25)
+	,	("2018-09-07 12-00-00", "Friday Fill", 26)
+	,	("2018-09-14 12-00-00", "Friday Fill", 27)
+	,	("2018-09-21 12-00-00", "Friday Fill", 28)
+	,	("2018-09-28 12-00-00", "Friday Fill", 29)
+	,	("2018-10-05 12-00-00", "Friday Fill", 30)
+	,	("2018-10-12 12-00-00", "Friday Fill", 31)
+	,	("2018-10-18 12-00-00", "Friday Fill", 32)
+	,	("2018-10-26 12-00-00", "Friday Fill", 33)
