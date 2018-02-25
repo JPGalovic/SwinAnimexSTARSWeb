b3e8ffde4b745fd762b12858c1921a53ee0a65e4
James P. Galovic <jpgalovic@gmail.com>
2018-02-25 15:09:15 +1100

    Change: Trying to fix issues

--- code_tester.php
+++ code_tester.php
@@ -2,22 +2,29 @@
 	// Code Testing Platform, Used to test functionality of codegen.
 	include('code_gen/include.php'); // include base required files.2018-01-05 11:30:00
 
-	$data = get_event_data();
-	$data_row = $data->fetch_assoc();
-	print_r(array_values($data_row));
+	$data = get_newsletter_articles("2018-02-23 12-00-00");
+	//$data_row = $data->fetch_assoc();
+	//print_r(array_values($data_row));
 	
 	echo(current_time());
+
+	echo('<table width="600px" border="0" cellspacing="0" cellpadding="10" style="border: 1px solid black;"><tbody>');
+
+	while($data_row = $data->fetch_assoc())
+	{
+		echo('<tr><td colspan="4">');
+			if($data_row['ARTICLE_IMAGE_URL'] != null)
+				echo('<img src="'.$data_row['ARTICLE_IMAGE_URL'].'" alt="Showcase" width="100%">');
+			echo('<h2 style="color: #555555; font-size: 18px;">'.$data_row['ARTICLE_HEADLINE'].'</h2>');
+			echo('<p>'.$data_row['ARTICLE_TEXT'].'</p>');
+			echo('<p style="font-size: 12px; color: #A6A6A6;">Article written by: '.$data_row['ARTICLE_AUTHOR'].'</p>');
+		echo('</td></tr>');
+	}
+
+
+	echo('</tbody></table>');
 	
 
 ?>
-
-<table>
-	<tr>
-		<td colspan="4">
-			<img src="http://swinanime.net/image/news/volunteer_poster.png" alt="Showcase" width="100%">
-			<h2 style="color: #555555; font-size: 18px;">SwinAnime x S.T.A.R.S. Wants You</h2>
-			<p>We are always looking for volunteers to help us run and plan our events, as such if you would like to help us to run or plan our events, simply visit <a href="volunteer.swinanime.net" style="color: #FF6767; text-decoration: none;">volunteer.swinanime.net</a> and submit your interest today!</p>
-			<p style="font-size: 12px; color: #A6A6A6;">Article written by: James P. Galovic</p>
-		</td>  
-	</tr>
-</table>
+				
+				
--- sql/data/newsletter/article/misc_article.sql
+++ sql/data/newsletter/article/misc_article.sql
@@ -3,7 +3,7 @@ INSERT INTO NEWS_ARTICLE (ARTICLE_DATE, ARTICLE_HEADLINE, ARTICLE_IMAGE_URL, ART
 		("2018-02-22 12-00-00",
 		 "SwinAnime x S.T.A.R.S. Wants You",
 		 "http://swinanime.net/image/news/volunteer_poster.png",
-		 "We are always looking for volunteers to help us run and plan our events, as such if you would like to help us to run or plan our events, simply visit <a href=&quot;volunteer.swinanime.net&quot; style=&quot;color: #FF6767; text-decoration: none;&quot;>volunteer.swinanime.net</a> and submit your interest today!",
+		 "We are always looking for volunteers to help us run and plan our events, as such if you would like to help us to run or plan our events, simply visit <a href=&quot;http://volunteer.swinanime.net/&quot; style=&quot;color: #FF6767; text-decoration: none;&quot;>volunteer.swinanime.net</a> and submit your interest today!",
 		 "J.P. Galovic"
 		)
 ;
