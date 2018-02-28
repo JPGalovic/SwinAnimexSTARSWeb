484118fb49ddc12dd5dce7b060e11e677501fc42
James P. Galovic <jpgalovic@gmail.com>
2018-03-01 00:12:58 +1100

    Fix: Timezone Settings

--- code_gen/misc_var.php
+++ code_gen/misc_var.php
@@ -1,13 +1,10 @@
 <?php
 	//Misc Variables and Getters, Verson 1.0.1, FEB18, JPGalovic
-	date_default_timezone_set('Australia/Melbourne');
 
 	//gets current time
 	function current_time()
 	{
+		date_default_timezone_set('Australia/ACT');
 		return date('Y-m-d H:i:s', time());
 	}
-
-	$now = date('Y-m-d H:i:s', time() + (120 * 60));
-	$time = date('Y-m-d H:i:s', time());
 ?>
