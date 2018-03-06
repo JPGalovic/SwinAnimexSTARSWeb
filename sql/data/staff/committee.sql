2233625ae35f9e53331790cfc3a16c6d551cfb79
James P. Galovic <jpgalovic@gmail.com>
2018-03-06 19:08:10 +1100

    Add: Committee, Moderator and Volunteer SQL files TODO: Add Mod and Volunteer Details

---
+++ sql/data/staff/committee.sql
@@ -0,0 +1,18 @@
+-- Committee Staff, Version 1.0.0, MAR18, JPGalovic
+-- 2018 Club Committee
+INSERT INTO STAFF (STAFF_NAME, EMAIL, MOBILE_NUMBER, STUDENT_ID, DISCORD_USER, EVENT_SUPER, MARKETING_TEAM, WEBKIT, GAME_MASTER) VALUES
+		-- President
+		("Xander James George Boyle", "xander.boyle@swinanime.net", "0400 805 284", "6449573", "Xandahar#3867", true, true, true, true)
+		-- Vise President
+	,	("Luke &quot;Mecha&quote; Giesemann", "luke.giesemann@swinanime.net", "0439 815 698", "6514898", "Mechafox#2706", true, true, true, true)
+		-- Secretary
+	-- ,	("", ".@swinanime.net", "", "", "", true, true, true, true)	
+		-- Treasurer
+	,	("Jason Simmons-Browne", "jason.simmons@swinanime.net", "0410 295 278", "101535150", "Test name please ignore#0576", true, true, true, true)
+		-- Club Liasion
+	,	("James Galovic", "james.galovic@swinanime.net", "0410 279 719", "6819710", "dragonofdarkness#6763", true, true, true, true)
+		-- Anime Events Manager
+	,	("Thomas William Tuhan", "thomas.tuhan@swinanime.net", "0432 798 990", "1628534", "Redtitan15#7807", true, true, true, true)
+		-- Roleplay Events Manager
+	,	("Jonathan Lowe", "jonathan.lowe@swinanime.net", "0457 269 636", "100577290", "dmitriR#7165", true, true, true, true)
+;
