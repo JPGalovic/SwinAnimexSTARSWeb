ea765b9b867b77053534d247dde0f887ca72883f
James P. Galovic <jpgalovic@gmail.com>
2018-03-15 07:16:17 +1100

    Add: Staff Table

--- sql/admin/staff_data_core.sql
+++ sql/admin/staff_data_core.sql
@@ -1,4 +1,4 @@
--- Staff Data Core, Version 1.0.0, MAR18, J.P.Galovic
+-- Staff Data Core, Version 1.0.2, MAR18, J.P.Galovic
 -- Staff Type Key Table, Contains Data and Descriptors for the differnt types of Staff
 CREATE TABLE IF NOT EXISTS STAFF_TYPE (
 		TYPE					VARCHAR(10)
@@ -10,4 +10,16 @@ CREATE TABLE IF NOT EXISTS STAFF_TYPE (
 INSERT INTO STAFF_TYPE(TYPE, TYPE_DESCRIPTION) VALUES
 		("Club Committee", "Staff Role for Club Committee members.")
 	,	("Webteam", "Staff Role for those on the club's Web Development Team")
+	,	("Volunteer", "StaffGeneral Volunteer")
 ;
+
+-- Staff Table
+-- No local data is created
+CREATE TABLE IF NOT EXISTS STAFF (
+		NAME					VARCHAR(150)
+	,	STUDENT_ID				VARCHAR(15)
+	,	CONTACT_NUMBER			INT(11)
+	,	CONTACT_EMAIL			VARCHAR(150)
+	,	PASS					VARCHAR(100)
+	,	SALT					VARCHAR(25)
+)
